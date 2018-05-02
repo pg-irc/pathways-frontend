964fc642e6f4bc7205c371f5c08e550cd8a85fcc
Ben St John <ben@affinitybridge.com>
2018-05-02 12:29:53 -0700

    Issue #44 Add initial task detail, task list, and task label components and some dummy data

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -4,12 +4,39 @@ import { ConnectedHelloWorld } from '../components/hello_world/connected_hello_w
 import { ConnectedNavigationBar } from '../components/navigation_bar/connected_navigation_bar';
 import { store } from './store';
 import { ErrorBoundary } from './error_boundary';
+import { TaskList } from '../components/task_list/task_list';
+
+const tasks = [
+    {
+        id: 1,
+        complete: false,
+        starred: false,
+        taskDefinition: {
+            id: 1,
+            category: 'Education',
+            description: 'Enroll child in elementary school.',
+            labels: ['Important', 'Do Soon', 'Free'],
+        },
+    },
+    {
+        id: 2,
+        complete: false,
+        starred: true,
+        taskDefinition: {
+            id: 2,
+            category: 'Education',
+            description: 'Enroll child in middle or secondary school.',
+            labels: ['Important', 'Free'],
+        },
+    },
+];
 
 export const Application = (): JSX.Element => (
     <ErrorBoundary>
         <Provider store={store}>
             <ConnectedNavigationBar>
                 <ConnectedHelloWorld />
+                <TaskList tasks={tasks} />
             </ConnectedNavigationBar>
         </Provider>
     </ErrorBoundary>
