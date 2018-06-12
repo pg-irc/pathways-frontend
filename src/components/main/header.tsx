75672114f5591605eee45922d8d281d53ae17839
Tom Nightingale <tom@tnightingale.com>
2018-06-11 18:08:43 -0700

    Issue #106 Fix header icon direction in RTL locale

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header, Left, Button, Icon, Title, Body, Right } from 'native-base';
 import { CurrentLocale } from '../language_switcher/current_locale';
 import { Locale } from '../../locale';
+import { I18nManager } from 'react-native';
 
 export interface Props {
     readonly canGoBack: boolean;
@@ -19,7 +20,7 @@ export const Component: React.StatelessComponent<Props & Actions> = (props: Prop
         <Header>
             <Left>
                 <Button transparent disabled={!canGoBack} onPress={goBack}>
-                    <Icon name='arrow-back' />
+                    <Icon name={ I18nManager.isRTL ? 'arrow-forward' : 'arrow-back' } />
                 </Button>
             </Left>
             <Body>
