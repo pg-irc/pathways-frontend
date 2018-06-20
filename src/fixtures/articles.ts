810b82e85b57a882c30ef7963ad780877f6a1143
Ben St John <ben@affinitybridge.com>
2018-06-20 14:32:37 -0700

    Issue #110 Add initial shape for articles

---
+++ src/fixtures/articles.ts
@@ -0,0 +1,60 @@
+import { LocalizedText } from '../locale';
+import { Id as TaskId } from './tasks';
+
+export type Id = string;
+
+export interface Article {
+    readonly id: Id;
+    readonly name: LocalizedText;
+    readonly content: LocalizedText;
+    readonly relatedTasks?: ReadonlyArray<TaskId>;
+    readonly relatedArticles?: ReadonlyArray<Id>;
+    readonly isStarred: boolean;
+}
+
+export interface ArticleMap {
+    readonly [key: string]: Article;
+}
+
+export interface Store {
+    readonly articles: ArticleMap;
+}
+
+export const buildArticlesFixture = (): Store => (
+    {
+        articles: {
+            'a1': {
+                'id': 'a1',
+                'name': {
+                    'en': 'British Columbia\'s Education System',
+                    'ar': 'نظام التعليم في كولومبيا البريطانية',
+                    'zh': '卑诗省的教育体系',
+                },
+                'content': {
+                    'en': 'In British Columbia, all children between the ages of five and sixteen must go to school.',
+                    'ar': 'في كولومبيا البريطانية ، يجب أن يذهب جميع الأطفال الذين تتراوح أعمارهم بين الخامسة والسادسة عشرة إلى المدرسة.',
+                    'zh': '在不列颠哥伦比亚省，所有五至十六岁的孩子都必须上学。',
+                },
+                'relatedTasks': ['t1', 't2', 't3'],
+                'relatedArticles': ['a2'],
+                'isStarred': false,
+            },
+            'a2': {
+                'id': 'a2',
+                'name': {
+                    'en': 'Elementary, middle, and secondary school',
+                    'ar': 'المدرسة الابتدائية والمتوسطة والثانوية',
+                    'zh': '小学，中学和中学',
+                },
+                'content': {
+                    'en': 'In British Columbia, all children between the ages of five and sixteen must go to school.',
+                    'ar': 'في كولومبيا البريطانية ، يجب أن يذهب جميع الأطفال الذين تتراوح أعمارهم بين الخامسة والسادسة عشرة إلى المدرسة.',
+                    'zh': '在不列颠哥伦比亚省，所有五至十六岁的孩子都必须上学。',
+                },
+                'relatedTasks': ['t1', 't2', 't3'],
+                'relatedArticles': ['a1'],
+                'isStarred': false,
+            },
+        },
+    }
+);
