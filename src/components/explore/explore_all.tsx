5901db14689f5a1a916216d060ff3815adadb816
Ben St John <ben@affinitybridge.com>
2018-07-06 14:52:55 -0700

    Issue #107 Clean up explore work

--- src/components/explore/explore_all.tsx
+++ src/components/explore/explore_all.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Content, Grid, Row, Col, Button, Icon, Text } from 'native-base';
+import { View, Content, Button, Icon, Text } from 'native-base';
 import { ExploreSection } from '../../selectors/explore';
 import { Trans } from '@lingui/react';
-import { exploreStyles } from './styles';
 import { computeUniqueKeyForSections } from './compute_unique_key_for_sections';
 import { applicationStyles } from '../../application/styles';
 import { Id } from '../../stores/explore';
-import { SetExploreSectionPageAction } from '../../stores/page_switcher';
+import { SetExploreSectionPageAction, SetExplorePageAction } from '../../stores/page_switcher';
 import * as R from 'ramda';
 
 export interface ExploreAllProps {
@@ -22,18 +21,25 @@ type AllExploreProps = ExploreAllProps & ExploreAllActions;
 export const ExploreAllComponent: React.StatelessComponent<AllExploreProps> =
     (props: AllExploreProps): JSX.Element => {
         const sectionsGroupedIntoThrees = R.splitEvery(3, props.sections);
+        const renderSectionButton = (section: ExploreSection): JSX.Element => (
+            <SectionButton
+                onPress={(): SetExploreSectionPageAction => props.goToExploreSection(section.id)}
+                buttonStyle={{height: 100}}
+                {...section}
+            />
+        );
         return <Content padder>
             <Text style={applicationStyles.pageTitle}>Learn about:</Text>
-            <Grid>
+            <View style={[{flex: 1}]}>
                 {sectionsGroupedIntoThrees.map((sections: ReadonlyArray<ExploreSection>) => (
-                    <Row key={computeUniqueKeyForSections(sections)}>
-                        <RowOfSectionButtons
-                            sections={sections}
-                            goToExploreSection={props.goToExploreSection}
-                        />
-                    </Row>
+                    <RowOfSectionButtons
+                        key={computeUniqueKeyForSections(sections)}
+                        sections={sections}
+                        goToExploreSection={props.goToExploreSection}
+                        renderSectionButton={renderSectionButton}
+                    />
                 ))}
-            </Grid>
+            </View>
         </Content>;
     };
 
@@ -41,30 +47,37 @@ interface ButtonRowProps {
     readonly sections: ReadonlyArray<ExploreSection>;
 }
 
-const RowOfSectionButtons = (props: ButtonRowProps & ExploreAllActions): JSX.Element => (
-    <Row>
-        {props.sections.map((section: ExploreSection) => (
-            <SectionButton
-                key={section.id}
-                id={section.id}
-                name={section.name}
-                introduction={section.introduction}
-                icon={section.icon}
-                goToExploreSection={props.goToExploreSection}
-            />
-        ))}
-    </Row>
+interface ButtonRowActions {
+    readonly renderSectionButton: (section: ExploreSection) => JSX.Element;
+}
+
+export const RowOfSectionButtons = (props: ButtonRowProps & ButtonRowActions & ExploreAllActions): JSX.Element => (
+    <View style={[{flex: props.sections.length, flexDirection: 'row', justifyContent: 'space-evenly'}]}>
+        {R.map((section: ExploreSection) => (
+            <View key={section.id} style={[{flex: 1, margin: 10}]}>
+                {props.renderSectionButton(section)}
+            </View>
+        ), props.sections)}
+    </View>
 );
 
-const SectionButton = (props: ExploreSection & ExploreAllActions): JSX.Element => (
-    <Col>
-        <Content style={exploreStyles.sectionButtonContent}>
-            <Button block rounded light
-                style={exploreStyles.sectionButton}
-                onPress={(): SetExploreSectionPageAction => props.goToExploreSection(props.id)}>
-                <Icon type='MaterialCommunityIcons' name={props.icon} style={{ fontSize: 60 }} />
-            </Button>
-            <Text style={exploreStyles.sectionButtonCaption}><Trans>{props.name}</Trans></Text>
-        </Content>
-    </Col >
+interface SectionButtonProps {
+    readonly icon: string;
+    readonly name: string;
+    readonly buttonStyle?: object;
+}
+
+interface SectionButtonActions {
+    readonly onPress: () => SetExplorePageAction | SetExploreSectionPageAction;
+}
+
+export const SectionButton = (props: SectionButtonProps & SectionButtonActions): JSX.Element => (
+    <View>
+        <Button block rounded light
+            style={[props.buttonStyle]}
+            onPress={props.onPress} >
+            <Icon type='MaterialCommunityIcons' name={props.icon} style={{ fontSize: 40 }} />
+        </Button>
+        <Text style={[{textAlign: 'center'}]}><Trans>{props.name}</Trans></Text>
+    </View>
 );
