444e344a49e2f49e56ef5403bca82bc95e4ba6a7
rasmus <rasmus@peacegeeks.org>
2018-07-18 17:04:45 -0700

    Issue #147 Refactor up learn section on home screen

--- src/components/home_page/home_page.tsx
+++ src/components/home_page/home_page.tsx
@@ -21,45 +21,25 @@ export interface HomePageActions extends ExploreAllActions {
     readonly goToTaskDetail: (taskId: TaskId) => SetTaskDetailPageAction;
     readonly addToSavedList: (taskId: TaskId) => AddToSavedListAction;
 }
-type AllHomePageProps = HomePageProps & HomePageActions;
+type AllHomePageProps = I18nProps & HomePageProps & HomePageActions;
 
-export const HomePageComponent: React.StatelessComponent<AllHomePageProps> = (props: I18nProps & AllHomePageProps): JSX.Element => {
-    const i18n = props.i18n;
-    const sectionsGroupedIntoFour = R.splitEvery(4, R.slice(0, 8, props.sections));
-    const renderSectionButton = (section: ExploreSection): JSX.Element => {
-        const goToExplorePage = (): SetExplorePageAction => props.goToExplorePage();
-        const goToExploreSection = (): SetExploreSectionPageAction => props.goToExploreSection(section.id);
-        const style = {height: 70};
-        const moreButtonProps = {onPress: goToExplorePage, icon: 'apps', name: i18n.t`More`, buttonStyle: style};
-        const sectionButtonProps = {onPress: goToExploreSection, buttonStyle: style, ...section};
-        // TODO Improve the "more" functionality once designs are nailed down
-        return section.id === 's8' ? <SectionButton {...moreButtonProps} /> : <SectionButton {...sectionButtonProps} />;
-    };
+export const HomePageComponent: React.StatelessComponent<AllHomePageProps> = (props: AllHomePageProps): JSX.Element => {
     return (
         <Content padder>
             <Text style={applicationStyles.pageTitle}><Trans>How can we help you?</Trans></Text>
             <View style={applicationStyles.hr} />
             <Text style={applicationStyles.bold}><Trans>LEARN ABOUT</Trans></Text>
-            <View>
-                {sectionsGroupedIntoFour.map((sections: ReadonlyArray<ExploreSection>) => (
-                    <RowOfSectionButtons
-                        key={computeUniqueKeyForSections(sections)}
-                        sections={sections}
-                        goToExploreSection={props.goToExploreSection}
-                        renderSectionButton={renderSectionButton}
-                    />
-                ))}
-            </View>
+            <LearnSectionComponent {...props} />
             <View style={applicationStyles.hr} />
-            <Text style={[applicationStyles.bold, {marginBottom: 10}]}><Trans>PLAN FOR SUCCESS</Trans></Text>
+            <Text style={[applicationStyles.bold, { marginBottom: 10 }]}><Trans>PLAN FOR SUCCESS</Trans></Text>
             <TaskListComponent
                 tasks={props.tasks}
                 goToTaskDetail={props.goToTaskDetail}
                 addToSavedList={props.addToSavedList}
-                listItemStyle={{backgroundColor: colors.lighterGrey}}/>
-            <View style={[{flex: 1, flexDirection: 'row', justifyContent: 'center', paddingTop: 10}]}>
+                listItemStyle={{ backgroundColor: colors.lighterGrey }} />
+            <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center', paddingTop: 10 }]}>
                 <Button
-                    style={[{backgroundColor: colors.darkGrey}]}
+                    style={[{ backgroundColor: colors.darkGrey }]}
                     onPress={props.goToPlanPage}>
                     <Text><Trans>SEE MORE TASKS IN MY PLAN</Trans></Text>
                 </Button>
@@ -68,4 +48,38 @@ export const HomePageComponent: React.StatelessComponent<AllHomePageProps> = (pr
             <CopyrightComponent />
         </Content>
     );
+};
+
+const LearnSectionComponent: React.StatelessComponent<AllHomePageProps> = (props: AllHomePageProps): JSX.Element => {
+    const sectionsGroupedIntoFour = R.splitEvery(4, R.slice(0, 8, props.sections));
+    return <View>
+        {sectionsGroupedIntoFour.map((sections: ReadonlyArray<ExploreSection>) => (
+            <RowOfSectionButtons
+                key={computeUniqueKeyForSections(sections)}
+                sections={sections}
+                goToExploreSection={props.goToExploreSection}
+                renderSectionButton={renderButton(props)}
+            />
+        ))}
+    </View>;
+};
+
+const renderButton = R.curry((props: AllHomePageProps, section: ExploreSection): JSX.Element => {
+    // TODO Improve the "more" functionality once designs are nailed down
+    return section.id === 's8' ? renderMoreButton(props) : renderSectionButton(props, section);
+});
+
+const renderSectionButton = (props: AllHomePageProps, section: ExploreSection): JSX.Element => {
+    const goToExploreSection = (): SetExploreSectionPageAction => props.goToExploreSection(section.id);
+    const style = { height: 70 };
+    const buttonProps = { onPress: goToExploreSection, buttonStyle: style, ...section };
+    return <SectionButton {...buttonProps} />;
+};
+
+const renderMoreButton = (props: AllHomePageProps): JSX.Element => {
+    const i18n = props.i18n;
+    const goToExplorePage = (): SetExplorePageAction => props.goToExplorePage();
+    const style = { height: 70 };
+    const buttonProps = { onPress: goToExplorePage, icon: 'apps', name: i18n.t`More`, buttonStyle: style };
+    return <SectionButton {...buttonProps} />;
 };
