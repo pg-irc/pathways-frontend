6351882377b4a3773a9dfa34c9144f3d66590e83
rasmus <rasmus@peacegeeks.org>
2018-07-20 12:06:42 -0700

    Issue #152 Show string in My Plan when there are no tasks

--- src/components/tasks/task_list.tsx
+++ src/components/tasks/task_list.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as R from 'ramda';
-import { View } from 'native-base';
+import { Text, View } from 'native-base';
+import { Trans } from '@lingui/react';
 import { TaskListItem } from '../../selectors/tasks';
 import { Id as TaskId } from '../../stores/tasks';
 import { TaskListItemComponent, TaskListItemActions, TaskListItemStyleProps } from './task_list_item';
@@ -9,12 +10,21 @@ import { RouterProps } from '../../application/routing';
 export interface TaskListProps extends TaskListItemStyleProps {
     readonly tasks: ReadonlyArray<TaskListItem>;
 }
+
 export interface TaskListActions extends TaskListItemActions {
     readonly shouldDisplayTaskInteractions?: (taskId: TaskId) => boolean;
 }
 type AllTaskListProps = TaskListProps & TaskListActions & RouterProps;
 
 export const TaskListComponent: React.StatelessComponent<AllTaskListProps> = (props: AllTaskListProps): JSX.Element => (
+    R.isEmpty(props.tasks) ? <EmptyTaskListComponent /> : <NonEmptyTaskListComponent {...props} />
+);
+
+const EmptyTaskListComponent: React.StatelessComponent = (): JSX.Element => (
+    <Text><Trans>No tasks added yet</Trans></Text>
+);
+
+const NonEmptyTaskListComponent: React.StatelessComponent<AllTaskListProps> = (props: AllTaskListProps): JSX.Element => (
     <View>
         {R.map((task: TaskListItem) =>
             <TaskListItemComponent
