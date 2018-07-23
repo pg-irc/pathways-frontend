6c36d3853e7e36de0922db71e81aeecb96cde374
Ben St John <ben@affinitybridge.com>
2018-07-23 10:29:01 -0700

    Issue #154 Removed page_switcher store and add routing tests

---
+++ src/application/__tests__/routing.test.ts
@@ -0,0 +1,44 @@
+// tslint:disable:no-expression-statement
+import { Routes, routePath, routePathWithArgument } from '../routing';
+
+describe('the routePath function', () => {
+
+    it('For Routes.Home returns path: "/"', () => {
+        expect(routePath(Routes.Welcome)).toEqual('/');
+    });
+
+    it('For Routes.Welcome returns path: "/welcome"', () => {
+        expect(routePath(Routes.Home)).toEqual('/home');
+    });
+
+    it('For Routes.Questionnaire returns path: "/questionnaire', () => {
+        expect(routePath(Routes.Questionnaire)).toEqual('/questionnaire');
+    });
+
+    it('For Routes.MyPlan returns path: "/my-plan', () => {
+        expect(routePath(Routes.MyPlan)).toEqual('/my-plan');
+    });
+
+    it('For Routes.Learn returns path: "/learn', () => {
+        expect(routePath(Routes.Learn)).toEqual('/learn');
+    });
+
+    it('For Routes.LearnDetail returns path: "/learn/:learnId', () => {
+        expect(routePath(Routes.LearnDetail)).toEqual('/learn/:learnId');
+    });
+
+    it('For Routes.TaskDetail returns path: "/task/:taskId', () => {
+        expect(routePath(Routes.TaskDetail)).toEqual('/task/:taskId');
+    });
+
+    it('For Routes.ArticleDetail returns path: "/article/:articleId', () => {
+        expect(routePath(Routes.ArticleDetail)).toEqual('/article/:articleId');
+    });
+});
+
+describe('the routePathWithArgument function', () => {
+
+    it('Replaces a Route argument placeholder with provided value', () => {
+        expect(routePathWithArgument(Routes.LearnDetail, 'l1')).toEqual('/learn/l1');
+    });
+});
