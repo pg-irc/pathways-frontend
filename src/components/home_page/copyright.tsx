caaef4663a65e2ccbec4669f6865f3d864d6d685
rasmus <rasmus@peacegeeks.org>
2018-07-27 16:00:21 -0700

    Issue #79 Polish to copyright notice: Make link clickable

--- src/components/home_page/copyright.tsx
+++ src/components/home_page/copyright.tsx
@@ -1,13 +1,22 @@
+// tslint:disable:no-expression-statement
+
 import React from 'react';
 import { View, Text } from 'native-base';
-import { values } from '../../application/styles';
+import { values, colors } from '../../application/styles';
+import { Linking } from 'react-native';
+
+const welcomeBcUrl = 'https://www.welcomebc.ca/Start-Your-Life-in-B-C/Newcomers-Guides/Newcomers-Guide-Provincial';
 
 export const CopyrightComponent: React.StatelessComponent = (): JSX.Element => (
-    <View style={[{marginTop: 10}]}>
-        <Text style={[{fontSize: values.smallTextSize, textAlign: 'center'}]}>
-            British Columbia Newcomers' Guide to Resources and Services. WelcomeBC
-            https://www.welcomebc.ca/Start-Your-Life-in-B-C/Newcomers-Guides/Newcomers-Guide-Provincial
-            Copyright Province of British Columbia. All rights reserved.
+    <View style={[{ marginTop: 10 }]}>
+        <Text style={[{ fontSize: values.smallTextSize, textAlign: 'center' }]}>
+            British Columbia Newcomers' Guide to Resources and Services.
+            WelcomeBC <Text
+                onPress={(): void => { Linking.openURL(welcomeBcUrl); }}
+                style={[
+                    { color: colors.urlColor },
+                    { fontSize: values.smallTextSize },
+                ]}>{welcomeBcUrl}</Text> Copyright Province of British Columbia. All rights reserved.
         </Text>
     </View>
 );
