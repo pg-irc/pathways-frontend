a32207bcf16e5320fa19cc97a95156a5776e42e5
Ben St John <ben@affinitybridge.com>
2018-07-27 17:41:44 -0700

    Issue #150 Implement accordion for questionnaire questions

--- src/components/questionnaire/question.tsx
+++ src/components/questionnaire/question.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Button, Grid, Row, Col, Text, View } from 'native-base';
+import { Button, Grid, Row, Col, Text } from 'native-base';
 import { Answer, AnswerActions } from './answer';
 import * as selector from '../../selectors/questionnaire';
 import { QuestionnaireActions } from './actions';
-import { applicationStyles } from '../../application/styles';
 import { questionStyles } from './styles';
 import { Trans } from '@lingui/react';
-import { Collapser } from '../collapser/collapser';
 import { RouterProps, goToRouteWithoutParameter, Routes } from '../../application/routing';
 import { History } from 'history';
 
@@ -17,52 +15,28 @@ export interface QuestionProps {
 type Props = QuestionProps & QuestionnaireActions & AnswerActions & RouterProps;
 
 export const Question: React.StatelessComponent<Props> = (props: Props): JSX.Element => {
+    const { question, selectAnswer, isFinalQuestion, history }: Props = props;
     return (
-        <Collapser
-            collapsedHeader={renderQuestionCollapsedHeader(props)}
-            expandedHeader={renderQuestionExpandedHeader(props)}
-            content={renderQuestionContent(props)}
-            // TODO - This should likely be the next unaswered question in the questionnaire
-            initiallyCollapsed={props.question.number === 1 ? false : true }
-        />
+        <Grid style={questionStyles.questionWrapper}>
+            <Row>
+                <Col>
+                {question.explanation ? <Text style={questionStyles.explanationText}>{question.explanation}</Text> : undefined}
+                {question.answers.map((answer: selector.Answer) => (
+                    <Answer
+                        key={answer.id}
+                        answer={answer}
+                        selectAnswer={selectAnswer}
+                        acceptMultipleAnswers={answer.acceptMultipleAnswers}
+                    />
+                ))}
+                </Col>
+            </Row>
+            <Row style={questionStyles.buttonsWrapper}>
+                {isFinalQuestion ? renderFinalQuestionButton(history) : renderNextQuestionButton()}
+            </Row>
+        </Grid>
     );
 };
-
-const renderQuestionCollapsedHeader = ({ question }: Props): JSX.Element => (
-    <Text style={applicationStyles.bold}>
-        <Text style={applicationStyles.bold}>{question.number}. {question.text}</Text>
-    </Text>
-);
-
-const renderQuestionExpandedHeader = ({ question }: Props): JSX.Element => (
-    <Row>
-        <View style={questionStyles.expandedBG}>
-            <Text style={questionStyles.expandedText}>{question.number}</Text>
-        </View>
-        <Text style={applicationStyles.bold}>{question.text}</Text>
-    </Row>
-);
-
-const renderQuestionContent = ({ question, isFinalQuestion, selectAnswer, history }: Props): JSX.Element => (
-    <Grid style={questionStyles.questionWrapper}>
-        <Row>
-            <Col>
-            {question.explanation ? <Text style={questionStyles.explanationText}>{question.explanation}</Text> : undefined}
-            {question.answers.map((answer: selector.Answer) => (
-                <Answer
-                    key={answer.id}
-                    answer={answer}
-                    selectAnswer={selectAnswer}
-                    acceptMultipleAnswers={answer.acceptMultipleAnswers}
-                />
-            ))}
-            </Col>
-        </Row>
-        <Row style={questionStyles.buttonsWrapper}>
-            {isFinalQuestion ? renderFinalQuestionButton(history) : renderNextQuestionButton()}
-        </Row>
-    </Grid>
-);
 
 const renderFinalQuestionButton = (history: History): JSX.Element => (
     <Button style={questionStyles.nextButton} small onPress={goToRouteWithoutParameter(Routes.MyPlan, history)}>
--- src/components/questionnaire/questionnaire.tsx
+++ src/components/questionnaire/questionnaire.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import * as R from 'ramda';
+import Accordion from 'react-native-collapsible/Accordion';
 import { Content, Text } from 'native-base';
 import { Question} from './question';
 import * as selector from '../../selectors/questionnaire';
 import { QuestionnaireActions } from './actions';
-import { applicationStyles } from '../../application/styles';
+import { applicationStyles, colors } from '../../application/styles';
 import { questionnaireStyles } from './styles';
 import { Trans } from '@lingui/react';
 import { RouterProps } from '../../application/routing';
@@ -19,14 +21,42 @@ export const Component: React.StatelessComponent<Props> = (props: Props): JSX.El
         <Text style={questionnaireStyles.introText}>
             <Trans>There is no requirement to answer any of the following questions but in doing so you help us recommend tasks right for you.</Trans>
         </Text>
-        {props.questionnaire.map((question: selector.Question) => (
-            <Question
-                {...props}
-                key={question.id}
-                question={question}
-                selectAnswer={props.selectAnswer}
-                isFinalQuestion={question.number === props.questionnaire.length}
-            />
-        ))}
+        <Accordion
+            sections={getAccordionSections(props)}
+            renderHeader={(section: AccordionSection): JSX.Element => section.title}
+            renderContent={(section: AccordionSection): JSX.Element => section.content}
+            underlayColor={colors.lightGrey}
+            duration={400}
+        />
     </Content>
+);
+
+interface AccordionSection {
+    readonly title: JSX.Element;
+    readonly content: JSX.Element;
+}
+
+// tslint:disable-next-line:readonly-array
+const getAccordionSections = (props: Props): Array<AccordionSection> => (
+    R.map((question: selector.Question) => (
+        {
+            title: renderQuestionTitle(question),
+            content: renderQuestionContent(question, props),
+        }
+    ), props.questionnaire)
+);
+
+const renderQuestionTitle = (question: selector.Question): JSX.Element => (
+    <Text style={[
+        { padding: 10 },
+    ]}>{question.number}. {question.text}</Text>
+);
+
+const renderQuestionContent = (question: selector.Question, props: Props): JSX.Element => (
+    <Question
+        {...props}
+        key={question.id}
+        question={question}
+        selectAnswer={props.selectAnswer}
+        isFinalQuestion={question.number === props.questionnaire.length} />
 );
