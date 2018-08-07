8d886feb493610e2c4b65f6f83c1810cecd64dd9
Ben St John <ben@affinitybridge.com>
2018-08-07 15:22:54 -0700

    Issue #156 Add image to related articles

--- src/components/articles/article_list_item.tsx
+++ src/components/articles/article_list_item.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ListItem, Text } from 'native-base';
-import { Col, Row, Grid } from 'react-native-easy-grid';
+import { Image } from 'react-native';
+import { ListItem, Text, View } from 'native-base';
 import { applicationStyles } from '../../application/styles';
 import { ArticleListItem } from '../../selectors/articles';
 import { RouterProps, Routes, goToRouteWithParameter } from '../../application/routing';
@@ -10,31 +10,32 @@ export interface ArticleListItemProps {
 }
 type Props = ArticleListItemProps & RouterProps;
 
+// tslint:disable-next-line:no-var-requires
+const starImage = require('../../../assets/images/icon.png');
+
 export const ArticleListItemComponent: React.StatelessComponent<Props> =
     (props: Props): JSX.Element => {
     const goToArticleDetail = goToRouteWithParameter(Routes.ArticleDetail, props.article.id, props.history);
     return (
         <ListItem noIndent noBorder button onPress={goToArticleDetail}>
-            <Grid>
-                <Row>
-                    <Col size={20}>
-                        <Text>[image]</Text>
-                    </Col>
-                    <Col size={80}>
-                        <Row>
-                            <Text style={[
-                                applicationStyles.bold,
-                                { textAlign: 'left' },
-                            ]}>
-                                {props.article.title}
-                            </Text>
-                        </Row>
-                        <Row>
-                            <Text style={[{ textAlign: 'left' }]} numberOfLines={1} note>{props.article.description}</Text>
-                        </Row>
-                    </Col>
-                </Row>
-            </Grid>
+            <View style={[ { flex: 1 }, { flexDirection: 'row' }, { alignItems: 'center' } ]}>
+                <View style={[ { flex: 1 } ]}>
+                    <Image source={starImage} style={[
+                        { width: 50 },
+                        { height: 50 },
+                    ]} />
+                </View>
+                <View style={[ { flex: 4 }, { marginLeft: 10 } ]}>
+                    <View style={[ { flexDirection: 'column' } ]}>
+                        <Text style={[applicationStyles.bold, { textAlign: 'left' } ]}>
+                            {props.article.title}
+                        </Text>
+                        <Text style={[{ textAlign: 'left' }]} numberOfLines={1} note>
+                            {props.article.description}
+                        </Text>
+                    </View>
+                </View>
+            </View>
         </ListItem>
     );
 };
