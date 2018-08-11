98dd60d650c5a5f8a63627791b552ebe05028c35
rasmus <rasmus@peacegeeks.org>
2018-08-11 16:17:06 -0700

    Issue #123 Refactor selectExploreSectionList

--- src/components/home_page/home_page_connected_component.tsx
+++ src/components/home_page/home_page_connected_component.tsx
@@ -2,7 +2,7 @@ import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { Store } from '../../stores';
 import { HomePageProps, HomePageComponent } from './home_page';
-import { selectLearnSections } from '../../selectors/explore';
+import { selectExploreSectionList } from '../../selectors/explore/select_explore_section_list';
 import { selectRecommendedTasks, selectSavedTasksIdList } from '../../selectors/tasks';
 import { Id as TaskId } from '../../stores/tasks';
 import { AddToSavedListAction, addToSavedList } from '../../stores/tasks';
@@ -11,7 +11,7 @@ import { TaskListItemActions } from '../tasks/task_list_item';
 
 const mapStateToProps = (store: Store): HomePageProps => ({
     tasks: selectRecommendedTasks(store),
-    sections: selectLearnSections(store),
+    sections: selectExploreSectionList(store),
     savedTasksIdList: selectSavedTasksIdList(store),
 });
 
