555688999ad82e8e7a221c0e33960e0a59a465fe
rasmus <rasmus@peacegeeks.org>
2018-08-12 06:34:57 -0700

    Issue #123 Refactor toSelectorAnswerListForQuestion

--- src/selectors/questionnaire/to_selector_question_list.ts
+++ src/selectors/questionnaire/to_selector_question_list.ts
@@ -2,9 +2,8 @@ import * as model from '../../stores/questionnaire';
 import { getLocalizedText } from '../locale/get_localized_text';
 import { Locale } from '../../locale/types';
 import { toValidOrThrow } from '../../stores/questionnaire/stores';
-import { QuestionList, Answer } from './types';
-import { toSelectorAnswerList } from './to_selector_answer_list';
-import { filterAnswerIdsToGivenQuestion } from './filter_answer_ids_to_given_question';
+import { QuestionList } from './types';
+import { toSelectorAnswerListForQuestion } from './to_selector_answer_listFor_question';
 
 export const toSelectorQuestionList = (locale: Locale, modelStore: model.Store): QuestionList => {
     const { questions, answers }: model.ValidStore = toValidOrThrow(modelStore);
@@ -16,13 +15,7 @@ export const toSelectorQuestionList = (locale: Locale, modelStore: model.Store):
             number: index + 1,
             text: getLocalizedText(locale, question.text),
             explanation: question.explanation ? getLocalizedText(locale, question.explanation) : undefined,
-            answers: selectAnswersForQuestion(locale, question, answers),
+            answers: toSelectorAnswerListForQuestion(locale, question, answers),
         };
     });
-};
-
-const selectAnswersForQuestion = (locale: Locale, question: model.Question, answers: model.AnswersMap): ReadonlyArray<Answer> => {
-    const keys = filterAnswerIdsToGivenQuestion(question.id, answers);
-
-    return toSelectorAnswerList(locale, keys, answers, question.acceptMultipleAnswers);
 };
