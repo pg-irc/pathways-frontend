cd8fa5bfe415bae4d67d7292f06cee0dafab5788
rasmus <rasmus@peacegeeks.org>
2018-08-12 07:16:39 -0700

    Issue #123 Refactor getTaxonomyTermsForChosenAnswers

--- src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
+++ src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
@@ -1,14 +1,8 @@
 import { Store } from '../../stores';
-import * as model from '../../stores/questionnaire';
 import { TaxonomyTermReference } from '../../stores/taxonomies';
 import { pullQuestionnaire } from '../questionnaire/pull_questionnaire';
-import { filterChosenAnswers } from '../questionnaire/filter_chosen_answers';
-import { getTaxonomyTermsFromAnswers } from './get_taxonomy_terms_from_answers';
+import { getTaxonomyTermsForChosenAnswers } from './get_taxonomy_terms_for_chosen_answers';
 
 export const selectTaxonomyTermsForChosenAnswers = (store: Store): ReadonlyArray<TaxonomyTermReference> => (
-    filterTaxonomyTermsForChosenAnswers(pullQuestionnaire(store).answers)
-);
-
-export const filterTaxonomyTermsForChosenAnswers = (answers: model.AnswersMap): ReadonlyArray<TaxonomyTermReference> => (
-    getTaxonomyTermsFromAnswers(filterChosenAnswers(answers))
+    getTaxonomyTermsForChosenAnswers(pullQuestionnaire(store).answers)
 );
