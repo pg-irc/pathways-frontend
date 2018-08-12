0015bb2066f7c14537672e5e29053dabcb473791
rasmus <rasmus@peacegeeks.org>
2018-08-12 06:10:07 -0700

    pickQuestionnaire

--- src/selectors/questionnaire/select_ids_of_chosen_questions.ts
+++ src/selectors/questionnaire/select_ids_of_chosen_questions.ts
@@ -1,12 +1,8 @@
 import { Store } from '../../stores';
 import { Id } from '../../stores/questionnaire';
-import { toValidOrThrow, ValidStore as QuestionnaireStore } from '../../stores/questionnaire/stores';
 import { getIdsOfChosenAnswers } from './get_ids_of_chosen_answers';
+import { pickQuestionnaire } from './pick_questionnaire';
 
 export const selectIdsOfChosenAnswers = (appStore: Store): ReadonlyArray<Id> => (
     getIdsOfChosenAnswers(pickQuestionnaire(appStore).answers)
-);
-
-const pickQuestionnaire = (appStore: Store): QuestionnaireStore => (
-    toValidOrThrow(appStore.questionnaireInStore)
 );
