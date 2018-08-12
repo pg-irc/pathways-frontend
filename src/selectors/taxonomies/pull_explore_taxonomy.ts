d6a20ac119fc2c1fb9b5f35ea8db7da79e2c396c
rasmus <rasmus@peacegeeks.org>
2018-08-11 17:41:43 -0700

    Issue #123 Refactor filterExploreTaxonomyTerms

--- src/selectors/taxonomies/pull_explore_taxonomy.ts
+++ src/selectors/taxonomies/pull_explore_taxonomy.ts
@@ -1,11 +1,8 @@
 import { Store } from '../../stores';
 import { Taxonomy, ExploreTaxonomyId } from '../../stores/taxonomies';
-import * as R from 'ramda';
 
 export { TaxonomyTermReference } from '../../stores/taxonomies';
 
 export const pullExploreTaxonomy = (store: Store): Taxonomy => (
     store.taxonomiesInStore.taxonomyMap[ExploreTaxonomyId]
 );
-
-export const getExploreTaxonomyTerms = R.filter(R.propEq('taxonomyId', ExploreTaxonomyId));
