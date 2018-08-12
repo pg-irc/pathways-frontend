15a6f594a3da0a8ed7b2b843e66ffa3cb5eb2625
rasmus <rasmus@peacegeeks.org>
2018-08-11 17:04:50 -0700

    pullAvailableLocales

--- src/selectors/locale/index.ts
+++ src/selectors/locale/index.ts
@@ -1,12 +1,7 @@
-import { Store } from '../../stores';
 import { LocalizedText } from '../../locale';
-import { Locale, LocaleInfo } from '../../locale/types';
+import { Locale } from '../../locale/types';
 
 export { Locale, LocaleInfo } from '../../locale/types';
-
-export function selectAvailableLocales(appStore: Store): ReadonlyArray<LocaleInfo> {
-    return appStore.localeInStore.availableLocales;
-}
 
 export function selectLocalizedText(locale: Locale, localizedText: LocalizedText): string {
     if (localizedText[locale.code]) {
