15fcd58974a86ed6dafcea61f4a83460e8cc831f
rasmus <rasmus@peacegeeks.org>
2018-08-12 06:16:03 -0700

    filterChosenAnswers

--- src/selectors/questionnaire/select_question_list.ts
+++ src/selectors/questionnaire/select_question_list.ts
@@ -2,8 +2,10 @@ import { Store } from '../../stores';
 import { selectLocale } from '../locale/select_locale';
 import { QuestionList } from './types';
 import { buildQuestionList } from './build_question_list';
+import { pickQuestionnaire } from './pick_questionnaire';
 
 export const selectQuestionList = (appStore: Store): QuestionList => {
     const locale = selectLocale(appStore);
-    return buildQuestionList(locale, appStore.questionnaireInStore);
+    const questionnaire = pickQuestionnaire(appStore);
+    return buildQuestionList(locale, questionnaire);
 };
