09c834254c226f9e4984922fb1236771069dee86
rasmus <rasmus@peacegeeks.org>
2018-08-12 07:12:44 -0700

    Issue #123 Refactor getTaxonomyTermsFromAnswers

--- src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
+++ src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
@@ -1,16 +1,14 @@
-import * as R from 'ramda';
 import { Store } from '../../stores';
 import * as model from '../../stores/questionnaire';
 import { TaxonomyTermReference } from '../../stores/taxonomies';
 import { pullQuestionnaire } from '../questionnaire/pull_questionnaire';
+import { filterChosenAnswers } from '../questionnaire/filter_chosen_answers';
+import { getTaxonomyTermsFromAnswers } from './get_taxonomy_terms_from_answers';
 
 export const selectTaxonomyTermsForChosenAnswers = (store: Store): ReadonlyArray<TaxonomyTermReference> => (
     filterTaxonomyTermsForChosenAnswers(pullQuestionnaire(store).answers)
 );
 
-export const filterTaxonomyTermsForChosenAnswers = (answers: model.AnswersMap): ReadonlyArray<TaxonomyTermReference> => {
-    type ReferenceArray = ReadonlyArray<TaxonomyTermReference>;
-    const flatten = R.reduce((acc: ReferenceArray, val: ReferenceArray): ReferenceArray => [...acc, ...val], []);
-
-    return flatten(R.pluck('taxonomyTerms', R.filter(R.propEq('isChosen', true), R.values(answers))));
-};
+export const filterTaxonomyTermsForChosenAnswers = (answers: model.AnswersMap): ReadonlyArray<TaxonomyTermReference> => (
+    getTaxonomyTermsFromAnswers(filterChosenAnswers(answers))
+);
