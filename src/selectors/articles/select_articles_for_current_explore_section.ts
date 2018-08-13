1559dae382da5f132c8c35d3d06d21d1a2bfc505
rasmus <rasmus@peacegeeks.org>
2018-08-13 11:30:48 -0700

    Issue #123 Clean up

--- src/selectors/articles/select_articles_for_current_explore_section.ts
+++ src/selectors/articles/select_articles_for_current_explore_section.ts
@@ -7,7 +7,7 @@ import { ArticleListItem } from './article_list_item';
 import { pullExploreSectionById } from '../explore/pull_explore_section_by_id';
 import { findItemByLearnTaxonomyTerm } from '../taxonomies/find_item_by_explore_taxonomy_term';
 
-export const selectArticlesForExploreDetail = (store: Store, routerProps: RouterProps): ReadonlyArray<ArticleListItem> => {
+export const selectArticlesForCurrentExploreSection = (store: Store, routerProps: RouterProps): ReadonlyArray<ArticleListItem> => {
     const exploreId = routerProps.match.params.learnId;
     const exploreSection = pullExploreSectionById(store, exploreId);
     const articles = store.articlesInStore.articles;
