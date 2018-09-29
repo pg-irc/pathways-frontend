0291b5008968fa3b1d8a6c713bcddaadbb4c36f0
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-09-28 17:21:43 -0700

    Issue #239 Set up styles for navigation bar

--- src/components/main/footer.tsx
+++ src/components/main/footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import * as ReactNative from 'react-native';
 import { Footer, FooterTab, Button, Icon, Text } from 'native-base';
 import { History, Location } from 'history';
 import { Trans } from '@lingui/react';
 import { Routes, routePathWithoutParameter, goToRouteWithoutParameter } from '../../application/routing';
 import { emptyComponent } from '../empty_component/empty_component';
+import { colors } from '../../application/styles';
 
 export interface FooterProps {
     readonly history: History;
@@ -12,28 +14,49 @@ export interface FooterProps {
 
 export const FooterComponent: React.StatelessComponent<FooterProps> = (props: FooterProps): JSX.Element => {
     const path = props.location.pathname;
-    if (path === '/' || path === '/help') {
+
+    const isWelcome = path === routePathWithoutParameter(Routes.Welcome);
+    const isHelp = path === routePathWithoutParameter(Routes.Help);
+    const isHome = path === routePathWithoutParameter(Routes.Home);
+    const isOnMyPlan = path === routePathWithoutParameter(Routes.MyPlan);
+    const isOnLearn = path === routePathWithoutParameter(Routes.Learn);
+
+    if (isWelcome || isHelp) {
         return emptyComponent();
     }
+
     return (
         <Footer>
-            <FooterTab>
-                <Button vertical active={path === routePathWithoutParameter(Routes.Home)}
-                        onPress={goToRouteWithoutParameter(Routes.Home, props.history)}>
-                    <Icon name='home' />
-                    <Text><Trans>Home</Trans></Text>
+            <FooterTab style={footerStyle()}>
+                <Button vertical active={isHome} style={buttonStyle(isHome)}
+                    onPress={goToRouteWithoutParameter(Routes.Home, props.history)}>
+                    <Icon name='home' style={textStyle(isHome)} />
+                    <Text style={textStyle(isHome)}><Trans>Home</Trans></Text>
                 </Button>
-                <Button vertical active={path === routePathWithoutParameter(Routes.MyPlan)}
-                        onPress={goToRouteWithoutParameter(Routes.MyPlan, props.history)}>
-                    <Icon name='camera' />
-                    <Text><Trans>My plan</Trans></Text>
+                <Button vertical active={isOnMyPlan} style={buttonStyle(isOnMyPlan)}
+                    onPress={goToRouteWithoutParameter(Routes.MyPlan, props.history)}>
+                    <Icon name='camera' style={textStyle(isOnMyPlan)} />
+                    <Text style={textStyle(isOnMyPlan)}><Trans>My plan</Trans></Text>
                 </Button>
-                <Button vertical active={path === routePathWithoutParameter(Routes.Learn)}
-                        onPress={goToRouteWithoutParameter(Routes.Learn, props.history)}>
-                    <Icon active name='apps' />
-                    <Text><Trans>Learn</Trans></Text>
+                <Button vertical active={isOnLearn} style={buttonStyle(isOnLearn)}
+                    onPress={goToRouteWithoutParameter(Routes.Learn, props.history)}>
+                    <Icon name='apps' style={textStyle(isOnLearn)} />
+                    <Text style={textStyle(isOnLearn)}><Trans>Learn</Trans></Text>
                 </Button>
             </FooterTab>
         </Footer>
     );
 };
+
+const footerStyle = (): ReactNative.ViewStyle => (
+    { backgroundColor: colors.blue }
+);
+
+const buttonStyle = (isActive: boolean): ReactNative.ViewStyle => (
+    isActive ? { backgroundColor: colors.blue } : { backgroundColor: colors.lightGrey }
+);
+
+// tslint:disable-next-line:no-any
+const textStyle = (isActive: boolean): ReactNative.StyleProp<ReactNative.TextStyle> => (
+    isActive ? { color: colors.white } : { color: colors.darkGrey }
+);
