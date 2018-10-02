c6ae3b8925d1fee12cf59ae6f1f93a50a77d0620
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-02 11:40:34 -0700

    Issue #244 Register router on the listener

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { Store } from '../../stores';
 import * as main from './main';
@@ -6,6 +7,7 @@ import { isApplicationLoading } from '../../selectors/is_application_loading';
 import { selectLocale } from '../../selectors/locale/select_locale';
 import { withRouter } from 'react-router-native';
 import { RouterProps } from '../../application/routing';
+import { Location, Action } from 'history';
 
 const mapStateToProps = (store: Store, ownProps: RouterProps): LoaderProps & main.Props & RouterProps => ({
     currentLocale: selectLocale(store),
@@ -16,6 +18,10 @@ const mapStateToProps = (store: Store, ownProps: RouterProps): LoaderProps & mai
     staticContext: ownProps.staticContext,
 });
 
+const mapDispatchToProps = (dispatch: Dispatch<Store>): main.Actions => ({
+    routeChanged: (location: Location, _: Action): main.RouteChangedAction => dispatch(main.routeChanged(location)),
+});
+
 const MainComponent = withLoader<main.Props>(main.Component);
-const connector = connect(mapStateToProps, {});
+const connector = connect(mapStateToProps, mapDispatchToProps);
 export const ConnectedComponent = withRouter(connector(MainComponent));
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -7,31 +7,48 @@ import { NotificationsConnectedComponent } from '../notifications/notifications_
 import { FooterComponent, FooterProps } from './footer';
 import { ConnectedLanguageSwitcher } from '../language_switcher/connected_language_switcher';
 import { RouterProps } from '../../application/routing';
+import { Location, Action } from 'history';
+import * as constants from '../../application/constants';
+import * as helpers from '../../stores/helpers/make_action';
 
-interface DrawerState {
+interface State {
     readonly isDrawerOpen: boolean;
+}
+
+export type RouteChangedAction = Readonly<ReturnType<typeof routeChanged>>;
+
+// tslint:disable-next-line:typedef
+export const routeChanged = (location: Location) => (
+    helpers.makeAction(constants.CLEAR_ERROR_STATE, { location })
+);
+
+export interface Actions {
+    readonly routeChanged: (location: Location, action: Action) => RouteChangedAction;
 }
 
 export type Props = HeaderProps & FooterProps & RouterProps;
 
-export class Component extends React.Component<Props, DrawerState> {
+export type MainComponentProps = Props & Actions;
 
-    readonly state: DrawerState = {
+export class Component extends React.Component<MainComponentProps, State> {
+
+    readonly state: State = {
         isDrawerOpen: false,
     };
 
-    constructor(props: Props) {
+    constructor(props: MainComponentProps) {
         super(props);
         this.openDrawer = this.openDrawer.bind(this);
         this.closeDrawer = this.closeDrawer.bind(this);
+        props.history.listen(props.routeChanged);
     }
 
     render(): JSX.Element {
         return (
             <Drawer open={this.state.isDrawerOpen}
-                    side='right'
-                    content={<ConnectedLanguageSwitcher />}
-                    onClose={this.closeDrawer} >
+                side='right'
+                content={<ConnectedLanguageSwitcher />}
+                onClose={this.closeDrawer} >
                 <Container>
                     <HeaderComponent onLanguageSelect={this.openDrawer} {...this.props} />
                     <MainPageSwitcherComponent />
