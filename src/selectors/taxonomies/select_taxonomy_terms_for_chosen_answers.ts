b502558803226bb19805d0db3cfcf7e818ffaa4e
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-09 14:28:57 -0700

    Issue #244 Refactor

--- src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
+++ src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
@@ -1,9 +1,8 @@
 import { Store } from '../../stores';
 import { TaxonomyTermReference } from '../../stores/taxonomies';
-import { pickQuestionnaire } from '../questionnaire/pick_questionnaire';
 import { getTaxonomyTermsForChosenAnswers } from './get_taxonomy_terms_for_chosen_answers';
+import { pickAnswers } from '../questionnaire/pick_answers';
 
-// TODO remove this function?
 export const selectTaxonomyTermsForChosenAnswers = (store: Store): ReadonlyArray<TaxonomyTermReference> => (
-    getTaxonomyTermsForChosenAnswers(pickQuestionnaire(store).answers)
+    getTaxonomyTermsForChosenAnswers(pickAnswers(store))
 );
