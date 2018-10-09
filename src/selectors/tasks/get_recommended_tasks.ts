e2daa282bcd92bc501ea1d0508c556b6235ccb38
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-09 14:40:23 -0700

    Issue #244 Refactor, create new function for getting newly recommended tasks

--- src/selectors/tasks/get_recommended_tasks.ts
+++ src/selectors/tasks/get_recommended_tasks.ts
@@ -3,12 +3,12 @@ import { Task } from '../../stores/tasks';
 import { rejectSavedAndCompletedTasks } from './reject_saved_and_completed_tasks';
 import { pickSavedTaskIds } from './pick_saved_task_ids';
 import { getRecommendedTasksFromSelectedAnswers } from './get_recommended_tasks_from_selected_answers';
+import { pickAnswers } from '../questionnaire/pick_answers';
 import { pickTasks } from './pick_tasks';
-import { pickQuestionnaire } from '../questionnaire/pick_questionnaire';
 
-export const getNewlyRecommendedTasks = (appStore: Store): ReadonlyArray<Task> => (
+export const getRecommendedTasks = (appStore: Store): ReadonlyArray<Task> => (
     rejectSavedAndCompletedTasks(
         pickSavedTaskIds(appStore),
-        getRecommendedTasksFromSelectedAnswers(pickQuestionnaire(appStore).oldAnswers, pickTasks(appStore)),
+        getRecommendedTasksFromSelectedAnswers(pickAnswers(appStore), pickTasks(appStore)),
     )
 );
--- src/selectors/tasks/select_recommended_tasks.ts
+++ src/selectors/tasks/select_recommended_tasks.ts
@@ -1,19 +1,9 @@
 import * as R from 'ramda';
 import { Store } from '../../stores';
 import { Task } from './task';
-import { pickSavedTaskIds } from './pick_saved_task_ids';
-import { pickTasks } from './pick_tasks';
-import { pickAnswers } from '../questionnaire/pick_answers';
-import { rejectSavedAndCompletedTasks } from './reject_saved_and_completed_tasks';
-import { getRecommendedTasksFromSelectedAnswers } from './get_recommended_tasks_from_selected_answers';
 import { buildSelectorTask } from './build_selector_task';
+import { getRecommendedTasks } from './get_recommended_tasks';
 
 export const selectRecommendedTasks = (appStore: Store): ReadonlyArray<Task> => (
-    R.map(
-        buildSelectorTask(appStore),
-        rejectSavedAndCompletedTasks(
-            pickSavedTaskIds(appStore),
-            getRecommendedTasksFromSelectedAnswers(pickAnswers(appStore), pickTasks(appStore)),
-        ),
-    )
+    R.map(buildSelectorTask(appStore), getRecommendedTasks(appStore))
 );
