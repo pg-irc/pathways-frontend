34b378b6b49b20395737ef4047406a285a31a9df
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-09 13:27:29 -0700

    Issue #244 Refactor

--- src/selectors/tasks/select_recommended_tasks.ts
+++ src/selectors/tasks/select_recommended_tasks.ts
@@ -1,7 +1,6 @@
 import * as R from 'ramda';
 import { Store } from '../../stores';
 import * as store from '../../stores/tasks';
-import { selectTaxonomyTermsForChosenAnswers } from '../taxonomies/select_taxonomy_terms_for_chosen_answers';
 import { selectLocale } from '../locale/select_locale';
 import { Task } from './task';
 import { toSelectorTaskWithoutRelatedEntities } from './to_selector_task_without_related_entities';
@@ -11,16 +10,20 @@ import { filterTasksByTaxonomyTerms } from './filter_tasks_by_taxonomy_terms';
 import { rejectCompletedTasks } from './reject_completed_tasks';
 import { pickSavedTaskIds } from './pick_saved_task_ids';
 import { pickTasks } from './pick_tasks';
+import { getTaxonomyTermsForChosenAnswers } from '../taxonomies/get_taxonomy_terms_for_chosen_answers';
+import { AnswersMap } from '../../stores/questionnaire';
+import { pullQuestionnaire } from '../questionnaire/pull_questionnaire';
 
 export const selectRecommendedTasks = (appStore: Store): ReadonlyArray<Task> => {
-    const taxonomyTerms = selectTaxonomyTermsForChosenAnswers(appStore);
-    const filterTasks = filterTasksByTaxonomyTerms(taxonomyTerms);
+    const answers = pullQuestionnaire(appStore).answers;
+    const allTasks = pickTasks(appStore);
+
+    const taxonomyTerms = getTaxonomyTermsForChosenAnswers(answers);
+    const matchingTasks = filterTasksByTaxonomyTerms(taxonomyTerms, allTasks);
 
     const savedTaskIds = pickSavedTaskIds(appStore);
     const rejectSavedTasks = rejectTasksWithIdsInList(savedTaskIds);
 
-    const allTasks = pickTasks(appStore);
-    const matchingTasks = filterTasks(allTasks);
     const nonSavedTasks = rejectSavedTasks(matchingTasks);
     const nonCompletedTasks = rejectCompletedTasks(nonSavedTasks);
 
@@ -32,4 +35,10 @@ export const selectRecommendedTasks = (appStore: Store): ReadonlyArray<Task> => 
     };
 
     return R.map(buildSelectorTask, nonCompletedTasks);
+};
+
+export const newRecommendedTasks = (answers: AnswersMap, allTasks: store.TaskMap): ReadonlyArray<store.Task> => {
+    const taxonomyTerms = getTaxonomyTermsForChosenAnswers(answers);
+    const filterTasks = filterTasksByTaxonomyTerms(taxonomyTerms);
+    return filterTasks(allTasks);
 };
--- src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
+++ src/selectors/taxonomies/select_taxonomy_terms_for_chosen_answers.ts
@@ -3,6 +3,7 @@ import { TaxonomyTermReference } from '../../stores/taxonomies';
 import { pullQuestionnaire } from '../questionnaire/pull_questionnaire';
 import { getTaxonomyTermsForChosenAnswers } from './get_taxonomy_terms_for_chosen_answers';
 
+// TODO remove this function?
 export const selectTaxonomyTermsForChosenAnswers = (store: Store): ReadonlyArray<TaxonomyTermReference> => (
     getTaxonomyTermsForChosenAnswers(pullQuestionnaire(store).answers)
 );
