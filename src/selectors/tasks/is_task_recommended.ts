77fac0eb2e01ff10cfb61136c66a6ab9866be9d3
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 15:28:03 -0700

    Issue #61 Simplify isTaskRecommended by appending system taxonomy term to argument terms

--- src/selectors/tasks/is_task_recommended.ts
+++ src/selectors/tasks/is_task_recommended.ts
@@ -1,18 +1,13 @@
 import { Task } from '../../stores/tasks';
 import { TaxonomyTermReference } from '../taxonomies/pull_explore_taxonomy';
-import { Taxonomies as TaxonomyConstants } from '../../application/constants';
+import { Taxonomies } from '../../application/constants';
 import * as R from 'ramda';
 
-export const isTaskRecommended = R.curry((selectedAnswerTaxonomyTerms: ReadonlyArray<TaxonomyTermReference>, task: Task): boolean => {
-    const taskContainsTaxonomyTerms = R.curry((targetTerms: ReadonlyArray<TaxonomyTermReference>, aTask: Task): boolean => (
-        R.not(R.isEmpty(R.intersection(targetTerms, aTask.taxonomyTerms)))
-    ));
+export const isTaskRecommended = R.curry((taxonomyTerms: ReadonlyArray<TaxonomyTermReference>, task: Task): boolean => {
+    const taxonomyId = Taxonomies.RECOMMENDATION_TAXONOMY_ID;
+    const taxonomyTermId = Taxonomies.RECOMMEND_TO_ALL_TAXONOMY_TERM_ID;
 
-    const taxonomyId = TaxonomyConstants.RECOMMENDATION_TAXONOMY_ID;
-    const taxonomyTermId = TaxonomyConstants.RECOMMEND_TO_ALL_TAXONOMY_TERM_ID;
+    const taxonomyTermsWithDefault = R.append({ taxonomyId, taxonomyTermId }, taxonomyTerms);
 
-    const isRecommendedToAll = taskContainsTaxonomyTerms([{ taxonomyId, taxonomyTermId }]);
-    const isRecommendedBecauseOfSelecedAnswers = taskContainsTaxonomyTerms(selectedAnswerTaxonomyTerms);
-
-    return isRecommendedToAll(task) || isRecommendedBecauseOfSelecedAnswers(task);
+    return R.not(R.isEmpty(R.intersection(taxonomyTermsWithDefault, task.taxonomyTerms)));
 });
