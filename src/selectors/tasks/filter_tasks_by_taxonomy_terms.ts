f618125f0c82996d05421bf80f1670ec9a804e40
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 15:42:14 -0700

    Issue #61 Remove filterTasksByTaxonomyTerms() and call R.filter inline with predicate instead

--- src/selectors/tasks/filter_tasks_by_taxonomy_terms.ts
+++
@@ -1,9 +0,0 @@
-import { Task, TaskMap } from '../../stores/tasks';
-import { TaxonomyTermReference } from '../taxonomies/pull_explore_taxonomy';
-import { isTaskRecommended } from './is_task_recommended';
-import * as R from 'ramda';
-
-export const filterTasksByTaxonomyTerms =
-    R.curry((taxonomyTerms: ReadonlyArray<TaxonomyTermReference>, tasks: TaskMap): ReadonlyArray<Task> => (
-        R.filter(isTaskRecommended(taxonomyTerms), R.values(tasks))
-    ));
