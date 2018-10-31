d11e444487d93e76a588b6b53b688a0336b8e7c9
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 15:42:14 -0700

    Issue #61 Remove filterTasksByTaxonomyTerms() and call R.filter inline with predicate instead

--- src/selectors/tasks/get_recommended_tasks.ts
+++ src/selectors/tasks/get_recommended_tasks.ts
@@ -2,14 +2,12 @@ import { AnswersMap } from '../../stores/questionnaire';
 import { TaskMap, Task } from '../../stores/tasks';
 import { Id } from '../../fixtures/types/explore';
 import { rejectSavedAndCompletedTasks } from './reject_saved_and_completed_tasks';
-import { filterTasksByTaxonomyTerms } from './filter_tasks_by_taxonomy_terms';
 import { getTaxonomyTermsForChosenAnswers } from '../taxonomies/get_taxonomy_terms_for_chosen_answers';
+import { isTaskRecommended } from './is_task_recommended';
+import * as R from 'ramda';
 
 export const getRecommendedTasks = (answers: AnswersMap, tasks: TaskMap, savedTaskIds: ReadonlyArray<Id>): ReadonlyArray<Task> => (
     rejectSavedAndCompletedTasks(savedTaskIds,
-        filterTasksByTaxonomyTerms(
-            getTaxonomyTermsForChosenAnswers(answers),
-            tasks,
-        ),
+        R.filter(isTaskRecommended(getTaxonomyTermsForChosenAnswers(answers)), R.values(tasks)),
     )
 );
