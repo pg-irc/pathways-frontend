4f3d927161ab3ec525d9efaa6ca298bf94989023
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 22:18:31 -0700

    Issue #61 Inline a simple function

--- src/selectors/tasks/get_recommended_tasks.ts
+++ src/selectors/tasks/get_recommended_tasks.ts
@@ -2,14 +2,13 @@ import { AnswersMap } from '../../stores/questionnaire';
 import { TaskMap, Task, Id } from '../../stores/tasks';
 import { getTaxonomyTermsForChosenAnswers } from '../taxonomies/get_taxonomy_terms_for_chosen_answers';
 import { isTaskRecommended } from './is_task_recommended';
-import { idIsInList } from './id_is_in_list';
 import { isCompleted } from './is_completed';
 import * as R from 'ramda';
 
 export const getRecommendedTasks = (answers: AnswersMap, tasks: TaskMap, savedTaskIds: ReadonlyArray<Id>): ReadonlyArray<Task> => {
     const chosenTaxonomyTerms = getTaxonomyTermsForChosenAnswers(answers);
     const isRecommended = isTaskRecommended(chosenTaxonomyTerms);
-    const isSaved = idIsInList(savedTaskIds);
+    const isSaved = (task: Task): boolean => R.contains(task.id, savedTaskIds);
 
     const isInRecommendedList = (task: Task): boolean => (
         R.not(isCompleted(task)) && R.not(isSaved(task)) && isRecommended(task)
