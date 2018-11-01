e67885ecc7ba2ba3e6f70d4eccbdfecf74288328
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 22:18:31 -0700

    Issue #61 Inline a simple function

--- src/selectors/tasks/id_is_in_list.ts
+++
@@ -1,6 +0,0 @@
-import { TaskList, Task } from '../../stores/tasks';
-import * as R from 'ramda';
-
-export const idIsInList = R.curry((taskIds: TaskList, task: Task): boolean => (
-    R.contains(task.id, taskIds)
-));
