5ff4231014da776a350c0ce595b1a07e5286ec0e
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 21:41:27 -0700

    Issue #61 Rename a parameter

--- src/selectors/tasks/is_task_recommended.ts
+++ src/selectors/tasks/is_task_recommended.ts
@@ -3,11 +3,11 @@ import { TaxonomyTermReference } from '../taxonomies/pull_explore_taxonomy';
 import { Taxonomies } from '../../application/constants';
 import * as R from 'ramda';
 
-export const isTaskRecommended = R.curry((taxonomyTerms: ReadonlyArray<TaxonomyTermReference>, task: Task): boolean => {
+export const isTaskRecommended = R.curry((chosenTaxonomyTerms: ReadonlyArray<TaxonomyTermReference>, task: Task): boolean => {
     const taxonomyId = Taxonomies.RECOMMENDATION_TAXONOMY_ID;
     const taxonomyTermId = Taxonomies.RECOMMEND_TO_ALL_TAXONOMY_TERM_ID;
 
-    const taxonomyTermsWithDefault = R.append({ taxonomyId, taxonomyTermId }, taxonomyTerms);
+    const taxonomyTermsWithDefault = R.append({ taxonomyId, taxonomyTermId }, chosenTaxonomyTerms);
 
     return R.not(R.isEmpty(R.intersection(taxonomyTermsWithDefault, task.taxonomyTerms)));
 });
