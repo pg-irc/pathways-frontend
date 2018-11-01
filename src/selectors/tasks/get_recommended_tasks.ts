b7019c358b57b50e49a4ae1041c080f26582d069
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-10-31 21:41:58 -0700

    Issue #61 Refactor getRecommendedTasks

--- src/selectors/tasks/get_recommended_tasks.ts
+++ src/selectors/tasks/get_recommended_tasks.ts
@@ -1,13 +1,19 @@
 import { AnswersMap } from '../../stores/questionnaire';
-import { TaskMap, Task } from '../../stores/tasks';
-import { Id } from '../../fixtures/types/explore';
-import { rejectSavedAndCompletedTasks } from './reject_saved_and_completed_tasks';
+import { TaskMap, Task, Id } from '../../stores/tasks';
 import { getTaxonomyTermsForChosenAnswers } from '../taxonomies/get_taxonomy_terms_for_chosen_answers';
 import { isTaskRecommended } from './is_task_recommended';
+import { idIsInList } from './id_is_in_list';
+import { isCompleted } from './is_completed';
 import * as R from 'ramda';
 
-export const getRecommendedTasks = (answers: AnswersMap, tasks: TaskMap, savedTaskIds: ReadonlyArray<Id>): ReadonlyArray<Task> => (
-    rejectSavedAndCompletedTasks(savedTaskIds,
-        R.filter(isTaskRecommended(getTaxonomyTermsForChosenAnswers(answers)), R.values(tasks)),
-    )
-);
+export const getRecommendedTasks = (answers: AnswersMap, tasks: TaskMap, savedTaskIds: ReadonlyArray<Id>): ReadonlyArray<Task> => {
+    const chosenTaxonomyTerms = getTaxonomyTermsForChosenAnswers(answers);
+    const isRecommended = isTaskRecommended(chosenTaxonomyTerms);
+    const isSaved = idIsInList(savedTaskIds);
+
+    const isInRecommendedList = (task: Task): boolean => (
+        R.not(isCompleted(task)) && R.not(isSaved(task)) && isRecommended(task)
+    );
+
+    return R.filter(isInRecommendedList, R.values(tasks));
+};
--- src/selectors/tasks/reject_saved_and_completed_tasks.ts
+++
@@ -1,8 +0,0 @@
-import { Id, Task } from '../../stores/tasks';
-import { isCompleted } from './is_completed';
-import { idIsInList } from './id_is_in_list';
-import * as R from 'ramda';
-
-export const rejectSavedAndCompletedTasks = (savedTaskIds: ReadonlyArray<Id>, tasks: ReadonlyArray<Task>): ReadonlyArray<Task> => (
-    R.reject(isCompleted, R.reject(idIsInList(savedTaskIds), tasks))
-);
