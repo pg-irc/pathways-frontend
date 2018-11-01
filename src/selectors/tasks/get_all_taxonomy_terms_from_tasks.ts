9fd9ebbe4f18cd3e69f5c4a986e774ecbd8946a3
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-11-01 09:17:02 -0700

    Issue #61 Clean up getAllTaxonomyTermsFromTasks()

--- src/selectors/tasks/get_all_taxonomy_terms_from_tasks.ts
+++ src/selectors/tasks/get_all_taxonomy_terms_from_tasks.ts
@@ -1,8 +1,9 @@
-import { Task, TaskMap } from '../../stores/tasks';
+import { TaskMap } from '../../stores/tasks';
 import { TaxonomyTermReference } from '../../stores/taxonomies';
+import { getTaxonomyTerms } from './get_taxonomy_terms';
 import { flattenOneLevel } from '../questionnaire/flatten_one_level';
 import * as R from 'ramda';
 
 export const getAllTaxonomyTermsFromTasks = (tasks: TaskMap): ReadonlyArray<TaxonomyTermReference> => (
-    R.uniq(flattenOneLevel(R.map((task: Task) => task.taxonomyTerms, R.values(tasks))))
+    R.uniq(flattenOneLevel(R.map(getTaxonomyTerms, R.values(tasks))))
 );
