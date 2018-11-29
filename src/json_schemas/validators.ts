4d478d6f19b4bac3d71b96d13d053d480467bc5e
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-11-29 10:59:58 -0800

    Progress on proper validation of user data

--- src/json_schemas/validators.ts
+++ src/json_schemas/validators.ts
@@ -2,6 +2,7 @@
 // This is the documenated syntax for Ajv. Using an import here causes a runtime error.
 const Ajv = require('ajv');
 import { serviceAtLocationArray } from '../stores/services';
+import { userData as userDataSchema } from '../stores/user_data/schemas';
 
 interface ValidatorResponse {
     readonly isValid: boolean;
@@ -12,6 +13,14 @@ export const servicesAtLocationValidator = (data: any): ValidatorResponse => {
     const ajv = new Ajv();
     const response = {
         isValid: ajv.validate(serviceAtLocationArray, data) as boolean,
+    };
+    return response.isValid ? response : { ...response, errors: ajv.errorsText(ajv.errors) };
+};
+
+export const userDataValidator = (data: any): ValidatorResponse => {
+    const ajv = new Ajv();
+    const response = {
+        isValid: ajv.validate(userDataSchema, data) as boolean,
     };
     return response.isValid ? response : { ...response, errors: ajv.errorsText(ajv.errors) };
 };
