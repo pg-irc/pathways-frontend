ec36fd2e2f7f67a4c43643bfac62dc269e27a2ae
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-11-29 16:37:25 -0800

    Issue #315 Show service description and full address in service listing

--- src/components/services/service_list_item_component.tsx
+++ src/components/services/service_list_item_component.tsx
@@ -6,6 +6,8 @@ import { View } from 'native-base';
 import { Text } from 'react-native';
 import { TextWithPhoneLinks } from '../link/text_with_phone_links';
 import { mapWithIndex } from '../../application/map_with_index';
+import { ExpandableContentComponent } from '../expandable_content/expandable_content_component';
+import { Trans } from '@lingui/react';
 
 interface ServiceListItemProps {
     readonly service: Service;
@@ -23,17 +25,20 @@ export const ServiceListItemComponent: React.StatelessComponent<ServiceListItemP
                 <Text style={textStyles.headlineH3StyleBlackLeft}>
                     {props.service.name}
                 </Text>
+                <ExpandableContentComponent content={<Text style={textStyles.paragraphStyle}>{props.service.description}</Text>} />
                 {
                     mapWithIndex((address: Address, index: number) =>
                         <View key={index}>
+                            <Text style={textStyles.paragraphBoldBlackLeft}><Trans>Address:</Trans></Text>
                             <Text style={textStyles.paragraphStyle}>{address.address}</Text>
+                            <Text style={textStyles.paragraphStyle}>{address.city + ' ' + address.stateProvince + ' ' + address.postalCode}</Text>
                         </View>, physicalAddresses)
                 }
                 {
                     mapWithIndex((phoneNumber: PhoneNumber, index: number) =>
                         <View key={index}>
                             <Text>
-                                <Text style={textStyles.paragraphStyle}>
+                                <Text style={textStyles.paragraphBoldBlackLeft}>
                                     {capitalizeFirstLetter(phoneNumber.type)}:
                             </Text> <TextWithPhoneLinks text={phoneNumber.phoneNumber} />
                             </Text>
