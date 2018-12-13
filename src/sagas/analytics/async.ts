46bf02f7fadd0588c5f90c1e2f3343373938efde
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-12-13 10:08:35 -0800

    Issue #177 Set middleware up to respond to the correct set of actions

--- src/sagas/analytics/async.ts
+++ src/sagas/analytics/async.ts
@@ -5,6 +5,8 @@ import { call, CallEffect, PutEffect, put } from 'redux-saga/effects';
 import { GOOGLE_ANALYTICS_TRACKING_ID } from 'react-native-dotenv';
 import { AnalyticsAsync } from './actions';
 import { WatchedAction } from './watch';
+import * as constants from '../../application/constants';
+import { pathMatchesRoute, Routes } from '../../application/routing';
 
 type AnalyticsActions = IterableIterator<CallEffect | PutEffect<AnalyticsAsync.SuccessAction | AnalyticsAsync.FailureAction>>;
 
@@ -12,9 +14,42 @@ interface AnalyticsData {
     readonly type: string;
 }
 
+const buildAnalyticsData = (action: WatchedAction): AnalyticsData => {
+    switch (action.type) {
+        case constants.ROUTE_CHANGED:
+            if (pathMatchesRoute(action.payload.location.pathname, Routes.TaskDetail)) {
+                return {
+                    type: action.type,
+                };
+            }
+            if (pathMatchesRoute(action.payload.location.pathname, Routes.Services)) {
+                return {
+                    type: action.type,
+                };
+            }
+            break;
+        case constants.CHOOSE_ANSWER:
+            return {
+                type: action.type,
+            };
+        case constants.ADD_TO_SAVED_TASKS:
+            return {
+                type: action.type,
+            };
+        default:
+            break;
+    }
+    return {
+        type: action.type,
+    };
+};
+
 export function* sendAnalyticsData(action: WatchedAction): AnalyticsActions {
     try {
-        const data: AnalyticsData = { type: action.type };
+        const data = buildAnalyticsData(action);
+        if (!data) {
+            return;
+        }
         yield call(sendAnalyticsDataAsync, data);
         yield put(AnalyticsAsync.success());
     } catch (error) {
@@ -24,8 +59,7 @@ export function* sendAnalyticsData(action: WatchedAction): AnalyticsActions {
 }
 
 export async function sendAnalyticsDataAsync(_data: AnalyticsData): Promise<void> {
-    const debug = true;
     // tslint:disable-next-line:no-null-keyword
-    const analytics = new ExpoAnalytics(GOOGLE_ANALYTICS_TRACKING_ID, null, { debug });
+    const analytics = new ExpoAnalytics(GOOGLE_ANALYTICS_TRACKING_ID, null, { debug: true });
     return analytics.hit(new PageHit('Home'));
 }
--- src/sagas/analytics/watch.ts
+++ src/sagas/analytics/watch.ts
@@ -4,9 +4,11 @@ import { ForkEffect, takeLatest } from 'redux-saga/effects';
 import { sendAnalyticsData } from './async';
 
 import * as constants from '../../application/constants';
-import { SetActiveQuestionAction, ChooseAnswerAction } from '../../stores/questionnaire';
+import { ChooseAnswerAction } from '../../stores/questionnaire';
+import { RouteChangedAction } from '../../stores/router_actions';
+import { AddToSavedListAction } from '../../stores/tasks';
 
-export type WatchedAction = SetActiveQuestionAction | ChooseAnswerAction;
+export type WatchedAction = RouteChangedAction | ChooseAnswerAction | AddToSavedListAction;
 
 export function* watchAnalytics(): IterableIterator<ForkEffect> {
     yield takeLatest(
