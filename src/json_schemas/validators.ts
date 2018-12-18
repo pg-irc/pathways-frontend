390ebb3d82cd1c4222973a55702f6ac630f9e1d4
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-12-18 13:01:34 -0800

    Revert "Progress on proper validation of user data"
    This reverts commit 4d478d6f19b4bac3d71b96d13d053d480467bc5e.

--- src/json_schemas/validators.ts
+++ src/json_schemas/validators.ts
@@ -2,7 +2,6 @@
 // This is the documenated syntax for Ajv. Using an import here causes a runtime error.
 const Ajv = require('ajv');
 import { serviceAtLocationArray } from '../stores/services';
-import { userData as userDataSchema } from '../stores/user_data/schemas';
 
 interface ValidatorResponse {
     readonly isValid: boolean;
@@ -13,14 +12,6 @@ export const servicesAtLocationValidator = (data: any): ValidatorResponse => {
     const ajv = new Ajv();
     const response = {
         isValid: ajv.validate(serviceAtLocationArray, data) as boolean,
-    };
-    return response.isValid ? response : { ...response, errors: ajv.errorsText(ajv.errors) };
-};
-
-export const userDataValidator = (data: any): ValidatorResponse => {
-    const ajv = new Ajv();
-    const response = {
-        isValid: ajv.validate(userDataSchema, data) as boolean,
     };
     return response.isValid ? response : { ...response, errors: ajv.errorsText(ajv.errors) };
 };
