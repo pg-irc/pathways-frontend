f1e468d21eb435fb49bb80105dea0b53d85ce227
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-12-20 12:15:19 -0800

    Issue #346 Simplify a test

--- src/application/__tests__/version.test.ts
+++ src/application/__tests__/version.test.ts
@@ -1,7 +1,5 @@
 // tslint:disable:no-expression-statement no-string-literal no-let typedef
 import { readFileSync, existsSync } from 'fs';
-import { EOL } from 'os';
-import * as R from 'ramda';
 
 describe('version id in VERSION.txt', () => {
     let version = '';
@@ -23,10 +21,7 @@ describe('version id in VERSION.txt', () => {
         const filename = '.env';
         if (existsSync(filename)) {
             const content = readFileSync(filename).toString();
-            const lines = content.split(EOL);
-            const versionLine = R.head(R.filter(line => line.startsWith('VERSION'), lines));
-
-            expect(versionLine).toBe(`VERSION=${version}`);
+            expect(content).toContain(`VERSION=${version}`);
         }
     });
 
