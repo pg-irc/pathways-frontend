577cae21792e294afe4bdfd9927898ffe0fbbcce
Rasmus Storjohann <rasmus@peacegeeks.org>
2018-12-20 11:38:45 -0800

    Issue #346 Add the version to .env as well for easy compilation into the app

--- src/application/__tests__/version.test.ts
+++ src/application/__tests__/version.test.ts
@@ -1,5 +1,7 @@
-// tslint:disable:no-expression-statement no-string-literal no-let
+// tslint:disable:no-expression-statement no-string-literal no-let typedef
 import { readFileSync } from 'fs';
+import { EOL } from 'os';
+import * as R from 'ramda';
 
 describe('version id in VERSION.txt', () => {
     let version = '';
@@ -14,6 +16,16 @@ describe('version id in VERSION.txt', () => {
         const json = JSON.parse(content);
 
         expect(json['expo']['version']).toBe(version);
+    });
+
+    it('matches with version id in .env', () => {
+
+        const filename = '.env';
+        const content = readFileSync(filename).toString();
+        const lines = content.split(EOL);
+        const versionLine = R.head(R.filter(line => line.startsWith('VERSION'), lines));
+
+        expect(versionLine).toBe(`VERSION=${version}`);
     });
 
     it('matches with version id in package.json', () => {
