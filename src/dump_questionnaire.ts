6188f926d812011f35f4a70d06c825c94ed0b643
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-03 12:23:41 -0800

    Issue pg-irc/pathways-backend#287 Create client side script to dump questionnaire data for review purposes

---
+++ src/dump_questionnaire.ts
@@ -0,0 +1,70 @@
+// tslint:disable:no-expression-statement no-string-literal
+
+/*
+To build and run:
+
+In order to run this with node, change tsconfig.json so it has
+
+    "target": "es6"
+    "module": "commonjs"
+
+    Don't check that in!!!
+
+Then run
+
+    yarn build
+    node lib/dump_questionnaire.js
+*/
+
+import { buildQuestionnaireFixture } from './fixtures/buildFixtures';
+import { Answer, Question } from './stores/questionnaire';
+import { TaxonomyTermReference } from './stores/taxonomies';
+import * as R from 'ramda';
+
+export const dumpQuestionnaireSummary = (): void => {
+    const fixture = buildQuestionnaireFixture();
+
+    const questions = R.values(fixture.questions);
+    questions.forEach((question: Question) => {
+        const answerBelongsToQuestion = (answer: Answer): boolean => (
+            answer.questionId === question.id
+        );
+        const answers = R.filter(answerBelongsToQuestion, R.values(fixture.answers));
+
+        logQuestion(question);
+        answers.forEach((answer: Answer) => {
+            logAnswer(answer);
+        });
+    });
+};
+
+const logQuestion = (question: Question): void => {
+    const text = question.text['en'];
+
+    console.log(`\n\n${text}`);
+};
+
+const logAnswer = (answer: Answer): void => {
+    const text = answer.text['en'];
+    const joinedTerms = getJoinedTaxonomyTerms(answer);
+
+    console.log(`\t${text}\t${joinedTerms}`);
+};
+
+const getJoinedTaxonomyTerms = (answer: Answer): string => {
+    if (R.isEmpty(answer.taxonomyTerms)) {
+        return 'none';
+    }
+    const termStrings = R.map(toString, answer.taxonomyTerms);
+    return R.reduce(joinWithTab, R.head(termStrings), R.tail(termStrings));
+};
+
+const toString = (term: TaxonomyTermReference): string => (
+    `${term.taxonomyId}:${term.taxonomyTermId}`
+);
+
+const joinWithTab = (left: string, right: string): string => (
+    `${left}\t${right}`
+);
+
+dumpQuestionnaireSummary();
