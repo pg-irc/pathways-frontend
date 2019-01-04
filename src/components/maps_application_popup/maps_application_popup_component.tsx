4efad6e6ac72d924a822b72d96a1ac0eadc004b7
Ben St John <ben@affinitybridge.com>
2019-01-04 10:50:26 -0800

    Issue #327 Simplify map functionality

--- src/components/maps_application_popup/maps_application_popup_component.tsx
+++ src/components/maps_application_popup/maps_application_popup_component.tsx
@@ -1,7 +1,7 @@
 // tslint:disable no-class no-this no-expression-statement
 import React from 'react';
 import { View, Text, Icon, Button } from 'native-base';
-import { Popup } from 'react-native-map-link';
+import { showLocation } from 'react-native-map-link';
 import { Trans } from '@lingui/react';
 import { textStyles, colors, values, applicationStyles } from '../../application/styles';
 
@@ -11,59 +11,30 @@ interface MapsApplicationPopupProps {
     readonly locationTitle?: string;
 }
 
-interface State {
-    readonly isVisible: boolean;
-}
-
-export class MapsApplicationPopupComponent extends React.Component<MapsApplicationPopupProps, State> {
-    constructor(props: MapsApplicationPopupProps) {
-        super(props);
-        this.state = {
-            isVisible: false,
-        };
-        this.open = this.open.bind(this);
-        this.close = this.close.bind(this);
-    }
-
-    render(): JSX.Element {
-        return (
-            <View>
-                <Popup
-                    isVisible={this.state.isVisible}
-                    onCancelPressed={this.close}
-                    onAppPressed={this.close}
-                    onBackButtonPressed={this.close}
-                    modalProps={{
-                        animationIn: 'slideInUp',
-                        onBackdropPress: this.close,
-                    }}
-                    appsWhiteList={['apple-maps', 'google-maps']}
-                    options={{
-                        title: this.props.locationTitle,
-                        latitude: this.props.latitude,
-                        longitude: this.props.longitude,
+export const MapsApplicationPopupComponent: React.StatelessComponent<MapsApplicationPopupProps> =
+    (props: MapsApplicationPopupProps): JSX.Element => (
+        <View>
+            <Button
+                onPress={(): Promise<void> =>
+                    showLocation({
+                        title: props.locationTitle,
+                        latitude: props.latitude,
+                        longitude: props.longitude,
+                        appsWhiteList: ['apple-maps', 'google-maps'],
+                    }).catch((): void => alert('Supported applications include Apple or Google maps.'))
+                }
+                iconLeft
+                style={applicationStyles.orangeButton}
+            >
+                <Icon
+                    type={'FontAwesome'}
+                    name={'map-marker'}
+                    style={{
+                        color: colors.white,
+                        fontSize: values.smallIconSize,
                     }}
                 />
-                <Button onPress={this.open} iconLeft style={applicationStyles.orangeButton}>
-                    <Icon
-                        type={'FontAwesome'}
-                        name={'map-marker'}
-                        style={{
-                            color: colors.white,
-                            fontSize: values.smallIconSize,
-                        }}
-                    />
-                    <Text style={textStyles.button}><Trans>Open in map</Trans></Text>
-                </Button>
-            </View>
-        );
-    }
-
-    open(): void {
-        this.setState({ isVisible: true });
-    }
-
-    close(): void {
-        this.setState({ isVisible: false });
-    }
-}
+                <Text style={textStyles.button}><Trans>Open in map</Trans></Text>
+            </Button>
+        </View>
+    );
