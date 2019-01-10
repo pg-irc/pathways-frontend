3aca337cf69c30909c4bdeb259a6b7d040bf4cd7
Ben St John <ben@affinitybridge.com>
2019-01-09 17:22:39 -0800

    Issue #374 Reduce size of hamburger icon

--- src/components/header_menu/header_menu_button_component.tsx
+++ src/components/header_menu/header_menu_button_component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Icon } from 'native-base';
 import { Locale } from '../../locale';
+import { values } from '../../application/styles';
 
 export interface Props {
     readonly locale: Locale;
@@ -14,7 +15,7 @@ export interface Actions {
 export const HeaderMenuButtonComponent = (props: Props & Actions): JSX.Element => {
     return (
         <Button transparent onPress={props.onPress} icon>
-            <Icon name='bars' type='FontAwesome' style={{ color: props.textColor }}/>
+            <Icon name='bars' type='FontAwesome' style={{ color: props.textColor, fontSize: values.mediumIconSize }}/>
         </Button>
     );
 };
