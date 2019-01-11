59ca6b79fd8a7b6db56aff82f4c739322d25245b
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-11 12:22:04 -0800

    Issue pg-irc/pathways-backend#289 Add step for building the app with translated strings

--- bin/strings.sh
+++ bin/strings.sh
@@ -16,16 +16,13 @@ locales=(ar fr)
 extract_all() {
     yarn build
     yarn extract-strings
+
     for locale in "${locales[@]}"
     do
         echo "Converting to CVS for ${locale}"
         po2csv locale/$locale/messages.po > locale/$locale/messages.csv
     done
 }
-
-if [ "$1" == "--extract" ]; then
-    extract_all
-fi
 
 # translate
 
@@ -34,10 +31,15 @@ fi
 # Convert CSV back to po files: csv2po locale/fr/messages.csv locale/fr/messages.po
 # Build strings files from po files with "yarn build-strings"
 
+build() {
+    for locale in "${locales[@]}"
+    do
+        echo "Converting from CVS for ${locale}"
+        csv2po locale/$locale/messages.csv locale/$locale/messages.po
+    done
 
-
-
-
+    yarn build-strings
+}
 
 # Update existing translation
 ################################################
@@ -57,3 +59,9 @@ fi
 # Combine old and new translations with "msgcat locale/ar/messages.po locale/ar/new-messages.po > locale/ar/joined-messages.po"
 # Rename new files to replace the old po files
 # Clean up temp files
+
+if [ "$1" == "--extract" ]; then
+    extract_all
+elif [ "$1" == "--build" ]; then
+    build
+fi
