2bf81c49803302a2376b76db301714c6a35a9506
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-11 13:59:38 -0800

    Issue pg-irc/pathways-backend#289 Add step for building app with changed strings translated

--- bin/strings.sh
+++ bin/strings.sh
@@ -1,76 +1,84 @@
 #!/bin/bash
 
-# Full translate sweep
-################################################
-
-#strings.sh --extract
-
-# Generate PO files from source code: yarn extract-strings
-# Convert po files to csv:  po2csv locale/fr/messages.po > locale/fr/messages.csv
-
-# locales[0] = "ar"
-# locales[1] = "fr"
-
 locales=(ar fr)
 
+
 extract_all() {
+    echo "Building the client ..."
     yarn build
+
+    echo "Generating PO files..."
     yarn extract-strings
 
     for locale in "${locales[@]}"
     do
-        echo "Converting to CVS for ${locale}"
+        echo "Converting PO files to CVS for ${locale}..."
         po2csv locale/$locale/messages.po > locale/$locale/messages.csv
     done
+
+    echo "Please send files locale/*/messages.csv or locale/*/messages.po for translation"
 }
 
-# translate
-
-#strings.sh --build
-
-# Convert CSV back to po files: csv2po locale/fr/messages.csv locale/fr/messages.po
-# Build strings files from po files with "yarn build-strings"
 
 build_all() {
     for locale in "${locales[@]}"
     do
-        echo "Converting from CVS for ${locale}"
+        echo "Converting CVS to PO files for ${locale}..."
         csv2po locale/$locale/messages.csv locale/$locale/messages.po
     done
 
+    echo "Converting PO files to Javascript..."
     yarn build-strings
+
+    echo "Building the app..."
+    yarn cbt
 }
 
-# Update existing translation
-################################################
-
-#strings.sh --extract-changed
-
-# Extract strings again, using "yarn extract-strings". 
-# > Now would be a good time for a no-op gettext transform to fix line wrap issues
-# Extract changed strings with "msggrep -v -T -i -e "." locale/fr/messages.po > locale/fr/new-messages.po"
-# Convert new strings to CSV with "po2csv locale/fr/messages.po > locale/fr/messages.csv"
 
 extract_changed() {
+    echo "Building the client ..."
     yarn build
+
+    echo "Generating PO files..."
     yarn extract-strings
 
     # TODO Fix string wrapping
 
     for locale in "${locales[@]}"
     do
-        echo "Filtering new strings for ${locale}"
+        echo "Filtering PO files for strings needing translation ${locale}..."
         # Use msggrep to get the strings needing translation
-        # -v invert the result
-        # -T match msgstr entries, i.e. the translated strings
-        # -e match using regular expression
-        # "." match strings with at least one character (inverted with -v to mean match empty strings)
+        #   -v invert the result
+        #   -T match msgstr entries, i.e. the translated strings
+        #   -e match using regular expression
+        #   "." match strings with at least one character (inverted with -v to mean match empty strings)
         # TODO remove the -i
         msggrep -v -T -i -e "." locale/$locale/messages.po > locale/$locale/new-messages.po
 
-        echo "Converting to CVS for ${locale}"
+        echo "Converting PO files to CVS for ${locale}..."
         po2csv locale/$locale/new-messages.po > locale/$locale/new-messages.csv
     done
+
+    echo "Please send files locale/*/new-messages.csv or locale/*/new-messages.po for translation"
+}
+
+build_changed() {
+    for locale in "${locales[@]}"
+    do
+        echo "Converting CVS to PO files for ${locale}..."
+        csv2po locale/$locale/new-messages.csv locale/$locale/new-messages.po
+
+        echo "Merging new translations into existing message file for ${locale}..."
+        msgcat locale/$locale/messages.po locale/$locale/new-messages.po > locale/$locale/merged-messages.po
+        cp locale/$locale/messages.po locale/$locale/messages.po.backup
+        cp locale/$locale/merged-messages.po locale/$locale/messages.po
+    done
+
+    echo "Converting PO files to Javascript..."
+    yarn build-strings
+
+    echo "Building the app..."
+    yarn cbt
 }
 
 # translate
@@ -81,6 +89,10 @@ extract_changed() {
 # Combine old and new translations with "msgcat locale/ar/messages.po locale/ar/new-messages.po > locale/ar/joined-messages.po"
 # Rename new files to replace the old po files
 # Clean up temp files
+
+clean() {
+    rm locale/*/new-messages.* locale/*/merged-messages.po locale/*/*.
+}
 
 help() {
     echo "Help"
@@ -94,6 +106,9 @@ elif [ "$1" == "--build-all" ]; then
 
 elif [ "$1" == "--extract-changed" ]; then
     extract_changed
+
+elif [ "$1" == "--build-changed" ]; then
+    build_changed
 
 else
     help
