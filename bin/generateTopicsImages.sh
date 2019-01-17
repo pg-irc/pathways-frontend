f3cd5973d834eb2bb8ad03039ddd769ad79a12a5
DarwishMenna <menna@peacegeeks.org>
2019-01-16 22:45:01 -0800

    Issue #367 Modify path

--- bin/generateTopicsImages.sh
+++ bin/generateTopicsImages.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-path='../src/application/topicsImages.ts'
+path='src/application/topicsImages.ts'
 
-echo "export const topicsImages = {" >> $path
-for f in ../assets/images/topics/*.png; do
-    filename=$(basename "$f" | cut -d. -f1) 
+echo "export const topicsImages: any = {" >> $path
+for f in assets/images/topics/*.png; do
+    filename=$(basename "$f" | cut -d. -f1)
     echo "'$filename' : require('../../assets/images/topics/$filename.png')," >> $path
 done
 echo "};" >> $path
