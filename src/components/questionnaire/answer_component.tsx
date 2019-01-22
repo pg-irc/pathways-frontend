9a6c061e1e5a0d49ebd4d8864c4c1600f929ad3b
Ben St John <ben@affinitybridge.com>
2019-01-22 13:46:20 -0800

    Issue #369 Use TouchableHighlight to improve responsiveness of questionnaire buttons

--- src/components/questionnaire/answer_component.tsx
+++ src/components/questionnaire/answer_component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableHighlight } from 'react-native';
 import { Text } from 'native-base';
 import { ChooseAnswerAction } from '../../stores/questionnaire';
 import { Id } from '../../stores/questionnaire';
@@ -19,14 +19,16 @@ type Props = AnswerProps & AnswerActions;
 export const AnswerComponent: React.StatelessComponent<Props> = (props: Props): JSX.Element => {
     const onPress = (): ChooseAnswerAction => props.chooseAnswer(props.answer.id);
     return (
-        <TouchableOpacity
+        <TouchableHighlight
             onPress={onPress}
+            underlayColor={props.answer.isChosen ? colors.orange : colors.white }
+            activeOpacity={.75}
             style={{
                 padding: 10,
                 backgroundColor: props.answer.isChosen ? colors.orange : colors.white,
                 borderRadius: values.roundedBorderRadius,
                 borderWidth: 3,
-                borderColor: props.answer.isChosen ? colors.orange : colors.lightGrey,
+                borderColor: props.answer.isChosen ? 'transparent' : colors.lightGrey,
                 margin: 3,
                 width: '100%',
             }}
@@ -34,6 +36,6 @@ export const AnswerComponent: React.StatelessComponent<Props> = (props: Props): 
             <Text style={props.answer.isChosen ? textStyles.headlineH3StyleWhiteCenter : textStyles.headlineH3StyleBlackCenter}>
                 {props.answer.text}
             </Text>
-        </TouchableOpacity>
+        </TouchableHighlight>
     );
 };
