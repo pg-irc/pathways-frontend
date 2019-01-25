264819d3778ec55c502dd51193413018bc2a8b9e
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-25 15:10:52 -0800

    Issue #423 Rename a function

--- src/selectors/tasks/select_task_for_current_explore_section.ts
+++ src/selectors/tasks/select_task_for_current_explore_section.ts
@@ -4,7 +4,7 @@ import * as store from '../../stores/tasks';
 import { selectTaxonomyTermsForChosenAnswers } from '../taxonomies/select_taxonomy_terms_for_chosen_answers';
 import { RouterProps } from '../../application/routing';
 import { selectLocale } from '../locale/select_locale';
-import { findItemsByLearnTaxonomyTerm } from '../taxonomies/find_items_by_explore_taxonomy_term';
+import { findItemsByExploreTaxonomyTerm } from '../taxonomies/find_items_by_explore_taxonomy_term';
 import { Task } from './task';
 import { toSelectorTaskWithoutRelatedEntities } from './to_selector_task_without_related_entities';
 import { selectExploreSectionFromTask } from './select_explore_section_from_task';
@@ -16,7 +16,7 @@ import { sortTaskList } from './sort_task_list';
 export const selectTaskForCurrentExploreSection = (appStore: Store, routerProps: RouterProps): ReadonlyArray<Task> => {
     const currentExploreSection = pickExploreSectionById(appStore, routerProps.match.params.learnId);
     const tasks = pickTasks(appStore);
-    const matchingTasks = findItemsByLearnTaxonomyTerm(currentExploreSection.taxonomyTerms, tasks);
+    const matchingTasks = findItemsByExploreTaxonomyTerm(currentExploreSection.taxonomyTerms, tasks);
 
     const locale = selectLocale(appStore);
 
