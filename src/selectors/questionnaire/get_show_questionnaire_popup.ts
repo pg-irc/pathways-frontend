865b15054960873b603dda9f7724d671e77d3734
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-01-29 09:16:16 -0800

    Issue #369 Implement new selector that is fast when the newly recommended tasks popup should not be shown

--- src/selectors/questionnaire/get_show_questionnaire_popup.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from '../../stores';
-import { pickQuestionnaire } from './pick_questionnaire';
-import { QuestionnaireRouteState } from '../../fixtures/types/questionnaire';
-
-export const getShowQuestionnairePopup = (appStore: Store): boolean => (
-    pickQuestionnaire(appStore).questionnaireRouteState === QuestionnaireRouteState.ShowQuestionnairePopup
-);
