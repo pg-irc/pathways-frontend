01de8a3bd9b932319203074e38f2562d6808f6d1
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-02-01 08:36:27 -0800

    Split file up

---
+++ src/selectors/tasks/is_task_recommended_to_all.ts
@@ -0,0 +1,13 @@
+import { Task } from '../../stores/tasks';
+import { Taxonomies } from '../../application/constants';
+import { TaxonomyTermReference } from '../taxonomies/pull_explore_taxonomy';
+import * as R from 'ramda';
+
+const recommendedToAll: TaxonomyTermReference = {
+    taxonomyId: Taxonomies.RECOMMENDATION_TAXONOMY_ID,
+    taxonomyTermId: Taxonomies.RECOMMEND_TO_ALL_TAXONOMY_TERM_ID,
+};
+
+export const isTaskRecommendedToAll = (task: Task): boolean => (
+    R.any(R.equals(recommendedToAll), task.taxonomyTerms)
+);
