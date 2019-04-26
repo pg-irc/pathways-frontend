472e868aec359a6825fa0ec44a4d418c839424b6
Ben St John <ben@affinitybridge.com>
2019-04-26 13:36:10 -0700

    Issue #618 Flesh out how we send link click events to Google Analytics and implement for services

--- src/components/link/text_with_phone_links.tsx
+++ src/components/link/text_with_phone_links.tsx
@@ -1,12 +1,15 @@
 // tslint:disable:no-let readonly-array no-expression-statement
 import React from 'react';
 import { Text } from 'native-base';
-import { Link } from './link';
+import { AnalyticsLink } from './link';
 import { phoneNumberRegex } from '../../application/regular_expressions';
 import { mapWithIndex } from '../../application/map_with_index';
 
 export interface TextWithPhoneLinksProps {
     readonly text: string;
+    readonly currentPath: string;
+    readonly linkContext: string;
+    readonly linkSource: string;
 }
 
 export const TextWithPhoneLinks: React.StatelessComponent<TextWithPhoneLinksProps> = (props: TextWithPhoneLinksProps): JSX.Element => {
@@ -18,7 +21,17 @@ export const TextWithPhoneLinks: React.StatelessComponent<TextWithPhoneLinksProp
                 mapWithIndex((word: string, currentIndex: number): string | JSX.Element => {
                     const wordForSentence = currentIndex === lastIndex ? word : word + ' ';
                     if (phoneNumberRegex.test(word)) {
-                        return <Link key={currentIndex} href={'tel:' + word} >{wordForSentence}</Link>;
+                        return (
+                            <AnalyticsLink
+                                key={currentIndex}
+                                href={`tel: ${word}`}
+                                currentPath={props.currentPath}
+                                linkContext={props.linkContext}
+                                linkSource={props.linkSource}
+                            >
+                                {wordForSentence}
+                            </AnalyticsLink>
+                        );
                     }
                     return wordForSentence;
                 }, words)
