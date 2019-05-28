e598899d75654b7ecb1abe8821e98c4e6d368efb
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-05-28 15:35:02 -0700

    Issue #606 Clean up

--- src/stores/locale/index.ts
+++ src/stores/locale/index.ts
@@ -41,18 +41,14 @@ export const reducer = (store: LocaleStore = buildDefaultStore(), action?: Reduc
             return { ...store, errorMessage: '', loading: true };
         case constants.LOAD_CURRENT_LOCALE_SUCCESS:
             return { ...store, errorMessage: '', loading: false, isSet: action.payload.isSet, code: action.payload.localeCode };
-        case constants.LOAD_CURRENT_LOCALE_FAILURE: {
-            const payload = action.payload;
-            return { ...store, errorMessage: payload.message, loading: false };
-        }
+        case constants.LOAD_CURRENT_LOCALE_FAILURE:
+            return { ...store, errorMessage: action.payload.message, loading: false };
         case constants.SET_LOCALE_REQUEST:
             return { ...store, errorMessage: '', loading: true };
         case constants.SET_LOCALE_SUCCESS:
             return { ...store, errorMessage: '', loading: false, code: action.payload.localeCode };
-        case constants.SET_LOCALE_FAILURE: {
-            const payload = action.payload;
-            return { ...store, errorMessage: payload.message, loading: false };
-        }
+        case constants.SET_LOCALE_FAILURE:
+            return { ...store, errorMessage: action.payload.message, loading: false };
         default:
             return store;
     }
