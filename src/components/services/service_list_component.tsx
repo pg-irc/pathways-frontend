1383d94a1d393cd10f87876ee6603da3ed24d9d6
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-06-06 16:47:22 -0700

    Issue #644 Refactor to use styles for alwaysLeft components

--- src/components/services/service_list_component.tsx
+++ src/components/services/service_list_component.tsx
@@ -18,7 +18,6 @@ import { EmptyListComponent } from '../empty_component/empty_list_component';
 import { ServiceListErrorComponent } from './service_list_error_component';
 import { isErrorSelectorTaskServices } from '../../selectors/services/is_error_selector_task_services';
 import * as constants from '../../application/constants';
-import { alwaysLeftTextAlign } from './always_left_text_align';
 
 export interface ServiceListProps {
     readonly topic: Topic;
@@ -104,10 +103,10 @@ const ServiceListHeaderComponent = (props: Props): JSX.Element => (
                 fontSize: values.smallIconSize,
             }}
         />
-        <Text style={[textStyles.headlineH5StyleBlackLeft, { color: colors.white }, alwaysLeftTextAlign()]}>
-            FIND A SERVICE NEAR YOU
-        </Text>
-        <Text style={[textStyles.headlineH2StyleWhiteLeft, alwaysLeftTextAlign()]}>
+        <Text style={
+            [textStyles.headlineH5StyleBlackLeft, textStyles.alwaysLeftAlign, { color: colors.white }]
+        }>FIND A SERVICE NEAR YOU</Text>
+        <Text style={[textStyles.headlineH2StyleWhiteLeft, textStyles.alwaysLeftAlign]}>
             {props.topic.englishTitle}
         </Text>
     </View >
