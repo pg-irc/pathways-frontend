6b5f21fbb4969287e8c8ee27adbdfee5a16d9006
Tomy Reyes <tomy@peacegeeks.org>
2019-06-11 16:34:02 -0700

    Issue #625 remove merge conflict markers

--- src/selectors/explore/select_current_explore_section.ts
+++ src/selectors/explore/select_current_explore_section.ts
@@ -6,12 +6,7 @@ import { ExploreSection } from './types';
 import { buildExploreSection } from './build_explore_section';
 
 export const selectCurrentExploreSection = (store: Store, routerProps: RouterProps): ExploreSection => {
-<<<<<<< HEAD
-    const locale = selectLocale(store);
     const sections = store.exploreSections.sections;
-=======
-    const sections = store.exploreSectionsInStore.sections;
->>>>>>> Issue #625 pull explore strings from PO files
     const id = routerProps.match.params.learnId;
     const theSection = sections[id];
     const exploreTaxonomy = pullExploreTaxonomy(store);
--- src/selectors/explore/select_explore_section_list.ts
+++ src/selectors/explore/select_explore_section_list.ts
@@ -4,12 +4,7 @@ import { ExploreSection } from './types';
 import { buildExploreSectionList } from './build_explore_section_list';
 
 export const selectExploreSectionList = (store: Store): ReadonlyArray<ExploreSection> => {
-<<<<<<< HEAD
-    const locale = selectLocale(store);
     const sections = store.exploreSections.sections;
-=======
-    const sections = store.exploreSectionsInStore.sections;
->>>>>>> Issue #625 pull explore strings from PO files
     const exploreTaxonomy = pullExploreTaxonomy(store);
     return buildExploreSectionList(sections, exploreTaxonomy);
 };
