6bf791279ef38409cf83006071263aa227ada363
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-06-12 15:02:22 -0700

    Issue #356 Create UI on help page for settin the location

--- src/components/help/help_connected_component.tsx
+++ src/components/help/help_connected_component.tsx
@@ -4,13 +4,26 @@ import { Store } from '../../stores';
 import { HelpComponentActions, HelpComponent, HelpComponentProps } from './help_component';
 import { clearAllUserData, ClearAllUserDataAction } from '../../stores/questionnaire/actions';
 import { RouterProps } from '../../application/routing';
+import {
+    ClearManualUserLocationAction,
+    clearManualUserLocation,
+    SetManualUserLocationAction,
+    setManualUserLocation,
+    LatLong,
+} from '../../stores/manual_user_location';
+import { selectManualUserLocation } from '../../selectors/services/select_manual_user_location';
 
-const mapStateToProps = (_: Store, ownProps: RouterProps): HelpComponentProps => ({
+const mapStateToProps = (store: Store, ownProps: RouterProps): HelpComponentProps => ({
     history: ownProps.history,
+    manualUserLocation: selectManualUserLocation(store),
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<ClearAllUserDataAction>): HelpComponentActions => ({
+type Actions = ClearAllUserDataAction | SetManualUserLocationAction | ClearManualUserLocationAction;
+
+const mapDispatchToProps = (dispatch: Dispatch<Actions>): HelpComponentActions => ({
     clearAllUserState: (): ClearAllUserDataAction => dispatch(clearAllUserData()),
+    setManualUserLocation: (userLocation: LatLong): SetManualUserLocationAction => dispatch(setManualUserLocation(userLocation)),
+    clearManualUserLocation: (): ClearManualUserLocationAction => dispatch(clearManualUserLocation()),
 });
 
 export const HelpConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(HelpComponent);
