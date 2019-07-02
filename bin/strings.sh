ebe41d9e4a787ce4715ed6125e1e3b3b7bf050f0
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-07-02 09:47:21 -0700

    Issue #662 Remove strings commands that are now handled by weblate

--- bin/strings.sh
+++ bin/strings.sh
@@ -38,83 +38,6 @@ extract_all() {
     echo "Please send files locale/*/messages.csv or locale/*/messages.po for translation"
 }
 
-
-build_all() {
-    for locale in "${locales[@]}"
-    do
-        echo "Converting CSV to PO files for ${locale} ..."
-        csv2po --progress none locale/$locale/messages.csv locale/$locale/messages.po
-        checkForSuccess "convert csv files to po"
-    done
-
-    echo "Converting PO files to Javascript ..."
-    yarn build-strings
-    checkForSuccess "yarn build-strings"
-
-    echo "Building the app ..."
-    yarn cbt
-    checkForSuccess "yarn cbt"
-}
-
-
-extract_changed() {
-    echo "Building the client ..."
-    yarn build
-    checkForSuccess "yarn build"
-
-    echo "Generating PO files ..."
-    yarn extract-strings
-    checkForSuccess "yarn extract-strings"
-
-    for locale in "${locales[@]}"
-    do
-        # Use msggrep to get the strings needing translation
-        #   -v invert the result
-        #   -T match msgstr entries, i.e. the translated strings
-        #   -e match using regular expression
-        #   "." match strings with at least one character (inverted with -v to mean match empty strings)
-        echo "Filtering strings needing translation for ${locale} ..."
-        msggrep -v -T -e "." locale/$locale/messages.po > locale/$locale/new-messages.po
-        checkForSuccess "filter strings needing translation"
-
-        echo "Converting PO files to CSV for ${locale} ..."
-        po2csv --progress none locale/$locale/new-messages.po > locale/$locale/new-messages.csv
-        checkForSuccess "convert po files to csv"
-
-        echo "Fixing line breaks in PO file for ${locale}..."
-        csv2po --progress none locale/$locale/new-messages.csv > locale/$locale/new-messages.po
-        checkForSuccess "convert csv files back to po to normalize line breaks"
-    done
-
-    echo "Please send files locale/*/new-messages.csv or locale/*/new-messages.po for translation"
-}
-
-
-build_changed() {
-    for locale in "${locales[@]}"
-    do
-        echo "Converting CSV to PO files for ${locale} ..."
-        csv2po --progress none locale/$locale/new-messages.csv locale/$locale/new-messages.po
-        checkForSuccess "convert csv files to po"
-
-        echo "Merging new translations into existing message file for ${locale} ..."
-        msgcat locale/$locale/messages.po locale/$locale/new-messages.po > locale/$locale/merged-messages.po
-        checkForSuccess "merging new translations into po files"
-
-        cp locale/$locale/messages.po locale/$locale/messages.po.backup
-        cp locale/$locale/merged-messages.po locale/$locale/messages.po
-    done
-
-    echo "Converting PO files to Javascript ..."
-    yarn build-strings
-    checkForSuccess "yarn build-strings"
-
-    echo "Building the app ..."
-    yarn cbt
-    checkForSuccess "yarn cbt"
-}
-
-
 normalize_line_breaks(){
     for locale in "${locales[@]}"
     do
@@ -205,12 +128,9 @@ prompt_manual_steps_if_po_file_needs_editing() {
 
 help() {
     echo "$0 --extract-all        Extract all strings from source code to PO and CSV files, leaving out no longer used strings"
-    echo "$0 --build-all          Import all strings from CSV files and build the app"
-    echo "$0 --extract-changed    Extract just the changed strings from source code to PO and CSV files"
-    echo "$0 --build-changed      Import just the changed strings from CSV files and build the app"
     echo "$0 --normalize          Normalize line breaks in PO files to minimize diffs"
     echo "$0 --clean              Remove temporary files"
-    echo "$0 --combine-pos        Combine PO Files to prepare for deployment - Mandatory argument: path to directory which does not already exist to clone ui-strings repository"    
+    echo "$0 --combine-pos        Combine PO Files to prepare for deployment - Mandatory argument: path to directory which does not already exist to clone ui-strings repository"
     echo
 
     for locale in "${locales[@]}"
