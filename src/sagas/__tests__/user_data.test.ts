38708dfce6031c7d81cf4f0adb225fb196c56354
Ian Lin <ian@peacegeeks.org>
2019-07-26 14:08:19 -0700

    Issue #740 Changed constant name to be consistent

--- src/sagas/__tests__/user_data.test.ts
+++ src/sagas/__tests__/user_data.test.ts
@@ -4,20 +4,20 @@ import { PersistedUserDataBuilder } from '../../stores/__tests__/helpers/user_da
 import { aString } from '../../application/__tests__/helpers/random_test_values';
 import { serialize, deserialize } from '../user_data';
 
-describe('persistent user_data tests', () => {
+describe('persisted user_data tests', () => {
         
     test('chosen answer should persist between being serialized and deserialized', () => {
-        const persistentData = new PersistedUserDataBuilder().addChosenAnswer(aString()).buildObject();
-        expect(persistentData).toEqual(deserialize(serialize(persistentData)));
+        const persistedData = new PersistedUserDataBuilder().addChosenAnswer(aString()).buildObject();
+        expect(persistedData).toEqual(deserialize(serialize(persistedData)));
     });
         
     test('saved topics should persist between being serialized and deserialized', () => {
-        const persistentData = new PersistedUserDataBuilder().addSavedTopic(aString()).buildObject();
-        expect(persistentData).toEqual(deserialize(serialize(persistentData)));
+        const persistedData = new PersistedUserDataBuilder().addSavedTopic(aString()).buildObject();
+        expect(persistedData).toEqual(deserialize(serialize(persistedData)));
     });
 
     test('completed topics should persist between being serialized and deserialized', () => {
-        const persistentData = new PersistedUserDataBuilder().addChosenAnswer(aString()).buildObject();
-        expect(persistentData).toEqual(deserialize(serialize(persistentData)));
+        const persistedData = new PersistedUserDataBuilder().addChosenAnswer(aString()).buildObject();
+        expect(persistedData).toEqual(deserialize(serialize(persistedData)));
     });
 });
