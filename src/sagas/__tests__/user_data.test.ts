4570415f87896daeb23b49e47d2aaea69f939cf1
Ian Lin <ian@peacegeeks.org>
2019-07-26 14:54:38 -0700

    Issue #740 Refactored function name to be more accurate

--- src/sagas/__tests__/user_data.test.ts
+++ src/sagas/__tests__/user_data.test.ts
@@ -2,28 +2,28 @@
 
 import { PersistedUserDataBuilder } from '../../stores/__tests__/helpers/user_data_helpers';
 import { aString } from '../../application/__tests__/helpers/random_test_values';
-import { serializePersistedData, deserializePersistedData } from '../user_data';
+import { serializeUserData, deserializeUserData } from '../user_data';
 
 describe('persisted user_data tests', () => {
         
     test('chosen answer should persist between being serialized and deserialized', () => {
         const chosenAnswer = aString();
-        const persistedData = new PersistedUserDataBuilder().addChosenAnswer(chosenAnswer).buildObject();
-        const recreatedData = deserializePersistedData(serializePersistedData(persistedData));
-        expect(chosenAnswer).toEqual(recreatedData.chosenAnswers[0]);
+        const userData = new PersistedUserDataBuilder().addChosenAnswer(chosenAnswer).buildObject();
+        const recreatedData = deserializeUserData(serializeUserData(userData));
+        expect(recreatedData.chosenAnswers[0]).toEqual(chosenAnswer);
     });
         
     test('saved topics should persist between being serialized and deserialized', () => {
         const savedTopic = aString();
-        const persistedData = new PersistedUserDataBuilder().addSavedTopic(savedTopic).buildObject();
-        const recreatedData = deserializePersistedData(serializePersistedData(persistedData));
-        expect(savedTopic).toEqual(recreatedData.savedTopics[0]);
+        const userData = new PersistedUserDataBuilder().addSavedTopic(savedTopic).buildObject();
+        const recreatedData = deserializeUserData(serializeUserData(userData));
+        expect(recreatedData.savedTopics[0]).toEqual(savedTopic);
     });
 
     test('completed topics should persist between being serialized and deserialized', () => {
         const completedTopic = aString();
-        const persistedData = new PersistedUserDataBuilder().addCompletedTopic(completedTopic).buildObject();
-        const recreatedData = deserializePersistedData(serializePersistedData(persistedData));
-        expect(completedTopic).toEqual(recreatedData.completedTopics[0]);
+        const userData = new PersistedUserDataBuilder().addCompletedTopic(completedTopic).buildObject();
+        const recreatedData = deserializeUserData(serializeUserData(userData));
+        expect(recreatedData.completedTopics[0]).toEqual(completedTopic);
     });
 });
