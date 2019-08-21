15530d9885ab3bf88efdde5507fac03e5450dc6d
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-08-20 17:01:58 -0700

    Issue #704 Simplify helper function for OS

--- src/helpers/is_os.ts
+++ src/helpers/is_os.ts
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
 
-export const isOS = (OS: string): boolean => (
-    Platform.OS === OS
+export const isAndroid = (): boolean => (
+    Platform.OS === 'android'
 );
