d1a8ad3ad6311748da5e54354c59a22c2da36f34
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-08-27 13:39:30 -0700

    Issue pg-irc/pathways-backend#423 Remove infinite hits component

--- src/components/search/search_component.tsx
+++ src/components/search/search_component.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
-import { InstantSearch, connectInfiniteHits, connectSearchBox, connectAutoComplete } from 'react-instantsearch-native';
+import { InstantSearch, connectSearchBox, connectAutoComplete } from 'react-instantsearch-native';
 import { SearchBoxComponent } from './search_box_component';
-import { InfiniteHitsComponent } from './infinite_hits_component';
 import { AutoCompleteComponent, Suggestion } from './auto_complete_component';
 
 export interface SearchComponentProps {
@@ -13,13 +12,11 @@ export interface SearchComponentProps {
 
 export const SearchComponent: React.StatelessComponent<SearchComponentProps> = (props: SearchComponentProps): JSX.Element => {
     const SearchBoxConnectedComponent = connectSearchBox(SearchBoxComponent);
-    const InfiniteHitsConnectedComponent = connectInfiniteHits(InfiniteHitsComponent);
     const AutoCompleteConnectedComponent = connectAutoComplete<Suggestion>(AutoCompleteComponent);
     return <View>
         <InstantSearch {...props} >
             <SearchBoxConnectedComponent />
             <AutoCompleteConnectedComponent />
-            <InfiniteHitsConnectedComponent />
         </InstantSearch>
     </View>;
 };
