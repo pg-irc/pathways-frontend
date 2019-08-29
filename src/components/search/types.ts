e8e2077d3eec876e6e1cfac82ab21e5546c8ce44
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-08-29 11:09:41 -0700

    Issue #797 Refactor

---
+++ src/components/search/types.ts
@@ -0,0 +1,8 @@
+export interface ServiceHit {
+    readonly service_id: string;
+    readonly service_name: string;
+    readonly service_description: string;
+    readonly street_address: string;
+    readonly city: string;
+    readonly postal_code: string;
+}
