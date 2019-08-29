09ddb726481e7a4baa97a3dd5829c64ef9b7870c
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-08-29 09:47:23 -0700

    Issue #797 Improve widget layout

--- src/components/search/search_box_component.tsx
+++ src/components/search/search_box_component.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
+import { Icon } from 'native-base';
+import { values } from '../../application/styles';
 
 const styles = StyleSheet.create({
     container: {
@@ -34,18 +36,24 @@ export const SearchBoxComponent = (props: Props & Actions): JSX.Element => {
     const [location, setLocation]: [string, (s: string) => void] = useState(props.location);
 
     return <View style={styles.container}>
-        <TextInput
-            style={styles.input}
-            onChangeText={props.refine}
-            value={props.currentRefinement}
-            placeholder='Search for services and organizations' // TODO translate
-        />
-        <TextInput
-            style={styles.input}
-            onChangeText={setLocation}
-            value={location}
-            onEndEditing={(): void => props.setLocation(location)}
-            placeholder='Near My location' // TODO translate
-        />
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Icon name={'search'} type='FontAwesome' style={{ fontSize: values.smallIconSize, flex: .1, marginHorizontal: 3 }} />
+            <TextInput
+                style={[styles.input, { flex: 1 }]}
+                onChangeText={props.refine}
+                value={props.currentRefinement}
+                placeholder='Search for services and organizations' // TODO translate
+            />
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Icon name={'map-marker'} type='FontAwesome' style={{ fontSize: values.smallIconSize, flex: .1, marginHorizontal: 3 }} />
+            <TextInput
+                style={[styles.input, { flex: 1 }]}
+                onChangeText={setLocation}
+                value={location}
+                onEndEditing={(): void => props.setLocation(location)}
+                placeholder='Near My location' // TODO translate
+            />
+        </View>
     </View>;
 };
