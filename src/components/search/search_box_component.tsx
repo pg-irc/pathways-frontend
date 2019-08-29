3e3e87ca8c457644354ad097a3bb0d90f7674b68
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-08-29 10:29:47 -0700

    Issue #797 Refactor

--- src/components/search/search_box_component.tsx
+++ src/components/search/search_box_component.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Icon } from 'native-base';
 import { values } from '../../application/styles';
 
@@ -13,46 +13,45 @@ export interface Actions {
     readonly setLocation: (s: string) => void;
 }
 
+const styles = StyleSheet.create({
+    input: { flex: 1, height: 48, padding: 12, fontSize: 16 },
+});
+
 export const SearchBoxComponent = (props: Props & Actions): JSX.Element => {
     const [location, setLocation]: [string, (s: string) => void] = useState(props.location);
 
     return <View>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon name={'search'} type='FontAwesome' style={{ fontSize: values.smallIconSize, flex: .1, marginHorizontal: 3 }} />
+            {icon('search')}
             <TextInput
-                style={{
-                    flex: 1,
-                    height: 48,
-                    padding: 12,
-                    fontSize: 16,
-                }}
+                style={styles.input}
                 onChangeText={props.refine}
                 value={props.currentRefinement}
                 placeholder='Search for services and organizations' // TODO translate
             />
         </View>
-        <View style={{
-            borderBottomWidth: 1,
-            borderColor: '#ddd',
-        }} />
+        <Separator />
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon name={'map-marker'} type='FontAwesome' style={{ fontSize: values.smallIconSize, flex: .1, marginHorizontal: 3 }} />
+            {icon('map-marker')}
             <TextInput
-                style={{
-                    flex: 1,
-                    height: 48,
-                    padding: 12,
-                    fontSize: 16,
-                }}
+                style={styles.input}
                 onChangeText={setLocation}
                 value={location}
                 onEndEditing={(): void => props.setLocation(location)}
                 placeholder='Near My location' // TODO translate
             />
         </View>
-        <View style={{
-            borderBottomWidth: 1,
-            borderColor: '#ddd',
-        }} />
+        <Separator />
     </View>;
 };
+
+const icon = (name: string): JSX.Element => (
+    <Icon name={name} type='FontAwesome' style={{ fontSize: values.smallIconSize, flex: .1, marginHorizontal: 3 }} />
+);
+
+const Separator = (): JSX.Element => (
+    <View style={{
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+    }} />
+);
