30162b835f4e0e4ae0c9448256e0cf48ce3b9875
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-03 16:21:05 -0700

    Issue #797 Refactor

--- src/components/search/types.ts
+++ src/components/search/types.ts
@@ -6,3 +6,28 @@ export interface ServiceHit {
     readonly city: string;
     readonly postal_code: string;
 }
+
+export interface ServiceSearchItem {
+    readonly service_id: string;
+    readonly service_name: string;
+    readonly service_description: string;
+    readonly street_address: string;
+    readonly city: string;
+    readonly postal_code: string;
+    readonly _geoloc: GeoLocation;
+}
+
+export interface GeoLocation {
+    readonly lng: number;
+    readonly lat: number;
+}
+
+export interface OrganizationSearchItem {
+    // TODO add organization id to the index
+    readonly organization_id: string;
+    readonly organization_name: string;
+    readonly organization_description: string;
+    readonly organization_website: string;
+    readonly organization_email: string;
+}
+
