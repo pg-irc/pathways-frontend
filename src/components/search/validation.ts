ff37969116f6ccf04ba185c69668b84642ec44ff
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-04 17:12:50 -0700

    Issue #797 Cleanup

--- src/components/search/validation.ts
+++ src/components/search/validation.ts
@@ -3,49 +3,43 @@ const Ajv = require('ajv');
 import * as schema from './schema';
 import { ServiceSearchHit, OrganizationSearchHit, UnvalidatedData, SearchHit } from './types';
 
-export const toValidSearchHit = (data: UnvalidatedData): SearchHit => {
-    if (isServiceHit(data)) {
-        return toServiceHit(data);
+export const toValidSearchHit = (hit: UnvalidatedData): SearchHit => {
+    if (isServiceHit(hit)) {
+        return toServiceHit(hit);
     }
-    if (isOrganizationHit(data)) {
-        return toOrganizationHit(data);
+    if (isOrganizationHit(hit)) {
+        return toOrganizationHit(hit);
     }
     throw new Error('Search response failed validation as service or organization data');
 };
 
-const isServiceHit = (data: UnvalidatedData): boolean => {
+const isServiceHit = (hit: UnvalidatedData): boolean => {
     const ajv = new Ajv();
-    return ajv.validate(schema.serviceSearchItem, data) as boolean;
+    return ajv.validate(schema.serviceSearchItem, hit) as boolean;
 };
 
-const toServiceHit = (data: UnvalidatedData): ServiceSearchHit => {
-    const buildServiceSearchItem = (item: UnvalidatedData): ServiceSearchHit => ({
-        type: 'ServiceSearchItem',
-        service_id: item.service_id,
-        service_name: item.service_name,
-        service_description: item.service_description,
-        street_address: item.street_address,
-        city: item.city,
-        postal_code: item.postal_code,
-        latitude: item._geoloc.lat,
-        longitude: item._geoloc.lng,
-    });
-    return buildServiceSearchItem(data);
-};
+const toServiceHit = (hit: UnvalidatedData): ServiceSearchHit => ({
+    type: 'ServiceSearchItem',
+    service_id: hit.service_id,
+    service_name: hit.service_name,
+    service_description: hit.service_description,
+    street_address: hit.street_address,
+    city: hit.city,
+    postal_code: hit.postal_code,
+    latitude: hit._geoloc.lat,
+    longitude: hit._geoloc.lng,
+});
 
-const isOrganizationHit = (data: UnvalidatedData): boolean => {
+const isOrganizationHit = (hit: UnvalidatedData): boolean => {
     const ajv = new Ajv();
-    return ajv.validate(schema.organizationSearchItem, data) as boolean;
+    return ajv.validate(schema.organizationSearchItem, hit) as boolean;
 };
 
-const toOrganizationHit = (data: UnvalidatedData): OrganizationSearchHit => {
-    const buildOrganizationSearchItem = (item: UnvalidatedData): OrganizationSearchHit => ({
-        type: 'OrganizationSearchItem',
-        organization_id: 'TODO',
-        organization_name: item.organization_name,
-        organization_description: item.organization_description,
-        organization_website: item.organization_website,
-        organization_email: item.organization_email,
-    });
-    return buildOrganizationSearchItem(data);
-};
+const toOrganizationHit = (hit: UnvalidatedData): OrganizationSearchHit => ({
+    type: 'OrganizationSearchItem',
+    organization_id: hit.organization_id,
+    organization_name: hit.organization_name,
+    organization_description: hit.organization_description,
+    organization_website: hit.organization_website,
+    organization_email: hit.organization_email,
+});
