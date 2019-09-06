71a662a3f2d277e7b46434f0f29f0e9e04aa0900
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-06 10:38:33 -0700

    Issue #797 Pull out search configuration into its own file

--- src/components/search/search_component.tsx
+++ src/components/search/search_component.tsx
@@ -11,6 +11,7 @@ import { getStatusBarHeightForPlatform } from '../main/get_status_bar_height_for
 import { useOnlineStatus } from '../../hooks/use_online_status';
 import { LatLong } from './types';
 import { fetchLatLongFromAddress } from './fetch_lat_long_from_address';
+import { toServiceSearchConfiguration } from './configuration';
 
 export interface SearchComponentProps {
     readonly apiKey: string;
@@ -47,31 +48,14 @@ export const SearchComponent: React.StatelessComponent<SearchComponentProps> = (
     const SearchBoxConnectedComponent = connectSearchBox(SearchBoxComponent);
     const InfiniteHitsConnectedComponent = connectInfiniteHits(InfiniteHitsComponent);
 
-    // organization search cannot take aroundLatLng argument
-    // create a second InstantSearch with
-    //    a non-visible SearchBoxConnectedComponent that calls refine with the value from the first one
-    //    a ConfigureConnectedComponent that does not take the lat/long
-    //    the same InfiniteHitsConnectedComponent
     return <Content style={{ backgroundColor: colors.teal }}>
         <ScreenHeader {...props} />
         <InstantSearch indexName='dev_services' {...props} >
             <SearchBoxConnectedComponent location={location} setLocation={_setLocation} />
-            <ConfigureConnectedComponent {...toConfiguration(latlong)} />
+            <ConfigureConnectedComponent {...toServiceSearchConfiguration(latlong)} />
             <InfiniteHitsConnectedComponent />
         </InstantSearch>
     </Content>;
-};
-
-const toConfiguration = (latlong?: LatLong): Object => {
-    const hitsPerPage = 5;
-    if (latlong) {
-        const aroundLatLng = `${latlong.latitude},${latlong.longitude}`;
-        // tslint:disable-next-line:no-expression-statement
-        console.log(`aroundLatLng = ${aroundLatLng}`);
-
-        return { aroundLatLng, hitsPerPage };
-    }
-    return { hitsPerPage };
 };
 
 const ScreenHeader = (props: Props): JSX.Element => {
