041c9853c9fb28d62f6ccbe03c7e4a7e121fdc66
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-06 10:38:33 -0700

    Issue #797 Pull out search configuration into its own file

--- src/components/search/fetch_lat_long_from_address.ts
+++ src/components/search/fetch_lat_long_from_address.ts
@@ -7,13 +7,12 @@ import * as R from 'ramda';
 export const fetchLatLongFromAddress = (address: string, onlineStatus: OnlineStatus, setLatLong: (latLong: LatLong) => void): void => {
     if (address !== '' && onlineStatus === OnlineStatus.Online) {
         const url = `https://geocoder.ca/?locate=${address}&json=1`;
-
         fetch(url).
             then(toValidStringOrThrow).
             then(JSON.parse).
             then(toGeoCoderLatLong).
             then(setLatLong).
-            catch(setValueToUndefined(setLatLong));
+            catch(handleError(setLatLong));
     }
 };
 
@@ -24,7 +23,7 @@ const toValidStringOrThrow = (response: Response): Promise<string> => {
     return response.text();
 };
 
-const setValueToUndefined = R.curry((setLatLong: (latLong: LatLong) => void, error: string): void => {
+const handleError = R.curry((setLatLong: (latLong: LatLong) => void, error: string): void => {
     console.log(`LatLong set to undefined: ${error}`);
     setLatLong(undefined);
 });
