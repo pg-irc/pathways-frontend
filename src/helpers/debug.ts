1ce4d4c0ea8377db43ff70aad2d7eaf9205963ef
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-09-12 09:37:12 -0700

    Issue #797 Clean up

--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,5 +1,6 @@
 // tslint:disable:typedef no-expression-statement no-any
 import { useRef, useEffect } from 'react';
+import * as R from 'ramda';
 
 const DEBUG = false;
 
@@ -18,17 +19,22 @@ export const useTraceUpdate = (name: string, props: any): void => {
     const prevAsUnknown = useRef({ current: props }) as unknown;
     const prev = prevAsUnknown as HasOldProps;
     useEffect(() => {
-        const changedProps = Object.entries(props).reduce((ps: any, [key, v]) => {
-            if (prev.current[key] !== v) {
-                ps[key] = [prev.current[key], v];
-            }
-            return ps;
-        }, {});
-        if (Object.keys(changedProps).length > 0) {
-            const ddd = JSON.stringify(changedProps);
-            const eee = ddd.substring(0, 100);
-            console.log(`\n${name}: changed props: ${eee}`);
-        }
+        const changedProps = Object.entries(props).reduce(reducer(prev), {});
+        printPropsIfAny(name, changedProps);
         prev.current = props;
     });
+};
+
+const reducer = R.curry((reference: HasOldProps, accumulator: any, [key, newValue]: [string, any]) => {
+    const oldValue = reference.current[key];
+    if (oldValue !== newValue) {
+        accumulator[key] = [oldValue, newValue];
+    }
+    return accumulator;
+});
+
+const printPropsIfAny = (componentName: string, changedProps: object): void => {
+    if (Object.keys(changedProps).length > 0) {
+        console.log(`\n${componentName}: changed props: ${JSON.stringify(changedProps).substring(0, 100)}`);
+    }
 };
