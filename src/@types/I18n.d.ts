3685b05c7bb5e4c99df5451a7a8e582dc3d19709
Tomy Reyes <tomy@peacegeeks.org>
2019-09-16 15:40:52 -0700

    Issue #805 uncomment out types file for i18n

--- src/@types/I18n.d.ts
+++ src/@types/I18n.d.ts
@@ -1,8 +1,8 @@
-// // tslint:disable-next-line:no-class
-// declare class I18n {
-//     t(message: TemplateStringsArray): string;
-// }
+// tslint:disable-next-line:no-class
+declare class I18n {
+    t(message: TemplateStringsArray): string;
+}
 
-// declare interface I18nProps {
-//     readonly i18n: I18n;
-// }
+declare interface I18nProps {
+    readonly i18n: I18n;
+}
