1955af6d64bbbfd98a8c021ed142d4a78b2d8e4c
Ian Lin <ian@peacegeeks.org>
2019-10-07 11:47:44 -0700

    Issue #839 Make search placeholder white and translatable

--- src/components/search/search_input_component.tsx
+++ src/components/search/search_input_component.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { TextInput } from 'react-native';
-import { View, Icon } from 'native-base';
+import { View, Icon, Text } from 'native-base';
+import { Trans, I18n } from '@lingui/react';
 import { values, applicationStyles, colors } from '../../application/styles';
 import { LatLong } from './types';
 import { debug, useTraceUpdate } from '../../helpers/debug';
 import { InputFormSeparator } from './separators';
+import { ReactI18nRenderProp, ReactI18n } from '../../locale/types';
 
 export interface Props {
     readonly currentRefinement: string;
@@ -26,35 +28,48 @@ export const SearchInputComponent = (props: Props & Actions): JSX.Element => {
         props.refine(props.currentRefinement);
     }, [props.latLong]);
 
-    return <View style={{ paddingHorizontal: 20, paddingBottom: 20 }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <InputIcon name='search' />
-            <TextInput
-                style={applicationStyles.searchInput}
-                onChangeText={(d: string): void => {
-                    debug(`SearchInputComponent search text changed to '${d}'`);
-                    props.refine(d);
-                }}
-                value={props.currentRefinement}
-                placeholder='Search for services and organizations' // TODO translate
-            />
-        </View>
-        <InputFormSeparator />
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <InputIcon name='map-marker' />
-            <TextInput
-                style={applicationStyles.searchInput}
-                onChangeText={(d: string): void => {
-                    debug(`SearchInputComponent location text changed to '${d}'`);
-                    setLocation(d);
-                }}
-                value={location}
-                onEndEditing={(): void => props.setLocation(location)}
-                placeholder='Near My location' // TODO translate
-            />
-        </View>
-        <InputFormSeparator />
-    </View >;
+    const buildTranslatedPlaceholder = (i18n: ReactI18n, placeholder: string): string => {
+        const _ = i18n._.bind(i18n);
+        return _(placeholder);
+    };
+
+    return <I18n>
+        {(i18nRenderProp: ReactI18nRenderProp): JSX.Element => (
+
+            <View style={{ paddingHorizontal: 20, paddingBottom: 20 }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <InputIcon name='search' />
+                    <TextInput
+                        style={applicationStyles.searchInput}
+                        onChangeText={(d: string): void => {
+                            debug(`SearchInputComponent search text changed to '${d}'`);
+                            props.refine(d);
+                        }}
+                        value={props.currentRefinement}
+                        placeholder={buildTranslatedPlaceholder(i18nRenderProp.i18n, 'Search for services')} // TODO translate
+                        placeholderTextColor={colors.white}
+                    />
+                </View>
+                <InputFormSeparator />
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <InputIcon name='map-marker' />
+                    <TextInput
+                        style={applicationStyles.searchInput}
+                        onChangeText={(d: string): void => {
+                            debug(`SearchInputComponent location text changed to '${d}'`);
+                            setLocation(d);
+                        }}
+                        value={location}
+                        onEndEditing={(): void => props.setLocation(location)}
+                        placeholder={buildTranslatedPlaceholder(i18nRenderProp.i18n, 'Near My location')} // TODO translate
+                        placeholderTextColor={colors.white}
+                    />
+                </View>
+                <InputFormSeparator />
+            </View >
+        )}
+
+    </I18n>;
 };
 
 interface IconProps {
@@ -66,4 +81,13 @@ const InputIcon = ({ name }: IconProps): JSX.Element => (
         type='FontAwesome'
         style={{ color: colors.white, fontSize: values.smallIconSize, flex: .1, marginHorizontal: 3 }}
     />
+);
+
+export const extractSearchStrings = (): JSX.Element => (
+    <div>
+        <Text><Trans>Search for services</Trans></Text>
+        <Text>
+            <Trans>Near My location</Trans>
+        </Text>
+    </div>
 );
