307afb128b99def4a97aeaffac07d4eeb45c4195
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-10-22 11:58:45 -0700

    Issue #839 Populate client side DTO with additional fields from the JSON

--- src/components/search/types.ts
+++ src/components/search/types.ts
@@ -11,9 +11,14 @@ export interface ServiceSearchHit {
     readonly service_id: string;
     readonly service_name: string;
     readonly service_description: string;
+    readonly organization_name: string;
+    readonly organization_website: string;
+    readonly organization_email: string;
     readonly street_address: string;
     readonly city: string;
+    readonly province: string;
     readonly postal_code: string;
+    readonly country: string;
     readonly latitude: number;
     readonly longitude: number;
 }
