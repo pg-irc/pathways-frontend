07d1fea16658ed7955bf2f5c0d9f4b3d4fa7dcf4
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-10-23 11:28:37 -0700

    Issue #839 Cleanup

--- src/sagas/services.ts
+++ src/sagas/services.ts
@@ -1,55 +1,48 @@
 // tslint:disable:no-expression-statement
 import { CallEffect, PutEffect, ForkEffect, takeLatest, call, put } from 'redux-saga/effects';
 import * as constants from '../application/constants';
-import {
-    BuildServicesRequestAction, BuildServicesSuccessAction, BuildServicesErrorAction,
-    buildServicesSuccessAction, buildServicesErrorAction,
-} from '../stores/services/actions';
+import * as actions from '../stores/services/actions';
 import { validateServicesAtLocationArray } from '../validation/services';
 import { searchServices, APIResponse } from '../api';
 import { getDeviceLocation } from '../async/location';
-import {
-    isNoLocationPermissionError,
-    isLocationFetchTimeoutError,
-    isBadResponseError,
-} from '../validation/errors/is_error';
+import * as errors from '../validation/errors/is_error';
 import { Errors } from '../validation/errors/types';
 
 export function* watchUpdateTaskServices(): IterableIterator<ForkEffect> {
     yield takeLatest(constants.LOAD_SERVICES_REQUEST, updateTaskServices);
 }
 
-export function* updateTaskServices(action: BuildServicesRequestAction): UpdateResult {
+export function* updateTaskServices(action: actions.BuildServicesRequestAction): UpdateResult {
     const topicId = action.payload.topicId;
     try {
         const deviceLocationResponse = yield call(getDeviceLocation, action.payload.manualUserLocation);
 
-        if (isNoLocationPermissionError(deviceLocationResponse)) {
-            return yield put(buildServicesErrorAction(topicId, deviceLocationResponse.type));
+        if (errors.isNoLocationPermissionError(deviceLocationResponse)) {
+            return yield put(actions.buildServicesErrorAction(topicId, deviceLocationResponse.type));
         }
 
-        if (isLocationFetchTimeoutError(deviceLocationResponse)) {
-            return yield put(buildServicesErrorAction(topicId, deviceLocationResponse.type));
+        if (errors.isLocationFetchTimeoutError(deviceLocationResponse)) {
+            return yield put(actions.buildServicesErrorAction(topicId, deviceLocationResponse.type));
         }
 
-        const response: APIResponse = yield call(searchServices, topicId, deviceLocationResponse);
-        if (isBadResponseError(response)) {
-            return yield put(buildServicesErrorAction(topicId, Errors.BadServerResponse));
+        const apiResponse: APIResponse = yield call(searchServices, topicId, deviceLocationResponse);
+        if (errors.isBadResponseError(apiResponse)) {
+            return yield put(actions.buildServicesErrorAction(topicId, Errors.BadServerResponse));
         }
 
         // TODO refactor the search client code to follow the same pattern
-        const validationResult = validateServicesAtLocationArray(response.results);
-        if (!validationResult.isValid) {
-            return yield put(buildServicesErrorAction(topicId, Errors.InvalidServerData));
+        const validatedApiResponse = validateServicesAtLocationArray(apiResponse.results);
+        if (!validatedApiResponse.isValid) {
+            return yield put(actions.buildServicesErrorAction(topicId, Errors.InvalidServerData));
         }
 
-        yield put(buildServicesSuccessAction(topicId, validationResult.validData));
+        yield put(actions.buildServicesSuccessAction(topicId, validatedApiResponse.validData));
 
     } catch (error) {
-        yield put(buildServicesErrorAction(topicId, Errors.Exception));
+        yield put(actions.buildServicesErrorAction(topicId, Errors.Exception));
     }
 }
 
-type SuccessOrFailureResult = BuildServicesSuccessAction | BuildServicesErrorAction;
+type SuccessOrFailureResult = actions.BuildServicesSuccessAction | actions.BuildServicesErrorAction;
 
 type UpdateResult = IterableIterator<CallEffect | PutEffect<SuccessOrFailureResult>>;
