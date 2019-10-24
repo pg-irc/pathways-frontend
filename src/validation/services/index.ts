f85f53fa005ee974eb2561f89372ae4c7be15bc3
Tomy Reyes <tomy@peacegeeks.org>
2019-10-24 16:46:18 -0700

    Issue #839 move ordering of import statements

--- src/validation/services/index.ts
+++ src/validation/services/index.ts
@@ -1,10 +1,10 @@
-// This is the documenated syntax for Ajv. Using an import here causes a runtime error.
-// tslint:disable-next-line:no-var-requires
-const Ajv = require('ajv');
-
 import * as R from 'ramda';
 import * as types from './types';
 import { serviceAtLocationArray } from './schema';
+
+// This is the documenated syntax for Ajv. Using an import here causes a runtime error.
+// tslint:disable-next-line:no-var-requires
+const Ajv = require('ajv');
 
 // tslint:disable-next-line:no-any
 export const validateServicesAtLocationArray = (data: any): ValidationResult => {
