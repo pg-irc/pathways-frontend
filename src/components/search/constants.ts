7d012d79034f7b7248be8c237289be945861c566
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-10-25 09:58:46 -0700

    Issue #839 Move string constants into their own file

---
+++ src/components/search/constants.ts
@@ -0,0 +1,4 @@
+export const MODAL_NONE = '____MODAL_NONE____';
+export const MODAL_SEARCH_TERM = '____MODAL_SEARCH_TERM____';
+export const MODAL_LOCATION = '____MODAL_LOCATION____';
+export const USE_MY_LOCATION = '____USE_MY_LOCATION____';
