fa8d8e19ee7874798ccfd548b2e3cde08fae63cf
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-11-29 10:30:48 -0800

    Issue #916 Increase number of results returned from algolia from 5 to 100

--- src/components/search/__tests__/configuration.tests.ts
+++ src/components/search/__tests__/configuration.tests.ts
@@ -5,13 +5,13 @@ describe('configuration', () => {
     describe('for service search', () => {
         it('includes lat/long if given', () => {
             expect(toServiceSearchConfiguration({ lat: 1, lng: 1 })).toEqual({
-                hitsPerPage: 5,
+                hitsPerPage: 100,
                 aroundLatLng: '1,1',
             });
         });
         it('does not includes lat/long if missing', () => {
             expect(toServiceSearchConfiguration()).toEqual({
-                hitsPerPage: 5,
+                hitsPerPage: 100,
             });
         });
     });
