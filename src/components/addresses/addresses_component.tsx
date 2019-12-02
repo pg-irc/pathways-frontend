0cd94936937ff344dbd5932cca1c7451dc7ed747
Ben St John <ben@affinitybridge.com>
2019-12-02 14:27:47 -0800

    Issue #915 Fix bug that causes crash when onPress is undefined

--- src/components/addresses/addresses_component.tsx
+++ src/components/addresses/addresses_component.tsx
@@ -4,7 +4,7 @@ import { Trans } from '@lingui/react';
 import { Icon } from 'native-base';
 import { mapWithIndex } from '../../application/map_with_index';
 import { Address } from '../../validation/services/types';
-import { CardButtonComponent } from '../card_button/card_button_component';
+import { CardButtonComponent, CardButtonProps } from '../card_button/card_button_component';
 import { DividerComponent } from '../content_layout/divider_component';
 import { colors, values, textStyles } from '../../application/styles';
 
@@ -19,17 +19,7 @@ export const AddressesComponent = (props: Props): JSX.Element => (
     <View>
         {mapWithIndex((address: Address, index: number) =>
             <View key={index}>
-                <CardButtonComponent
-                    leftContent={<SingleAddressComponent address={address} />}
-                    rightContent={
-                        <Icon
-                            name={'location-arrow'}
-                            type={'FontAwesome'}
-                            style={{ color: colors.teal, fontSize: values.smallIconSize, paddingRight: 10 }}
-                        />
-                    }
-                    onPress={props.onPressForAddress(address)}
-                 />
+                <CardButtonComponent {...buildCardButtonProps(address, props)} />
                 <DividerComponent />
             </View>
         , props.addresses)}
@@ -45,3 +35,18 @@ export const SingleAddressComponent = (props: {readonly address: Address}): JSX.
         </Text>
     </View>
 );
+
+const buildCardButtonProps = (address: Address, props: Props): CardButtonProps => {
+    const leftContent = <SingleAddressComponent address={address} />;
+    const rightContent = (
+        <Icon
+            name={'location-arrow'}
+            type={'FontAwesome'}
+            style={{ color: colors.teal, fontSize: values.smallIconSize, paddingRight: 10 }}
+        />
+    );
+    const onPress = props.onPressForAddress ?
+        props.onPressForAddress(address) : (): undefined => undefined;
+
+    return { leftContent, rightContent, onPress };
+}
