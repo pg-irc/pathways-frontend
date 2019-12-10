40a0ef2b116892b22a8f6d43cff96258b5c14ab0
Ben St John <ben@affinitybridge.com>
2019-12-10 13:57:07 -0800

    Issue #915 Further improve and simplify logic around sending services count event

--- src/middleware/__tests__/analytics.tests.ts
+++ src/middleware/__tests__/analytics.tests.ts
@@ -1,29 +1,38 @@
 // tslint:disable:no-expression-statement
-import { computeNumberOfServicesAdded } from '../analytics';
-import { ServiceBuilder, buildNormalizedServices } from '../../stores/__tests__/helpers/services_helpers';
-import { buildDefaultStore } from '../../stores/services';
+import { shouldSendServicesReport } from '../analytics';
+import { aNumber } from '../../helpers/random_test_values';
 
 describe('Analytics memory report middleware helpers', () => {
 
-    describe('serviceWasAdded()', () => {
+    describe('shouldSendServicesReport()', () => {
 
-        it('returns true when next service store state has a new service', () => {
-            const aServiceBuilder = new ServiceBuilder();
-            const storeWithNoServices = buildDefaultStore();
-            const storeWithOneService = buildNormalizedServices([aServiceBuilder], []);
-            expect(computeNumberOfServicesAdded(storeWithNoServices, storeWithOneService)).toEqual(1);
+        it('returns true when the new services count passes the provided threshold', () => {
+            const oldServicesCount = 0;
+            const newServicesCount = 2;
+            const sendEveryCount = 1;
+            expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(true);
         });
 
-        it('returns false when next service store state has different services', () => {
-            const aServiceBuilder = new ServiceBuilder();
-            const storeWithOneService = buildNormalizedServices([aServiceBuilder], []);
-            const storeWithNoServices = buildDefaultStore();
-            expect(computeNumberOfServicesAdded(storeWithOneService, storeWithNoServices)).toEqual(0);
+        it('returns true when the new services count equals the provided threshold', () => {
+            const oldServicesCount = 0;
+            const newServicesCount = 1;
+            const sendEveryCount = 1;
+            expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(true);
         });
 
-        it('returns false when next service store state matches current state', () => {
-            const currentState = buildDefaultStore();
-            expect(computeNumberOfServicesAdded(currentState, currentState)).toEqual(0);
+        it('returns false when the new services count equals the old services count', () => {
+            const oldServicesCount = 1;
+            const newServicesCount = 1;
+            const sendEveryCount = aNumber();
+            expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(false);
         });
+
+        it('returns false when the new services count is less than the old services count', () => {
+            const oldServicesCount = 1;
+            const newServicesCount = 0;
+            const sendEveryCount = aNumber();
+            expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(false);
+        });
+
     });
 });
--- src/middleware/analytics.ts
+++ src/middleware/analytics.ts
@@ -4,7 +4,7 @@ import { Store } from '../stores';
 import * as R from 'ramda';
 import { sendServicesCountEvent } from '../sagas/analytics/events';
 import { SAVE_SERVICE, LOAD_SERVICES_SUCCESS, MEMORY_REPORT_SEND_EVERY_SERVICES_COUNT } from '../application/constants';
-import { reducer as servicesReducer, ServiceStore } from '../stores/services';
+import { reducer as servicesReducer } from '../stores/services';
 import { SaveServiceAction, BuildServicesSuccessAction } from '../stores/services/actions';
 
 // tslint:disable-next-line:no-any
@@ -23,19 +23,14 @@ type UpdateServicesActions = SaveServiceAction | BuildServicesSuccessAction;
 const sendServicesCountReport = (store: Store, action: UpdateServicesActions): void => {
     const oldStore = store.services;
     const newStore = servicesReducer(store.services, action);
-    const servicesAdded = computeNumberOfServicesAdded(oldStore, newStore);
-    const newServiceCount = R.length(R.keys(newStore.services));
+    const oldServicesCount = R.length(R.keys(oldStore.services));
+    const newServicesCount = R.length(R.keys(newStore.services));
 
-    if (servicesAdded > 0 && newServiceCount % MEMORY_REPORT_SEND_EVERY_SERVICES_COUNT === 0) {
-        sendServicesCountEvent(newServiceCount);
+    if (shouldSendServicesReport(oldServicesCount, newServicesCount, MEMORY_REPORT_SEND_EVERY_SERVICES_COUNT)) {
+        sendServicesCountEvent(newServicesCount);
     }
 };
 
-export const computeNumberOfServicesAdded = (oldStore: ServiceStore, newStore: ServiceStore): number => (
-    R.length(
-        R.difference(
-            R.keys(newStore.services),
-            R.keys(oldStore.services),
-        ),
-    )
+export const shouldSendServicesReport = (oldServicesCount: number, newServicesCount: number, sendEveryCount: number): boolean => (
+    Math.floor(oldServicesCount / sendEveryCount) < Math.floor(newServicesCount / sendEveryCount)
 );
