8b9e4293d2c796d8985bd747a83b1099365db30e
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-12-10 16:13:46 -0800

    Issue #915 Improve tests by using realistic values for numbers

--- src/middleware/__tests__/analytics.tests.ts
+++ src/middleware/__tests__/analytics.tests.ts
@@ -6,30 +6,30 @@ describe('Analytics memory report middleware helpers', () => {
 
     describe('shouldSendServicesReport()', () => {
 
-        it('returns true when the new services count passes the provided threshold', () => {
-            const oldServicesCount = 0;
-            const newServicesCount = 2;
-            const sendEveryCount = 1;
+        it('returns true when the new services count passes the threshold', () => {
+            const oldServicesCount = 8;
+            const newServicesCount = 12;
+            const sendEveryCount = 10;
             expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(true);
         });
 
-        it('returns true when the new services count equals the provided threshold', () => {
-            const oldServicesCount = 0;
-            const newServicesCount = 1;
-            const sendEveryCount = 1;
+        it('returns true when the new services count equals the threshold', () => {
+            const oldServicesCount = 9;
+            const newServicesCount = 10;
+            const sendEveryCount = 10;
             expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(true);
         });
 
         it('returns false when the new services count equals the old services count', () => {
-            const oldServicesCount = 1;
-            const newServicesCount = 1;
-            const sendEveryCount = aNumber();
+            const oldServicesCount = 9;
+            const newServicesCount = 9;
+            const sendEveryCount = 10;
             expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(false);
         });
 
         it('returns false when the new services count is less than the old services count', () => {
-            const oldServicesCount = 1;
-            const newServicesCount = 0;
+            const oldServicesCount = 12;
+            const newServicesCount = 8;
             const sendEveryCount = aNumber();
             expect(shouldSendServicesReport(oldServicesCount, newServicesCount, sendEveryCount)).toBe(false);
         });
