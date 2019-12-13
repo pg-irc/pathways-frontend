2e08637112093f7dc83b7478a8c95c473011a851
Rasmus Storjohann <rasmus@peacegeeks.org>
2019-12-13 11:18:15 -0800

    Issue #742 Refactor store for onboarding flag

--- src/stores/onboarding/index.ts
+++ src/stores/onboarding/index.ts
@@ -1,11 +1,25 @@
 import * as constants from '../../application/constants';
-import { OnboardingAction } from './actions';
+import * as helpers from '../helpers/make_action';
+import { UserDataPersistence } from '../user_data';
+import { ClearAllUserDataAction } from '../questionnaire/actions';
+
+export type SetOnboardingAction = Readonly<ReturnType<typeof setOnboarding>>;
+
+// tslint:disable-next-line:typedef
+export const setOnboarding = () => (
+    helpers.makeAction(constants.SET_ONBOARDING)
+);
+
+export type OnboardingAction =
+    SetOnboardingAction |
+    ClearAllUserDataAction |
+    UserDataPersistence.LoadSuccessAction;
 
 export interface OnboardingStore {
     readonly showOnboarding: boolean;
 }
 
-export const buildDefaultStore = (): OnboardingStore  => ({
+export const buildDefaultStore = (): OnboardingStore => ({
     showOnboarding: false,
 });
 
@@ -25,10 +39,10 @@ export const reducer = (store: OnboardingStore = buildDefaultStore(), action?: O
                 showOnboarding: action.payload.showOnboarding,
             });
         case constants.CLEAR_ALL_USER_DATA:
-        return ({
-            ...store,
-            showOnboarding: true,
-        });
+            return ({
+                ...store,
+                showOnboarding: true,
+            });
         default:
             return store;
     }
