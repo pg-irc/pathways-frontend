332bea75cf17c2f3697c7dd6f4c4d522e3cd65cd
Tomy Reyes <tomy@peacegeeks.org>
2019-12-19 11:22:21 -0800

    Issue #841 move bookmark header component and update copy

--- src/components/bookmarks/bookmarks_component.tsx
+++ src/components/bookmarks/bookmarks_component.tsx
@@ -5,6 +5,9 @@ import { ListActions } from './bookmarks_connected_component';
 import { TopicBookmarksComponent } from './topic_bookmarks_component';
 import { ServiceBookmarksComponent } from './service_bookmarks_component';
 import { HumanServiceData } from '../../validation/services/types';
+import { View, Text, Content } from 'native-base';
+import { Trans } from '@lingui/react';
+import { colors, textStyles, values } from '../../application/styles';
 
 export interface BookmarksProps {
     readonly bookmarkedServices: ReadonlyArray<HumanServiceData>;
@@ -15,9 +18,21 @@ type Props = BookmarksProps & ListActions & RouterProps ;
 
 export const BookmarksComponent: React.StatelessComponent<Props> = (props: Props): JSX.Element => {
     return (
-        <React.Fragment>
+        <Content style={{ backgroundColor: colors.lightGrey }}>
+            <HeaderComponent />
             <TopicBookmarksComponent {...props} />
             <ServiceBookmarksComponent {...props} />
-        </React.Fragment>
+        </Content>
     );
 };
+
+const HeaderComponent = (): JSX.Element => (
+    <View padder style={{ backgroundColor: colors.white, paddingHorizontal: values.backgroundTextPadding }}>
+    <Text style={textStyles.headlineH1StyleBlackLeft} >
+        <Trans>My bookmarks</Trans>
+    </Text>
+    <Text style={textStyles.paragraphStyle}>
+        <Trans>Save important topics and services to build your personal plan for settlement.</Trans>
+    </Text>
+</View>
+);
--- src/components/bookmarks/topic_bookmarks_component.tsx
+++ src/components/bookmarks/topic_bookmarks_component.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import * as R from 'ramda';
-import { Trans } from '@lingui/react';
-import { Text, View } from 'native-base';
 import { TopicListItem } from '../../selectors/topics/topic_list_item';
 import { TaskListActions, NoTasksAddedComponent, TaskListComponent } from '../topics/task_list_component';
 import { RouterProps } from '../../application/routing';
-import { textStyles, colors, values } from '../../application/styles';
+import { View } from 'native-base';
 
 export interface TopicBookmarksProps {
     readonly bookmarkedTopics: ReadonlyArray<TopicListItem>;
@@ -20,18 +18,7 @@ export const TopicBookmarksComponent: React.StatelessComponent<Props> = (props: 
             tasks={props.bookmarkedTopics}
             savedTasksIdList={R.map((topic: TopicListItem) => topic.id, props.bookmarkedTopics)}
             emptyTaskListContent={<NoTasksAddedComponent />}
-            headerContent={<TaskListHeaderComponent />}
+            headerContent={<View />}
         />
     );
 };
-
-const TaskListHeaderComponent = (): JSX.Element => (
-    <View padder style={{ backgroundColor: colors.white, paddingHorizontal: values.backgroundTextPadding }}>
-        <Text style={textStyles.headlineH1StyleBlackLeft} >
-            <Trans>My bookmarks</Trans>
-        </Text>
-        <Text style={textStyles.paragraphStyle}>
-            <Trans>Save important topics to build your personal plan for settlement.</Trans>
-        </Text>
-    </View>
-);
