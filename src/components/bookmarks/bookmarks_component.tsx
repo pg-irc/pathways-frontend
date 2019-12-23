b96dbd57b5e6c02ca99e36e1affa979a4f00ec80
Tomy Reyes <tomy@peacegeeks.org>
2019-12-23 15:30:17 -0800

    Issue #841 styling to match header mocks

--- src/components/bookmarks/bookmarks_component.tsx
+++ src/components/bookmarks/bookmarks_component.tsx
@@ -4,7 +4,7 @@ import { RouterProps } from '../../application/routing';
 import { ListActions } from './bookmarks_connected_component';
 import { TopicBookmarksComponent } from './topic_bookmarks_component';
 import { HumanServiceData } from '../../validation/services/types';
-import { View, Text, Content, Tabs, Tab, TabHeading } from 'native-base';
+import { View, Text, Content, Tabs, Tab, Container } from 'native-base';
 import { Trans } from '@lingui/react';
 import { colors, textStyles, values } from '../../application/styles';
 import { ServiceBookmarksComponent } from './service_bookmarks_component';
@@ -37,24 +37,25 @@ const HeaderComponent = (): JSX.Element => (
 );
 
 const TabSwitcher = (props: Props): JSX.Element => (
-    <Tabs>
-        <Tab
-            heading={
-                <TabHeading>
-                    <Text>Topics</Text>
-                </TabHeading>
-            }
-            >
-                <TopicBookmarksComponent {...props} />
-            </Tab>
-            <Tab
-                heading={
-                    <TabHeading>
-                        <Text>Services</Text>
-                    </TabHeading>
-                }
-            >
-                <ServiceBookmarksComponent {...props} />
-            </Tab>
+    <View style={{backgroundColor: colors.white}}>
+    <Tabs tabContainerStyle={{elevation: 0, width: '50%', marginHorizontal: 8}} tabBarUnderlineStyle={{width: 0}}
+    >
+        <Tab heading='Topics'
+        textStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+        tabStyle={{backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4}}
+        activeTabStyle={{backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4}}
+        activeTextStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+        >
+            <TopicBookmarksComponent {...props} />
+        </Tab>
+        <Tab heading='Services'
+        textStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+        tabStyle={{backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4}}
+        activeTabStyle={{backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4}}
+        activeTextStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+        >
+            <ServiceBookmarksComponent {...props} />
+        </Tab>
     </Tabs>
+    </View>
 );
