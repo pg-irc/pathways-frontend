557ba891d3d02f595501b085ec6a5cc22a57853e
Tomy Reyes <tomy@peacegeeks.org>
2019-12-24 08:54:54 -0800

    Issue #841 use i18n to localize headings outside of <Trans> tags

--- src/components/bookmarks/bookmarks_component.tsx
+++ src/components/bookmarks/bookmarks_component.tsx
@@ -4,10 +4,11 @@ import { RouterProps } from '../../application/routing';
 import { ListActions } from './bookmarks_connected_component';
 import { TopicBookmarksComponent } from './topic_bookmarks_component';
 import { HumanServiceData } from '../../validation/services/types';
-import { View, Text, Content, Tabs, Tab, Container } from 'native-base';
-import { Trans } from '@lingui/react';
+import { View, Text, Content, Tabs, Tab } from 'native-base';
+import { Trans, I18n } from '@lingui/react';
 import { colors, textStyles, values } from '../../application/styles';
 import { ServiceBookmarksComponent } from './service_bookmarks_component';
+import { ReactI18nRenderProp, ReactI18n } from '../../locale/types';
 
 export interface BookmarksProps {
     readonly bookmarkedServices: ReadonlyArray<HumanServiceData>;
@@ -20,42 +21,67 @@ export const BookmarksComponent: React.StatelessComponent<Props> = (props: Props
     return (
         <Content style={{ backgroundColor: colors.lightGrey }}>
             <HeaderComponent/>
-            <TabSwitcher {...props}/>
+            <I18n>
+                {(i18nRenderProp: ReactI18nRenderProp): JSX.Element => (
+                    <TabSwitcher i18n={i18nRenderProp.i18n} {...props}/>
+                )}
+            </I18n>
         </Content>
     );
 };
 
 const HeaderComponent = (): JSX.Element => (
     <View padder style={{ backgroundColor: colors.white, paddingHorizontal: values.backgroundTextPadding }}>
-    <Text style={textStyles.headlineH1StyleBlackLeft} >
-        <Trans>My bookmarks</Trans>
-    </Text>
-    <Text style={textStyles.paragraphStyle}>
-        <Trans>Save important topics and services to build your personal plan for settlement.</Trans>
-    </Text>
-</View>
+        <Text style={textStyles.headlineH1StyleBlackLeft} >
+            <Trans>My bookmarks</Trans>
+        </Text>
+        <Text style={textStyles.paragraphStyle}>
+            <Trans>Save important topics and services to build your personal plan for settlement.</Trans>
+        </Text>
+    </View>
 );
 
-const TabSwitcher = (props: Props): JSX.Element => (
-    <View style={{backgroundColor: colors.white}}>
-    <Tabs tabContainerStyle={{elevation: 0, width: '50%', marginHorizontal: 8}} tabBarUnderlineStyle={{width: 0}}
-    >
-        <Tab heading='Topics'
-        textStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
-        tabStyle={{backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4}}
-        activeTabStyle={{backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4}}
-        activeTextStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
-        >
-            <TopicBookmarksComponent {...props} />
-        </Tab>
-        <Tab heading='Services'
-        textStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
-        tabStyle={{backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4}}
-        activeTabStyle={{backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4}}
-        activeTextStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
-        >
-            <ServiceBookmarksComponent {...props} />
-        </Tab>
-    </Tabs>
+export interface TabSwitcherProps {
+    readonly i18n: ReactI18n;
+}
+
+const TabSwitcher = (props: Props & TabSwitcherProps): JSX.Element => {
+    const _ = props.i18n._.bind(props.i18n);
+    const topicTabHeading = _('Topics');
+    const serviceTabHeading = _('Services');
+    return (
+        <View style={{backgroundColor: colors.white}}>
+            <Tabs tabContainerStyle={{elevation: 0, width: '50%', marginHorizontal: 8}} tabBarUnderlineStyle={{width: 0}}>
+                <Tab heading={topicTabHeading}
+                textStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+                tabStyle={{backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4}}
+                activeTabStyle={{backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4}}
+                activeTextStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+                >
+                    <TopicBookmarksComponent {...props} />
+                </Tab>
+                <Tab heading={serviceTabHeading}
+                textStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+                tabStyle={{backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4}}
+                activeTabStyle={{backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4}}
+                activeTextStyle={[textStyles.headlineH4StyleBlackLeft, {fontWeight: 'bold'}]}
+                >
+                    <ServiceBookmarksComponent {...props} />
+                </Tab>
+            </Tabs>
+    </View>
+    );
+};
+
+// This is here to be able to extract heading strings with yarn extract-strings-clean.
+// These strings must match the strings within the TabSwitcher component.
+export const extractBookmarkComponentStrings = (): JSX.Element => (
+    <View>
+        <Text>
+            <Trans>Topics</Trans>
+        </Text>
+        <Text>
+            <Trans>Services</Trans>
+        </Text>
     </View>
 );
