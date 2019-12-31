7718ab0813340747b2581aab41cb9aa47c6f4bc0
Tomy Reyes <tomy@peacegeeks.org>
2019-12-30 17:15:46 -0800

    Issue #841 get react-native-tab-view working with similar styling

--- src/components/bookmarks/bookmarks_component.tsx
+++ src/components/bookmarks/bookmarks_component.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { TopicListItem } from '../../selectors/topics/topic_list_item';
 import { RouterProps } from '../../application/routing';
 import { ListActions } from './bookmarks_connected_component';
 import { TopicBookmarksComponent } from './topic_bookmarks_component';
 import { HumanServiceData } from '../../validation/services/types';
-import { View, Text, Content, Tabs, Tab } from 'native-base';
+import { View, Text, Content } from 'native-base';
 import { Trans, I18n } from '@lingui/react';
-import { colors, textStyles, values } from '../../application/styles';
+import { colors, textStyles, values, getNormalFontFamily } from '../../application/styles';
 import { ServiceBookmarksComponent } from './service_bookmarks_component';
 import { ReactI18nRenderProp, ReactI18n } from '../../locale/types';
-import { StyleSheet } from 'react-native';
+import { TabView, SceneMap, TabBar, SceneRendererProps, NavigationState, Route } from 'react-native-tab-view';
+import { Dimensions } from 'react-native';
 
 export interface BookmarksProps {
     readonly bookmarkedServices: ReadonlyArray<HumanServiceData>;
@@ -46,40 +47,64 @@ export interface TabSwitcherProps {
     readonly i18n: ReactI18n;
 }
 
+// navigation state requires routes to not be a ReadonlyArray
+// tslint:disable-next-line: readonly-array
+export type TabRoutes = Array<Route>;
+
 const TabSwitcher = (props: Props & TabSwitcherProps): JSX.Element => {
     const _ = props.i18n._.bind(props.i18n);
-    const topicTabHeading = _('Topics');
-    const serviceTabHeading = _('Services');
-    return (
+    const topicTabTitle = _('Topics');
+    const serviceTabTitle = _('Services');
+    const initialRoutes: TabRoutes = [
+        // key must be 'first', 'second', 'third' etc
+        { key: 'first', title: topicTabTitle },
+        { key: 'second', title: serviceTabTitle },
+    ];
+    const [index, setIndex]: [number, (n: number) => void] = React.useState(0);
+    const [routes]: [TabRoutes, Dispatch<SetStateAction<TabRoutes>>] = React.useState(initialRoutes);
+
+    // recommended to define components outside of renderScene to avoid unecessary re-renders
+    const FirstRoute = (): JSX.Element => (
         <View style={{backgroundColor: colors.white}}>
-            <Tabs tabContainerStyle={{elevation: 0, width: '50%', marginHorizontal: 8}} tabBarUnderlineStyle={{width: 0}} scrollWithoutAnimation>
-                <Tab heading={topicTabHeading} {...tabStyle}>
-                    <TopicBookmarksComponent {...props} />
-                </Tab>
-                <Tab heading={serviceTabHeading} {...tabStyle}>
-                    <ServiceBookmarksComponent {...props} />
-                </Tab>
-            </Tabs>
-    </View>
+            <TopicBookmarksComponent {...props} />
+        </View>
+    );
+
+    const SecondRoute = (): JSX.Element => (
+        <View style={{backgroundColor: colors.white}}>
+            <ServiceBookmarksComponent {...props} />
+            </View>
+        );
+
+    const renderScene = SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+    });
+
+    const renderTabBar = (tabBarProps: SceneRendererProps  & {
+        readonly navigationState: NavigationState<Route>}): JSX.Element => (
+        <TabBar
+        {...tabBarProps}
+        style={{ backgroundColor: colors.white, width: '55%', elevation: 0, marginHorizontal: 8 }}
+        indicatorStyle={{ backgroundColor: colors.teal, height: 4 }}
+        getLabelText={({ route }: { readonly route: Route}): string => route.title}
+        labelStyle={{color: colors.black, fontWeight: 'bold', fontFamily: getNormalFontFamily()}}
+        />
+    );
+
+    return (
+        <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={setIndex}
+        style={{backgroundColor: colors.white}}
+        // recommended to pass initialLayout to improve render performance
+        initialLayout={{ width: Dimensions.get('window').width }}
+        sceneContainerStyle={{backgroundColor: colors.lightGrey}}
+        />
     );
 };
-
-const tabStyle = StyleSheet.create({
-    textStyle: {
-        ...textStyles.headlineH4StyleBlackLeft,
-        fontWeight: 'bold',
-    },
-    tabStyle: {
-        backgroundColor: colors.white, borderBottomColor: colors.white, borderBottomWidth: 4,
-    },
-    activeTabStyle: {
-        backgroundColor: colors.white, borderBottomColor: colors.teal, borderBottomWidth: 4,
-    },
-    activeTextStyle: {
-        ...textStyles.headlineH4StyleBlackLeft,
-        fontWeight: 'bold',
-    },
-  });
 
 // This is here to be able to extract heading strings with yarn extract-strings-clean.
 // These strings must match the strings within the TabSwitcher component.
