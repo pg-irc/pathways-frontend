dfc9cf6e6aa04e1401970bea0f9150a777d89aed
Tomy Reyes <tomy@peacegeeks.org>
2020-01-16 11:12:02 -0800

    Issue #860 add EmptyListComponent to topic bookmarks screen

--- src/components/bookmarks/topic_bookmarks_component.tsx
+++ src/components/bookmarks/topic_bookmarks_component.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import * as R from 'ramda';
 import { TopicListItem } from '../../selectors/topics/types';
-import { TaskListActions, NoTasksAddedComponent, TaskListComponent } from '../topics/task_list_component';
+import { TaskListActions, TaskListComponent } from '../topics/task_list_component';
 import { RouterProps } from '../../application/routing';
 import { View } from 'native-base';
 import { colors } from '../../application/styles';
+import { EmptyListComponent } from '../empty_component/empty_list_component';
+import { emptyList } from '../../application/images';
+import { Trans } from '@lingui/react';
 
 export interface TopicBookmarksProps {
     readonly bookmarkedTopics: ReadonlyArray<TopicListItem>;
@@ -19,7 +22,7 @@ export const TopicBookmarksComponent: React.StatelessComponent<Props> = (props: 
                 {...props}
                 tasks={props.bookmarkedTopics}
                 savedTasksIdList={R.map((topic: TopicListItem) => topic.id, props.bookmarkedTopics)}
-                emptyTaskListContent={<NoTasksAddedComponent />}
+                emptyTaskListContent={<EmptyListComponent title={<Trans>No topics to show</Trans>} imageSource={emptyList} />}
                 headerContent={<View />}
             />
         </View>
