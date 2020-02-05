5d6116c01ce086bcd7d4f2c6b1c9ae093a468e44
Tomy Reyes <tomy@peacegeeks.org>
2020-02-05 12:16:30 -0800

    Issue #724 clean up phone component

--- src/components/phone_numbers/phone_numbers_component.tsx
+++ src/components/phone_numbers/phone_numbers_component.tsx
@@ -9,6 +9,7 @@ import { DividerComponent } from '../content_layout/divider_component';
 import { textStyles, colors, values } from '../../application/styles';
 import { openURL, LinkTypes } from '../link/link';
 import { AnalyticsLinkPressedAction } from '../../stores/analytics';
+import * as R from 'ramda';
 
 interface Props {
     readonly phoneNumbers: ReadonlyArray<PhoneNumber>;
@@ -20,40 +21,30 @@ interface Props {
 export const PhoneNumbersComponent = (props: Props): JSX.Element => {
     return (
         <View>
-            {mapWithIndex((phoneNumber: PhoneNumber, index: number): JSX.Element =>
-                <PhoneNumberCardComponent {...props} phoneNumber={phoneNumber} index={index} />
-            , props.phoneNumbers)}
+            {mapWithIndex(buildPhoneNumber(props), props.phoneNumbers)}
         </View>
     );
 };
 
-export interface PhoneNumberCardComponentProps {
-    readonly index: number;
-    readonly phoneNumber: PhoneNumber;
-    readonly linkContextForAnalytics: string;
-    readonly currentPathForAnalytics: string;
-    readonly analyticsLinkPressed: (currentPath: string, linkContext: string, linkType: string, linkValue: string) => AnalyticsLinkPressedAction;
-}
-
-const PhoneNumberCardComponent = (props: PhoneNumberCardComponentProps): JSX.Element => {
+const buildPhoneNumber = R.curry((props: Props, phoneNumber: PhoneNumber, index: number): JSX.Element => {
     const onPress = (): void => {
-        const linkValue = 'tel: ' + props.phoneNumber.phone_number;
+        const linkValue = 'tel: ' + phoneNumber.phone_number;
         props.analyticsLinkPressed(props.currentPathForAnalytics, props.linkContextForAnalytics, LinkTypes.phone, linkValue);
         openURL(linkValue);
     };
     return (
-        <View key={props.index}>
+        <View key={index}>
             <CardButtonComponent
-                leftContent={renderPhoneNumber(props.phoneNumber)}
+                leftContent={renderSinglePhoneNumber(phoneNumber)}
                 rightContent={renderIcon()}
                 onPress={onPress}
             />
             <DividerComponent />
         </View>
     );
-};
+});
 
-const renderPhoneNumber = (phoneNumber: PhoneNumber): JSX.Element => {
+const renderSinglePhoneNumber = (phoneNumber: PhoneNumber): JSX.Element => {
     const capitalizeFirstLetter = (s: string): string => (
         s.charAt(0).toUpperCase() + s.slice(1)
     );
