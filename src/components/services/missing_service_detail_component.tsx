bf121249bf1a0954d875cd462d4da826cc90003c
Tomy Reyes <tomy@peacegeeks.org>
2020-03-02 12:34:11 -0800

    Issue #995 create missing service detail component

---
+++ src/components/services/missing_service_detail_component.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { textStyles, colors } from '../../application/styles';
+import { DividerComponent } from '../content_layout/divider_component';
+
+interface Props {
+    readonly title: JSX.Element;
+}
+
+export const MissingServiceDetailComponent = (props: Props): JSX.Element => {
+    return (
+        <View>
+            <Text style={[textStyles.paragraphBoldBlackLeft, { color: colors.darkerGrey }]}>
+                {props.title}:
+            </Text>
+            <DividerComponent />
+        </View>
+    );
+};
