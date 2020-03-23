65a6a76211b13365c3dbb1fc43028230f8788296
Tomy Reyes <tomy@peacegeeks.org>
2020-03-23 09:59:45 -0700

    Issue #1024 refactor main component away from class component

--- src/components/main/main_component.tsx
+++ src/components/main/main_component.tsx
@@ -1,5 +1,5 @@
-// tslint:disable:no-class no-this no-expression-statement
-import React from 'react';
+// tslint:disable:no-expression-statement
+import React, { SetStateAction, Dispatch, useState, useEffect } from 'react';
 import { Container, Drawer, Root } from 'native-base';
 import { HeaderConnectedComponent } from './header_connected_component';
 import { MainPageSwitcherComponent } from './main_page_switcher';
@@ -30,106 +30,63 @@ interface MainProps {
 
 type Props = MainComponentProps & MainComponentActions;
 
-interface State {
-    readonly isHeaderMenuShowing: boolean;
-    readonly isAboutModalOpen: boolean;
-    readonly isDisclaimerModalOpen: boolean;
-}
+type BooleanSetterFunction = Dispatch<SetStateAction<boolean>>;
 
-export class MainComponent extends React.Component<Props, State> {
+export const MainComponent = (props: Props): JSX.Element => {
+    const [isHeaderMenuVisible, setIsHeaderMenuVisible]: readonly [boolean, BooleanSetterFunction] = useState(false);
+    const [isAboutModalVisible, setIsAboutModalVisible]: readonly [boolean, BooleanSetterFunction] = useState(false);
+    const [isDisclaimerModalVisible, setIsDisclaimerModalVisible]: readonly [boolean, BooleanSetterFunction] = useState(false);
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            isHeaderMenuShowing: false,
-            isAboutModalOpen: false,
-            isDisclaimerModalOpen: false,
-        };
-        this.openDrawer = this.openDrawer.bind(this);
-        this.closeDrawer = this.closeDrawer.bind(this);
-        this.onHardwareBackButtonPress = this.onHardwareBackButtonPress.bind(this);
-        this.openAboutModal = this.openAboutModal.bind(this);
-        this.openDisclaimerModal = this.openDisclaimerModal.bind(this);
-        this.closeAboutModal = this.closeAboutModal.bind(this);
-        this.closeDisclaimerModal = this.closeDisclaimerModal.bind(this);
-    }
-
-    componentDidMount(): void {
-        this.props.history.listen((location: Location, _: Action) =>
-            this.props.sendAnalyticsData(location, this.props.locale),
+    useEffect((): void => {
+        props.history.listen((location: Location, _: Action): RouteChangedAction =>
+            props.sendAnalyticsData(location, props.locale),
         );
-        Notifications.addListener(notificationListener(this.props.history));
-    }
+        Notifications.addListener(notificationListener(props.history));
+    }, []);
 
-    render(): JSX.Element {
-        return (
-            <Root>
-                <Drawer
-                    side='right'
-                    onClose={this.closeDrawer}
-                    open={this.state.isHeaderMenuShowing}
-                    content={
-                        <HeaderMenuConnectedComponent
-                            history={this.props.history}
-                            closeMenu={this.closeDrawer}
-                            openAboutModal={this.openAboutModal}
-                            openDisclaimerModal={this.openDisclaimerModal}
-                        />
-                    }
-                >
-                    <Container>
-                        <HeaderConnectedComponent
-                            history={this.props.history}
-                            location={this.props.location}
-                            openMenu={this.openDrawer}
-                            closeAboutModal={this.closeAboutModal}
-                            closeDisclaimerModal={this.closeDisclaimerModal}
-                        />
-                        <MainPageSwitcherComponent {...this.props} />
-                        <FooterComponent {...this.props} />
-                        <HardwareBackButtonHandlerComponent onHardwareBackButtonPress={this.onHardwareBackButtonPress} />
-                        <AppModalsComponent
-                            isAboutVisible={this.state.isAboutModalOpen}
-                            isDisclaimerVisible={this.state.isDisclaimerModalOpen}
-                            closeAboutModal={this.closeAboutModal}
-                            closeDisclaimerModal={this.closeDisclaimerModal}
-                        />
-                    </Container>
-                </Drawer>
-            </Root>
-        );
-    }
-
-    closeDrawer(): void {
-        this.setState({ isHeaderMenuShowing: false });
-    }
-
-    openDrawer(): void {
-        this.setState({ isHeaderMenuShowing: true });
-    }
-
-    closeAboutModal(): void {
-        this.setState({ isAboutModalOpen: false });
-    }
-
-    openAboutModal(): void {
-        this.setState({ isAboutModalOpen: true });
-    }
-
-    closeDisclaimerModal(): void {
-        this.setState({ isDisclaimerModalOpen: false });
-    }
-
-    openDisclaimerModal(): void {
-        this.setState({ isDisclaimerModalOpen: true });
-    }
-
-    onHardwareBackButtonPress(): boolean {
-        if (this.state.isHeaderMenuShowing) {
-            this.closeDrawer();
+    const onHardwareBackButtonPress = (): boolean => {
+        if (!isHeaderMenuVisible) {
+            goBack(props.history);
         } else {
-            goBack(this.props.history);
+            setIsHeaderMenuVisible(false);
         }
         return true;
-    }
-}
+    };
+
+    return (
+        <Root>
+            <Drawer
+                side='right'
+                onClose={(): void => setIsHeaderMenuVisible(false)}
+                open={isHeaderMenuVisible}
+                content={
+                    <HeaderMenuConnectedComponent
+                        history={props.history}
+                        closeMenu={(): void => setIsHeaderMenuVisible(false)}
+                        openAboutModal={(): void => setIsAboutModalVisible(true)}
+                        openDisclaimerModal={(): void => setIsDisclaimerModalVisible(true)}
+                    />
+                }
+            >
+                <Container>
+                    <HeaderConnectedComponent
+                        history={props.history}
+                        location={props.location}
+                        openMenu={(): void => setIsHeaderMenuVisible(true)}
+                        closeAboutModal={(): void => setIsAboutModalVisible(false)}
+                        closeDisclaimerModal={(): void => setIsDisclaimerModalVisible(false)}
+                    />
+                    <MainPageSwitcherComponent {...props} />
+                    <FooterComponent {...props} />
+                    <HardwareBackButtonHandlerComponent onHardwareBackButtonPress={onHardwareBackButtonPress} />
+                    <AppModalsComponent
+                        isAboutVisible={isAboutModalVisible}
+                        isDisclaimerVisible={isDisclaimerModalVisible}
+                        closeAboutModal={(): void => setIsAboutModalVisible(false)}
+                        closeDisclaimerModal={(): void => setIsDisclaimerModalVisible(false)}
+                    />
+                </Container>
+            </Drawer>
+        </Root>
+    );
+};
