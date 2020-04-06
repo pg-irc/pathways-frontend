0976320dad3164349aa2ab6225e170fc34674ffe
Tomy Reyes <tomy@peacegeeks.org>
2020-04-06 09:04:03 -0700

    Issue #1096 adjust status bar logic to check if app is using expo or standalone

--- src/components/main/get_status_bar_height_for_platform.ts
+++ src/components/main/get_status_bar_height_for_platform.ts
@@ -1,5 +1,13 @@
 import { Platform, StatusBar } from 'react-native';
+import Constants, { AppOwnership } from 'expo-constants';
 
-export const getStatusBarHeightForPlatform = (): number => (
-    Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+export const getStatusBarHeightForPlatform = (): number => {
+    if (Platform.OS === 'ios') {
+        return 0;
+    }
+    return isStandaloneAndroidBuild() ? StatusBar.currentHeight : 0;
+};
+
+const isStandaloneAndroidBuild = (): boolean => (
+    Constants.appOwnership === AppOwnership.Standalone
 );
