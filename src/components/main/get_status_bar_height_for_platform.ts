2dadf1bb0c87fce7eedcae6847708354574d8bf5
Tomy Reyes <tomy@peacegeeks.org>
2020-04-07 16:48:14 -0700

    Issue #1096 address PR comments on platform checking consistency

--- src/components/main/get_status_bar_height_for_platform.ts
+++ src/components/main/get_status_bar_height_for_platform.ts
@@ -1,10 +1,11 @@
-import { Platform, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import Constants, { AppOwnership } from 'expo-constants';
+import { isAndroid } from '../../application/helpers/is_android';
 
 export const getStatusBarHeightForPlatform = (): number => (
     isStandaloneAndroidBuild() ? StatusBar.currentHeight : 0
 );
 
 const isStandaloneAndroidBuild = (): boolean => (
-    Platform.OS === 'android' && Constants.appOwnership === AppOwnership.Standalone
+    isAndroid() && Constants.appOwnership === AppOwnership.Standalone
 );
