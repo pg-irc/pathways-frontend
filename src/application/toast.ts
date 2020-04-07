96a7b876dcb87849f0b74809c88f3a1050656083
Ben St John <ben@affinitybridge.com>
2020-04-07 09:39:36 -0700

    Issue #985 Address PR review

--- src/application/toast.ts
+++ src/application/toast.ts
@@ -3,13 +3,10 @@ import { textStyles, applicationStyles } from './styles';
 import { Toast } from 'native-base';
 
 export const showToast = (text: string): void => {
-    const toastProps = {
+    Toast.show({
         style: applicationStyles.toast,
         textStyle: textStyles.toast,
         duration: 2000,
-    };
-    Toast.show({
-        ...toastProps,
         text,
     });
 };
