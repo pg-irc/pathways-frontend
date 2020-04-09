c7b4f7ebeea39f490ed4fff5fc0f236988e35c7e
rasmus <rasmus@peacegeeks.org>
2020-04-09 13:05:55 -0700

    Issue #985 Navigate to the screen for receiving updates

--- src/stores/feedback/actions.ts
+++ src/stores/feedback/actions.ts
@@ -6,6 +6,7 @@ export enum FeedbackScreen {
     EditableServiceDetailPage,
     RemoveServicePage,
     OtherChangesPage,
+    ReceiveUpdatesModal,
 }
 
 // tslint:disable-next-line:typedef
@@ -19,22 +20,26 @@ const SUGGEST_AN_UPDATE = 'SUGGEST_AN_UPDATE';
 const CHOOSE_CHANGE_NAME_AND_DETAILS = 'CHOOSE_CHANGE_NAME_AND_DETAILS';
 const CHOOSE_REMOVE_SERVICE = 'CHOOSE_REMOVE_SERVICE';
 const CHOOSE_OTHER_CHANGES = 'CHOOSE_OTHER_CHANGES';
+const SUBMIT = 'SUBMIT';
 
 // tslint:disable: typedef
 export const suggestAnUpdate = () => helpers.makeAction(SUGGEST_AN_UPDATE);
 export const chooseChangeNameOrDetails = () => helpers.makeAction(CHOOSE_CHANGE_NAME_AND_DETAILS);
 export const chooseRemoveService = () => helpers.makeAction(CHOOSE_REMOVE_SERVICE);
 export const chooseOtherChanges = () => helpers.makeAction(CHOOSE_OTHER_CHANGES);
+export const submit = () => helpers.makeAction(SUBMIT);
 
 export type SuggestAnUpdateAction = Readonly<ReturnType<typeof suggestAnUpdate>>;
 export type ChooseChangeNameOrDetailsAction = Readonly<ReturnType<typeof chooseChangeNameOrDetails>>;
 export type ChooseRemoveServiceAction = Readonly<ReturnType<typeof chooseRemoveService>>;
 export type ChooseOtherChangesAction = Readonly<ReturnType<typeof chooseOtherChanges>>;
+export type SubmitAction = Readonly<ReturnType<typeof submit>>;
 
 type ReducerActions = SuggestAnUpdateAction |
         ChooseChangeNameOrDetailsAction |
         ChooseRemoveServiceAction |
-        ChooseOtherChangesAction;
+        ChooseOtherChangesAction |
+        SubmitAction;
 
 export const reduce = (store: FeedbackStore = buildDefaultStore(), action?: ReducerActions): FeedbackStore => {
     if (!action) {
@@ -49,6 +54,8 @@ export const reduce = (store: FeedbackStore = buildDefaultStore(), action?: Redu
             return { screen: FeedbackScreen.RemoveServicePage };
         case CHOOSE_OTHER_CHANGES:
             return { screen: FeedbackScreen.OtherChangesPage };
+        case SUBMIT:
+            return { screen: FeedbackScreen.ReceiveUpdatesModal };
         default:
             return store;
     }
