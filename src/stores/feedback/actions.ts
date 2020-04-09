514cfca6413bfc8e57fbf757eaf72158e3836f07
rasmus <rasmus@peacegeeks.org>
2020-04-09 12:55:30 -0700

    Issue #985 Navigate to remove service screen

--- src/stores/feedback/actions.ts
+++ src/stores/feedback/actions.ts
@@ -4,6 +4,7 @@ export enum FeedbackScreen {
     ServiceDetail,
     ChooseFeedbackModeModal,
     EditableServiceDetailPage,
+    RemoveServicePage,
 }
 
 // tslint:disable-next-line:typedef
@@ -15,18 +16,22 @@ export type FeedbackStore = Readonly<ReturnType<typeof buildDefaultStore>>;
 
 const SUGGEST_AN_UPDATE = 'SUGGEST_AN_UPDATE';
 const CHOOSE_CHANGE_NAME_AND_DETAILS = 'CHOOSE_CHANGE_NAME_AND_DETAILS';
+const CHOOSE_REMOVE_SERVICE = 'CHOOSE_REMOVE_SERVICE';
 
 // tslint:disable: typedef
 export const suggestAnUpdate = () => helpers.makeAction(SUGGEST_AN_UPDATE);
 export const chooseChangeNameOrDetails = () => helpers.makeAction(CHOOSE_CHANGE_NAME_AND_DETAILS);
+export const chooseRemoveService = () => helpers.makeAction(CHOOSE_REMOVE_SERVICE);
 
 export type SuggestAnUpdateAction = Readonly<ReturnType<typeof suggestAnUpdate>>;
 export type ChooseChangeNameOrDetailsAction = Readonly<ReturnType<typeof chooseChangeNameOrDetails>>;
+export type ChooseRemoveServiceAction = Readonly<ReturnType<typeof chooseRemoveService>>;
 
-type Action = SuggestAnUpdateAction |
-    ChooseChangeNameOrDetailsAction;
+type ReducerActions = SuggestAnUpdateAction |
+        ChooseChangeNameOrDetailsAction |
+        ChooseRemoveServiceAction;
 
-export const reduce = (store: FeedbackStore = buildDefaultStore(), action?: Action): FeedbackStore => {
+export const reduce = (store: FeedbackStore = buildDefaultStore(), action?: ReducerActions): FeedbackStore => {
     if (!action) {
         return store;
     }
@@ -35,6 +40,8 @@ export const reduce = (store: FeedbackStore = buildDefaultStore(), action?: Acti
             return { screen: FeedbackScreen.ChooseFeedbackModeModal };
         case CHOOSE_CHANGE_NAME_AND_DETAILS:
             return {screen: FeedbackScreen.EditableServiceDetailPage};
+        case CHOOSE_REMOVE_SERVICE:
+            return {screen: FeedbackScreen.RemoveServicePage};
         default:
             return store;
     }
