1fc737254e20e250404ba63af0dcc189d08d764a
rasmus <rasmus@peacegeeks.org>
2020-04-09 12:46:35 -0700

    Issue #985 Navigate to editable service screen

--- src/stores/feedback/actions.ts
+++ src/stores/feedback/actions.ts
@@ -3,6 +3,7 @@ import * as helpers from '../helpers/make_action';
 export enum FeedbackScreen {
     ServiceDetail,
     ChooseFeedbackModeModal,
+    EditableServiceDetailPage,
 }
 
 // tslint:disable-next-line:typedef
@@ -13,14 +14,28 @@ export const buildDefaultStore = () => ({
 export type FeedbackStore = Readonly<ReturnType<typeof buildDefaultStore>>;
 
 const SUGGEST_AN_UPDATE = 'SUGGEST_AN_UPDATE';
+const CHOOSE_CHANGE_NAME_AND_DETAILS = 'CHOOSE_CHANGE_NAME_AND_DETAILS';
 
 // tslint:disable: typedef
 export const suggestAnUpdate = () => helpers.makeAction(SUGGEST_AN_UPDATE);
+export const chooseChangeNameOrDetails = () => helpers.makeAction(CHOOSE_CHANGE_NAME_AND_DETAILS);
 
 export type SuggestAnUpdateAction = Readonly<ReturnType<typeof suggestAnUpdate>>;
+export type ChooseChangeNameOrDetailsAction = Readonly<ReturnType<typeof chooseChangeNameOrDetails>>;
 
-type Action = SuggestAnUpdateAction;
+type Action = SuggestAnUpdateAction |
+    ChooseChangeNameOrDetailsAction;
 
-export const reduce = (__: FeedbackStore = buildDefaultStore(), _?: Action): FeedbackStore => {
-    return { screen: FeedbackScreen.ChooseFeedbackModeModal };
+export const reduce = (store: FeedbackStore = buildDefaultStore(), action?: Action): FeedbackStore => {
+    if (!action) {
+        return store;
+    }
+    switch (action.type) {
+        case SUGGEST_AN_UPDATE:
+            return { screen: FeedbackScreen.ChooseFeedbackModeModal };
+        case CHOOSE_CHANGE_NAME_AND_DETAILS:
+            return {screen: FeedbackScreen.EditableServiceDetailPage};
+        default:
+            return store;
+    }
 };
