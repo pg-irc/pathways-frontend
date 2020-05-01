ada0d070695e4b24da3317851d690fbaaaa88e10
niko-pg <niko@peacegeeks.org>
2020-04-30 18:57:49 -0700

    Issue #958 enforce useEffect listeners convention

--- src/components/main/use_hardware_back_button_press.ts
+++ src/components/main/use_hardware_back_button_press.ts
@@ -1,11 +1,20 @@
+
+// tslint:disable: no-expression-statement
 import { EffectCallback, useEffect } from 'react';
 import { BackHandler } from 'react-native';
 
 export const useHardwareBackButtonPress = (onHardwareBackPress: () => boolean): void => {
     useEffect((): EffectCallback => {
-        // tslint:disable-next-line: no-expression-statement
-        const backHandler = BackHandler.addEventListener('hardwareBackPress', onHardwareBackPress);
+        const addListener = (): void => {
+            BackHandler.addEventListener('hardwareBackPress', onHardwareBackPress);
+        };
 
-        return backHandler.remove;
+        const removeListener = (): void => {
+            BackHandler.removeEventListener('hardwareBackPress', onHardwareBackPress);
+        };
+
+        addListener();
+
+        return removeListener;
     }, []);
 };
