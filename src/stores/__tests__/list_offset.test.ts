5d90621c9955d5bf554c2949f1e5937c805facea
Tomy Reyes <tomy@peacegeeks.org>
2020-06-02 12:20:44 -0700

    Issue #1106 create individual offset states for the different services list to improve the ux of navigating between lists

---
+++ src/stores/__tests__/list_offset.test.ts
@@ -0,0 +1,46 @@
+// tslint:disable:no-expression-statement
+import { buildDefaultStore, reducer, saveListOffset, saveSearchOffset, saveTopicServicesOffset, saveBookmarkedServicesOffset } from '../list_offset';
+import { aNumber } from '../../application/helpers/random_test_values';
+
+describe('the list offset reducer', () => {
+
+    describe('the list offset state', () => {
+
+        it('is set a number value by the save list offset action', () => {
+            const oldStore = buildDefaultStore();
+            const offset = aNumber();
+            const newStore = reducer(oldStore, saveListOffset(offset));
+            expect(newStore.listOffset).toBe(offset);
+        });
+    });
+
+    describe('the search offset state', () => {
+
+        it('is set to an offset number value by the save search offset action', () => {
+            const oldStore = buildDefaultStore();
+            const offset = aNumber();
+            const newStore = reducer(oldStore, saveSearchOffset(offset));
+            expect(newStore.searchOffset).toBe(offset);
+        });
+    });
+
+    describe('the topic services offset state', () => {
+
+        it('is set to an offset number value by the save topic services offset action', () => {
+            const oldStore = buildDefaultStore();
+            const offset = aNumber();
+            const newStore = reducer(oldStore, saveTopicServicesOffset(offset));
+            expect(newStore.topicServicesOffset).toBe(offset);
+        });
+    });
+
+    describe('the bookmarked services offset state', () => {
+
+        it('is set to an offset number value by the save bookmarked services offset action', () => {
+            const oldStore = buildDefaultStore();
+            const offset = aNumber();
+            const newStore = reducer(oldStore, saveBookmarkedServicesOffset(offset));
+            expect(newStore.bookmarkedServicesOffset).toBe(offset);
+        });
+    });
+});
--- src/stores/list_offset.ts
+++ src/stores/list_offset.ts
@@ -3,12 +3,23 @@ import * as helpers from './helpers/make_action';
 
 export interface ListOffsetStore {
     readonly listOffset: number;
+    readonly searchOffset: number;
+    readonly topicServicesOffset: number;
+    readonly bookmarkedServicesOffset: number;
 }
 
 export type SaveListOffsetAction = Readonly<ReturnType<typeof saveListOffset>>;
 export type ClearListOffsetAction = Readonly<ReturnType<typeof clearListOffset>>;
+export type SaveSearchOffsetAction = Readonly<ReturnType<typeof saveSearchOffset>>;
+export type SaveTopicServicesOffsetAction = Readonly<ReturnType<typeof saveTopicServicesOffset>>;
+export type SaveBookmarkedServicesOffsetAction = Readonly<ReturnType<typeof saveBookmarkedServicesOffset>>;
 
-export type ListOffsetAction = SaveListOffsetAction | ClearListOffsetAction;
+export type ListOffsetAction =
+SaveListOffsetAction |
+ClearListOffsetAction |
+SaveSearchOffsetAction |
+SaveTopicServicesOffsetAction |
+SaveBookmarkedServicesOffsetAction;
 
 // tslint:disable-next-line:typedef
 export const saveListOffset = (offset: number) => (
@@ -20,8 +31,26 @@ export const clearListOffset = () => (
     helpers.makeAction(constants.CLEAR_LIST_OFFSET)
 );
 
+// tslint:disable-next-line:typedef
+export const saveSearchOffset = (offset: number) => (
+    helpers.makeAction(constants.SAVE_SEARCH_OFFSET, { offset })
+);
+
+// tslint:disable-next-line:typedef
+export const saveTopicServicesOffset = (offset: number) => (
+    helpers.makeAction(constants.SAVE_TOPIC_SERVICES_OFFSET, { offset })
+);
+
+// tslint:disable-next-line:typedef
+export const saveBookmarkedServicesOffset = (offset: number) => (
+    helpers.makeAction(constants.SAVE_BOOKMARKED_SERVICES_OFFSET, { offset })
+);
+
 export const buildDefaultStore = (): ListOffsetStore => ({
     listOffset: 0,
+    searchOffset: 0,
+    topicServicesOffset: 0,
+    bookmarkedServicesOffset: 0,
 });
 
 export const reducer = (store: ListOffsetStore = buildDefaultStore(), action?: ListOffsetAction): ListOffsetStore => {
@@ -36,6 +65,21 @@ export const reducer = (store: ListOffsetStore = buildDefaultStore(), action?: L
             });
         case constants.CLEAR_LIST_OFFSET:
             return buildDefaultStore();
+        case constants.SAVE_SEARCH_OFFSET:
+            return ({
+                ...store,
+                searchOffset: action.payload.offset,
+            });
+        case constants.SAVE_TOPIC_SERVICES_OFFSET:
+            return ({
+                ...store,
+                topicServicesOffset: action.payload.offset,
+            });
+        case constants.SAVE_BOOKMARKED_SERVICES_OFFSET:
+            return ({
+                ...store,
+                bookmarkedServicesOffset: action.payload.offset,
+            });
         default:
             return store;
     }
