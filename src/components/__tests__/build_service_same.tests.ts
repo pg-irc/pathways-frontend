b14405f705f790d3e0dbc3e3a6c43f8e66eee33f
rasmus <rasmus@peacegeeks.org>
2020-07-29 08:53:12 -0700

    Issue #1214 Fix strings in tests

--- src/components/__tests__/build_service_same.tests.ts
+++ src/components/__tests__/build_service_same.tests.ts
@@ -2,14 +2,14 @@
 import { aString } from '../../application/helpers/random_test_values';
 import { buildServiceName } from '../services/build_service_name';
 
-describe('getLocationTitleFromAddresses', () => {
+describe('build service name', () => {
     it('returns organization and service name when they differ', () => {
         const organizationName = aString();
         const serviceName = aString();
         const expected = `${organizationName} - ${serviceName}`;
         expect(buildServiceName(organizationName, serviceName)).toBe(expected);
     });
-    it('returns just service name when they are the same', () => {
+    it('returns just service name when organization name and service name are the same', () => {
         const serviceName = aString();
         const organizationName = serviceName;
         expect(buildServiceName(organizationName, serviceName)).toBe(serviceName);
