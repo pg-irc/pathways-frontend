1ee066aecac8ca22819a881610c24db3d32b04b7
rasmus <rasmus@peacegeeks.org>
2020-08-12 10:08:04 -0700

    Issue PeaceGeeksSociety/content#764 Build warns on stale deploy working directories with secrets

--- bin/check_for_deploy.sh
+++ bin/check_for_deploy.sh
@@ -1,5 +1,5 @@
-if [ -d ../pathways-deploy/ ]; then
-    echo "../pathways-deploy exists, confirm with 'yes' to continue the build"
+if [ -d ../pathways-deploy/ ] || [ -d ../*/pathways-deploy/ ]; then
+    echo "../pathways-deploy or ../*/pathways-deploy exists, confirm with 'yes' to continue the build"
     read answer
     if [ "$answer" = "yes" ]; then
         exit 0
