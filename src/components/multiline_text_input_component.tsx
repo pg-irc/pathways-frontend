a16ebde989d73b494bdf7db7388469797269186f
Tomy Reyes <tomy@peacegeeks.org>
2020-08-28 14:48:39 -0700

    Issue #1202 create IOS specific text input

--- src/components/multiline_text_input_component.tsx
+++ src/components/multiline_text_input_component.tsx
@@ -1,6 +1,8 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
-import { TextInput, StyleProp, TextStyle, View } from 'react-native';
+import * as constants from '../application/constants';
+import { TextInput, StyleProp, TextStyle, View, InputAccessoryView } from 'react-native';
 import { colors } from '../application/styles';
+import { isAndroid } from '../application/helpers/is_android';
 
 export interface MultilineTextInputProps {
     readonly i18n: I18n;
@@ -16,8 +18,23 @@ export const MultilineTextInputComponent = (props: MultilineTextInputProps): JSX
     const onBlur = (): void => setTextcolor(colors.teal);
     const onFocus = (): void => setTextcolor(colors.black);
 
+    if (isAndroid()) {
+        return (
+            <AndroidTextInput
+                i18n={props.i18n}
+                value={props.value}
+                numberOfLines={props.numberOfLines}
+                placeholder={props.placeholder}
+                style={[props.style, { color: textColor}]}
+                onChangeText={props.onChangeText}
+                onBlur={onBlur}
+                onFocus={onFocus}
+            />
+        );
+    }
+
     return (
-        <AndroidTextInput
+        <IOSTextInput
             i18n={props.i18n}
             value={props.value}
             numberOfLines={props.numberOfLines}
@@ -47,5 +64,25 @@ const AndroidTextInput = (props: MultilineTextInputProps & TextInputProps): JSX.
             textAlignVertical='top'
             value={props.value}
         />
+    </View>
+);
+
+const IOSTextInput = (props: MultilineTextInputProps & TextInputProps): JSX.Element => (
+    <View>
+        <TextInput
+            multiline
+            numberOfLines={5}
+            onChangeText={props.onChangeText}
+            placeholder={props.i18n._(props.placeholder)}
+            placeholderTextColor={colors.darkerGrey}
+            style={props.style}
+            textAlignVertical='top'
+            value={props.value}
+            inputAccessoryViewID={constants.FEEDBACK_INPUT_ID}
+        />
+        {/* specific for ios only */}
+        <InputAccessoryView nativeID={constants.FEEDBACK_INPUT_ID}>
+            {/* Done button */}
+        </InputAccessoryView>
     </View>
 );
