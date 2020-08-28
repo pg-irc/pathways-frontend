8356db4d3caedf2862e0762daf5c103a0a12316d
Tomy Reyes <tomy@peacegeeks.org>
2020-08-28 15:02:38 -0700

    Issue #1202 create multiline keyboard done button that closes keyboard

--- src/components/multiline_text_input_component.tsx
+++ src/components/multiline_text_input_component.tsx
@@ -1,8 +1,10 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import * as constants from '../application/constants';
-import { TextInput, StyleProp, TextStyle, View, InputAccessoryView } from 'react-native';
-import { colors } from '../application/styles';
+import { TextInput, StyleProp, TextStyle, View, InputAccessoryView, TouchableOpacity, Text, Keyboard } from 'react-native';
+import { colors, getBoldFontStylesForOS } from '../application/styles';
 import { isAndroid } from '../application/helpers/is_android';
+import { useKeyboardIsVisible } from './use_keyboard_is_visible';
+import { EmptyComponent } from './empty_component/empty_component';
 
 export interface MultilineTextInputProps {
     readonly i18n: I18n;
@@ -67,22 +69,58 @@ const AndroidTextInput = (props: MultilineTextInputProps & TextInputProps): JSX.
     </View>
 );
 
-const IOSTextInput = (props: MultilineTextInputProps & TextInputProps): JSX.Element => (
-    <View>
-        <TextInput
-            multiline
-            numberOfLines={5}
-            onChangeText={props.onChangeText}
-            placeholder={props.i18n._(props.placeholder)}
-            placeholderTextColor={colors.darkerGrey}
-            style={props.style}
-            textAlignVertical='top'
-            value={props.value}
-            inputAccessoryViewID={constants.FEEDBACK_INPUT_ID}
-        />
-        {/* specific for ios only */}
-        <InputAccessoryView nativeID={constants.FEEDBACK_INPUT_ID}>
-            {/* Done button */}
-        </InputAccessoryView>
-    </View>
-);
+const IOSTextInput = (props: MultilineTextInputProps & TextInputProps): JSX.Element => {
+    const keyboardIsVisible = useKeyboardIsVisible();
+    return (
+        <View>
+            <TextInput
+                multiline
+                numberOfLines={5}
+                onChangeText={props.onChangeText}
+                placeholder={props.i18n._(props.placeholder)}
+                placeholderTextColor={colors.darkerGrey}
+                style={props.style}
+                textAlignVertical='top'
+                value={props.value}
+                inputAccessoryViewID={constants.FEEDBACK_INPUT_ID}
+            />
+            <InputAccessoryView nativeID={constants.FEEDBACK_INPUT_ID}>
+                <MultilineKeyboardDoneButton isVisible={keyboardIsVisible}/>
+            </InputAccessoryView>
+        </View>
+    );
+}
+
+interface MultilineKeyboardDoneButton {
+    readonly isVisible: boolean;
+}
+
+const MultilineKeyboardDoneButton = ({isVisible}: MultilineKeyboardDoneButton): JSX.Element => {
+    const onPress = (): void => {
+        // tslint:disable-next-line: no-expression-statement
+        Keyboard.dismiss();
+    };
+
+    if (!isVisible) {
+        return <EmptyComponent />;
+    }
+
+    return (
+        <View
+            style={{
+                flexDirection: 'row-reverse',
+                alignContent: 'center',
+                borderColor: colors.greyBorder,
+                borderTopWidth: 1,
+                backgroundColor: colors.white,
+            }}
+        >
+            <TouchableOpacity
+                style={{ marginRight: 12, marginVertical: 10 }}
+                onPress={onPress}
+            >
+                <Text style={{ color: colors.teal, fontSize: 18, ...getBoldFontStylesForOS() }}>Done</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
