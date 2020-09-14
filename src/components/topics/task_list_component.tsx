5a5f412bac3aecdb5d5e007dc8772cd09d63e5d5
rasmus <rasmus@peacegeeks.org>
2020-09-14 10:56:55 -0700

    Issue #1190 Wire up task list component to scroll event

--- src/components/topics/task_list_component.tsx
+++ src/components/topics/task_list_component.tsx
@@ -1,6 +1,6 @@
 // tslint:disable:no-class no-this no-expression-statement readonly-keyword
 import React from 'react';
-import { FlatList, ListRenderItemInfo } from 'react-native';
+import { FlatList, ListRenderItemInfo, NativeSyntheticEvent, ScrollViewProperties } from 'react-native';
 import { History } from 'history';
 import { TaskListItemComponent } from './task_list_item_component';
 import { Routes, goToRouteWithParameter } from '../../application/routing';
@@ -40,6 +40,7 @@ type State = {
     readonly sections: ReadonlyArray<ReadonlyArray<ListItem>>;
     readonly sectionCount: number;
     readonly data: ReadonlyArray<ListItem>;
+    readonly scrollOffset: number;
 };
 
 type TaskListItemInfo = ListRenderItemInfo<ListItem>;
@@ -50,7 +51,8 @@ export class TaskListComponent extends React.PureComponent<Props, State> {
 
     constructor(props: Props) {
         super(props);
-        this.state = this.initialState();
+        console.log(`constructor with offset ${props.scrollOffset}`);
+        this.state = this.initialState(props.scrollOffset);
         this.setFlatListRef = this.setFlatListRef.bind(this);
         this.loadMoreData = this.loadMoreData.bind(this);
     }
@@ -58,13 +60,24 @@ export class TaskListComponent extends React.PureComponent<Props, State> {
     componentDidUpdate(previousProps: Props): void {
         if (previousProps.headerContentIdentifier !== this.props.headerContentIdentifier) {
             this.flatListRef.scrollToOffset({ animated: false, offset: 0 });
+        } else if (this.flatListRef) {
+            console.log(`initializing scrolling to ${this.state.scrollOffset}`);
+            this.flatListRef.scrollToOffset({ animated: false, offset: this.state.scrollOffset});
         }
         if (this.tasksHaveChanged(previousProps)) {
-            this.setState(this.initialState());
+            const scrollToTopOfThePage = 0;
+            this.setState(this.initialState(scrollToTopOfThePage));
         }
     }
 
     render(): JSX.Element {
+        const onScrollEnd = (e: NativeSyntheticEvent<ScrollViewProperties>): void => {
+            console.log(`end drag getting scroll offset ${e.nativeEvent.contentOffset.y}`);
+            this.setState({
+                ...this.state,
+                scrollOffset: e.nativeEvent.contentOffset.y,
+            });
+        };
         return (
             <FlatList
                 ref={this.setFlatListRef}
@@ -77,6 +90,7 @@ export class TaskListComponent extends React.PureComponent<Props, State> {
                 ListEmptyComponent={this.props.emptyTaskListContent}
                 ListHeaderComponent={this.props.headerContent}
                 initialNumToRender={this.numberOfItemsPerSection}
+                onScrollEndDrag={onScrollEnd}
             />
         );
     }
@@ -88,12 +102,13 @@ export class TaskListComponent extends React.PureComponent<Props, State> {
         return R.not(R.equals(previousIds, currentIds));
     }
 
-    private initialState(): State {
+    private initialState(scrollOffset: number): State {
         const sections = this.taskSections();
         return {
             sections: sections,
             sectionCount: 1,
             data: sections[0],
+            scrollOffset,
         };
     }
 
@@ -122,6 +137,12 @@ export class TaskListComponent extends React.PureComponent<Props, State> {
         this.flatListRef = component as FlatListRef;
     }
 
+    private saveScrollOffsetToReduxAndGoToTopicDetail(item: ListItem): void {
+        console.log(`Saving scroll offset to redux: ${this.state.scrollOffset}`);
+        this.props.saveScrollOffset(this.state.scrollOffset);
+        goToRouteWithParameter(Routes.TopicDetail, item.id, this.props.history)();
+    }
+
     private renderTaskListItem(item: ListItem, props: Props): JSX.Element {
         if (isTopicListHeading(item)) {
             return <TopicListHeadingComponent heading={item.heading} icon={item.icon} />;
@@ -132,8 +153,8 @@ export class TaskListComponent extends React.PureComponent<Props, State> {
                 taskIsBookmarked={R.contains(item.id, props.bookmarkedTopicsIdList)}
                 bookmarkTopic={props.bookmarkTopic}
                 unbookmarkTopic={props.unbookmarkTopic}
-                goToTaskDetail={goToRouteWithParameter(Routes.TopicDetail, item.id, props.history)}
+                goToTaskDetail={(): void => this.saveScrollOffsetToReduxAndGoToTopicDetail(item)}
             />
         );
     }
-};
+}
