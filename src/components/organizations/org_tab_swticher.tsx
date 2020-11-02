af0ed0cb89c5dd9e503307b5151bb80e17bdbf39
Ian Lin <ian@peacegeeks.org>
2020-11-02 10:52:46 -0800

    Issue #1260 Duplicate render tab bar logic

--- src/components/organizations/org_tab_swticher.tsx
+++ src/components/organizations/org_tab_swticher.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Text, Dimensions } from 'react-native';
-import { TabView, Route } from 'react-native-tab-view';
+import { TabView, Route, NavigationState, SceneRendererProps, TabBar } from 'react-native-tab-view';
 import { t } from '@lingui/macro';
 import { EmptyComponent } from '../empty_component/empty_component';
+import { colors, textStyles } from '../../application/styles';
 
 // tslint:disable-next-line: readonly-array
 export type TabRoutes = Array<Route>;
@@ -37,8 +38,24 @@ export default function OrgTabSwitcher(props: OrgTabSwitcherProps) {
         <TabView
             navigationState={{ index, routes }}
             renderScene={renderScene}
+            renderTabBar={renderTabBar}
             onIndexChange={setIndex}
             initialLayout={{width: Dimensions.get('window').width}}
         />
     );
 }
+export interface NavigationStateRoute {
+    readonly navigationState: NavigationState<Route>;
+}
+
+type TabBarProps = SceneRendererProps & NavigationStateRoute;
+
+const renderTabBar = (tabBarProps: TabBarProps): JSX.Element => (
+    <TabBar
+        {...tabBarProps}
+        style={{ backgroundColor: colors.white, elevation: 0 }}
+        indicatorStyle={{ backgroundColor: colors.teal, height: 4 }}
+        getLabelText={({ route }: { readonly route: Route}): string => route.title}
+        labelStyle={textStyles.paragraphStyle}
+    />
+);
