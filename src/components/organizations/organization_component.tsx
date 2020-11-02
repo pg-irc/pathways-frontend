955d2ddbfc7fa1060fe4a9b6613fc6a1b1914f03
Ian Lin <ian@peacegeeks.org>
2020-11-02 11:43:24 -0800

    Issue #1260 Remove all references to the old native base tab system

--- src/components/organizations/organization_component.tsx
+++ src/components/organizations/organization_component.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Trans } from '@lingui/react';
-import { Tab, Tabs, TabHeading, Content, Text, View } from 'native-base';
-import { textStyles, colors } from '../../application/styles';
+import { Content, View } from 'native-base';
+import { colors } from '../../application/styles';
 import { DescriptorComponent } from '../content_layout/descriptor_component';
 import { TitleComponent } from '../content_layout/title_component';
 import { HeaderComponent } from '../main/header_component';
@@ -18,8 +18,6 @@ import { HumanOrganizationData } from '../../validation/organizations/types';
 import { EmptyComponent } from '../empty_component/empty_component';
 import { SearchServiceData } from '../../validation/search/types';
 import { fetchServicesFromOrganization } from '../search/api/fetch_search_results_from_query';
-import { OrganizationServiceListComponent } from './organization_service_list_component';
-import { OrganizationDetailComponent } from './organization_detail_component';
 import { I18n } from '@lingui/react';
 import OrgTabSwitcher from './org_tab_swticher';
 
@@ -40,14 +38,6 @@ export interface OrganizationActions {
 type Props = OrganizationProps & OrganizationActions & RouterProps;
 
 export const OrganizationComponent = (props: Props): JSX.Element => {
-    // NativeBase's (Buggy) Tabs component notes:
-    //
-    // - Breaking out the Tab and TabHeading components from the Tabs component does not work as
-    //   they do not render in isolation, this makes our component a little harder to split up
-    // - The activeTextStyle prop only works with a text heading, we need to use a component for
-    //   translation purposes,
-    //   see: https://stackoverflow.com/questions/43113859/customise-tabs-of-native-base,
-    //   this means we cannot sensibly style active tab text
 
     const [organization, setOrganization]: readonly [HumanOrganizationData, (org: HumanOrganizationData) => void] = useState(undefined);
     const [services, setOrganizationServices]: readonly [ReadonlyArray<SearchServiceData>, (services: ReadonlyArray<SearchServiceData>) => void] = useState(undefined);
@@ -92,44 +82,6 @@ export const OrganizationComponent = (props: Props): JSX.Element => {
                             openHeaderMenu={props.openHeaderMenu}
                             currentPathForAnalytics={props.location.pathname}
                              />
-                        <Tabs tabBarUnderlineStyle={{ backgroundColor: colors.teal }}>
-                            <Tab
-                                heading={
-                                    <TabHeading style={{ backgroundColor: colors.white }}>
-                                        <Text style={textStyles.paragraphStyle}>
-                                            <Trans>About</Trans>
-                                        </Text>
-                                    </TabHeading>
-                                }
-                            >
-                                <OrganizationDetailComponent
-                                    organization={organization}
-                                    analyticsLinkPressed={props.analyticsLinkPressed}
-                                    currentPathForAnalytics={props.location.pathname}
-                                />
-                            </Tab>
-                            <Tab
-                                heading={
-                                    <TabHeading style={{ backgroundColor: colors.white }}>
-                                        <Text style={textStyles.paragraphStyle}>
-                                            <Trans>Services</Trans>
-                                        </Text>
-                                    </TabHeading>
-                                }
-                            >
-                                <OrganizationServiceListComponent
-                                    services={services}
-                                    history={props.history}
-                                    bookmarkService={props.bookmarkService}
-                                    unbookmarkService={props.unbookmarkService}
-                                    openServiceDetail={props.openServiceDetail}
-                                    analyticsLinkPressed={props.analyticsLinkPressed}
-                                    currentPathForAnalytics={props.location.pathname}
-                                    organizationServicesOffset={props.organizationServicesOffset}
-                                    saveOrganizationServicesOffset={props.saveOrganizationServicesOffset}
-                                />
-                            </Tab>
-                        </Tabs>
                     </Content>
                 </View>
             )}
