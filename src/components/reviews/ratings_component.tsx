9c209ea0d7292f8811ed5f460e52adead3e91d84
Tomy Reyes <tomy@peacegeeks.org>
2021-01-15 14:55:43 -0800

    Issue #1344 use rating component from react-native-ratings

--- src/components/reviews/ratings_component.tsx
+++ src/components/reviews/ratings_component.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Trans } from '@lingui/react';
 import { textStyles } from '../../application/styles';
+import { AirbnbRating } from 'react-native-ratings';
 
 export const RatingsComponent = (): JSX.Element => {
+    const ratingCompleted = (rating: number): void => {
+        // navigate to next screen
+        // choose rating action
+        console.log('Rating is: ' + rating)
+    };
     return (
         <View>
             <Text style={textStyles.headlineH3StyleBlackCenter}>
                 <Trans>Review this service</Trans>
             </Text>
+            <AirbnbRating
+                count={5}
+                defaultRating={0}
+                size={30}
+                showRating={false}
+                onFinishRating={ratingCompleted}
+            />
         </View>
     );
 };
+
