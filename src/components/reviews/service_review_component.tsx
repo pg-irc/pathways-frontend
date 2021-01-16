1f31258dfc7623db46013d8d32f071c5a3b9f380
Tomy Reyes <tomy@peacegeeks.org>
2021-01-15 16:25:24 -0800

    Issue #1344 add RatingsComponent to service service screen

--- src/components/reviews/service_review_component.tsx
+++ src/components/reviews/service_review_component.tsx
@@ -3,17 +3,29 @@ import { Text, View } from 'react-native';
 import { CloseButtonComponent } from '../close_button_component';
 import { colors, textStyles, applicationStyles } from '../../application/styles';
 import { Header } from 'native-base';
+import { RatingsComponent } from './ratings_component';
+import { chooseRating, ChooseRatingAction } from '../../stores/reviews/actions';
 
 export interface ServiceReviewProps {
     readonly serviceId: string;
     readonly serviceName: string;
+    readonly rating: number;
 }
 
-export const ServiceReviewComponent = (props: ServiceReviewProps): JSX.Element => (
-    <View>
+export interface ServiceReviewActions {
+    readonly chooseRating: (rating: number) => ChooseRatingAction;
+}
+
+type Props = ServiceReviewProps & ServiceReviewActions;
+
+export const ServiceReviewComponent = (props: Props): JSX.Element => {
+    return (
+        <View>
         <HeaderComponent name={props.serviceName}/>
+        <RatingsComponent rating={props.rating} onFinishRating={chooseRating}/>
     </View>
-);
+    );
+};
 
 export const HeaderComponent = ({name}: {readonly name: string}): JSX.Element => (
     <Header style={applicationStyles.headerContainer} androidStatusBarColor={colors.teal}>
--- src/components/reviews/service_review_connected_component.ts
+++ src/components/reviews/service_review_connected_component.ts
@@ -1,15 +1,26 @@
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { RouterProps } from '../../application/routing';
+import { selectRating } from '../../selectors/reviews/select_rating';
 import { selectServiceNameById } from '../../selectors/services/select_service_name_by_id';
 import { Store } from '../../stores';
-import { ServiceReviewComponent, ServiceReviewProps } from './service_review_component';
+import { chooseRating, ChooseRatingAction } from '../../stores/reviews/actions';
+import { ServiceReviewActions, ServiceReviewComponent, ServiceReviewProps } from './service_review_component';
 
 const mapStateToProps = (store: Store, ownProps: RouterProps): ServiceReviewProps => {
     const serviceId = ownProps.match.params.serviceId;
     return {
         serviceId: serviceId,
         serviceName: selectServiceNameById(store, serviceId),
+        rating: selectRating(store),
     };
 };
 
-export const ServiceReviewConnectedComponent = connect(mapStateToProps)(ServiceReviewComponent);
+type Actions = ChooseRatingAction;
+
+const mapDispatchToProps = (dispatch: Dispatch<Actions>): ServiceReviewActions => ({
+    chooseRating: (rating: number): ChooseRatingAction =>
+        dispatch(chooseRating(rating)),
+});
+
+export const ServiceReviewConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(ServiceReviewComponent);
