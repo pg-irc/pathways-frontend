c475ad76ca703016123fc858b6939c87fafb2460
Tomy Reyes <tomy@peacegeeks.org>
2021-01-18 13:47:49 -0800

    Issue #1344 add actions for the discard changes modal

--- src/stores/reviews/actions.ts
+++ src/stores/reviews/actions.ts
@@ -6,6 +6,20 @@ export const chooseRating = (rating: number) => (
     helpers.makeAction(constants.CHOOSE_RATING, { rating })
 );
 
-export type ChooseRatingAction = Readonly<ReturnType<typeof chooseRating>>;
+// tslint:disable-next-line: typedef
+export const openDiscardChangesModal = () => (
+    helpers.makeAction(constants.OPEN_DISCARD_CHANGES_MODAL)
+);
 
-export type ReviewAction = ChooseRatingAction;
+// tslint:disable-next-line: typedef
+export const closeDiscardChangesModal = () => (
+    helpers.makeAction(constants.CLOSE_DISCARD_CHANGES_MODAL)
+);
+
+export type ChooseRatingAction = Readonly<ReturnType<typeof chooseRating>>;
+export type OpenDiscardChangesModalAction = Readonly<ReturnType<typeof openDiscardChangesModal>>;
+export type CloseDiscardChangesModalAction  = Readonly<ReturnType<typeof closeDiscardChangesModal>>;
+
+export type ReviewAction = ChooseRatingAction |
+    OpenDiscardChangesModalAction |
+    CloseDiscardChangesModalAction;
