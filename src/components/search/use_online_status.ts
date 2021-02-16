b5a0c65f153d55ce8ecbd2a38aa0659cff03ed6a
Tomy Reyes <tomy@peacegeeks.org>
2021-02-16 14:19:44 -0800

    Issue #1360 clean up onlineStatus to prevent additional render

--- src/components/search/use_online_status.ts
+++ src/components/search/use_online_status.ts
@@ -13,7 +13,10 @@ const toStatus = (online: boolean): OnlineStatus => (online ? OnlineStatus.Onlin
 type SetOnlineStatus = Dispatch<SetStateAction<OnlineStatus>>;
 
 export const useOnlineStatus = (): OnlineStatus => {
-    const [onlineStatus, setOnlineStatus]: [OnlineStatus, SetOnlineStatus] = useState(OnlineStatus.Loading);
-    useEffect(() => { isDeviceOnline().then((isOnline: boolean) => setOnlineStatus(toStatus(isOnline))); });
+    const [onlineStatus, setOnlineStatus]: readonly [OnlineStatus, SetOnlineStatus] = useState(OnlineStatus.Loading);
+    useEffect((): () => Promise<void> => {
+        const unsubscribe = isDeviceOnline().then((isOnline: boolean): void => setOnlineStatus(toStatus(isOnline)));
+        return (): Promise<void> => unsubscribe;
+    }, []);
     return onlineStatus;
 };
