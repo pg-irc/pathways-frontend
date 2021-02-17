55a64ab420e10e75e34739b69ddce40ef9b39108
Tomy Reyes <tomy@peacegeeks.org>
2021-02-17 09:51:45 -0800

    Issue #1360 clean up

--- src/components/search/use_ask_location_permission.ts
+++ src/components/search/use_ask_location_permission.ts
@@ -3,16 +3,18 @@ import * as Permissions from 'expo-permissions';
 import { useEffect } from 'react';
 import { MY_LOCATION_MESSAGE_DESCRIPTOR } from '../partial_localization/to_location_for_query';
 import { getPermission } from './search_input_component';
-// tslint:disable-next-line: max-line-length
-export const useAskLocationPermission = (setSearchLocationInput: (location: string) => void, saveSearchLocation: (location: string) => void, i18n: I18n): void => {
+
+type LocationSetter = (location: string) => void;
+
+export const useAskLocationPermission = (setLocationInput: LocationSetter, saveLocation: LocationSetter, i18n: I18n): void => {
     useEffect((): void => {
         const askLocationPermission = async (): Promise<void> => {
             const status = await getPermission();
             if (status === Permissions.PermissionStatus.UNDETERMINED) {
                 const permissionResponse = await Permissions.askAsync(Permissions.LOCATION);
                 if (permissionResponse.status === Permissions.PermissionStatus.GRANTED) {
-                    setSearchLocationInput(i18n._(MY_LOCATION_MESSAGE_DESCRIPTOR));
-                    saveSearchLocation(i18n._(MY_LOCATION_MESSAGE_DESCRIPTOR));
+                    setLocationInput(i18n._(MY_LOCATION_MESSAGE_DESCRIPTOR));
+                    saveLocation(i18n._(MY_LOCATION_MESSAGE_DESCRIPTOR));
                 }
             }
         };
