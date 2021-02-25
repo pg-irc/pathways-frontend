92cb701490e21beee5f208ab8859c815e2938f36
rasmus <rasmus@peacegeeks.org>
2021-02-25 11:50:01 -0800

    Issue #1391 Remove isVisible parameter to divider component

--- src/components/content_layout/divider_component.tsx
+++ src/components/content_layout/divider_component.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { colors } from '../../application/styles';
-import { EmptyComponent } from '../empty_component/empty_component';
 
-interface Props {
-    readonly isVisible?: boolean;
-}
-
-export const DividerComponent = ({ isVisible = true }: Props): JSX.Element => (
-    isVisible ? <View style={{ height: 1, marginVertical: 15, marginHorizontal: -20, backgroundColor: colors.grey }}></View> : <EmptyComponent />
+export const DividerComponent = (): JSX.Element => (
+    <View style={{ height: 1, marginVertical: 15, marginHorizontal: -20, backgroundColor: colors.grey }}></View>
 );
