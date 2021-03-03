dbf0a5e31758570c33a3065ca228d1638d0c6f46
Tomy Reyes <tomy@peacegeeks.org>
2021-03-03 12:59:06 -0800

    Issue #1385 wire up with component

--- src/components/reviews/service_review_connected_component.ts
+++ src/components/reviews/service_review_connected_component.ts
@@ -3,12 +3,14 @@ import { Dispatch } from 'redux';
 import { RouterProps } from '../../application/routing';
 import { selectIsSendingReview } from '../../selectors/reviews/select_is_sending_review';
 import { selectRating } from '../../selectors/reviews/select_rating';
+import { selectComment } from '../../selectors/reviews/select_comment';
 import { selectShowDiscardChangesModal } from '../../selectors/reviews/select_show_discard_changes_modal';
 import { selectServiceNameById } from '../../selectors/services/select_service_name_by_id';
 import { Store } from '../../stores';
 import { Rating } from '../../stores/reviews';
 import { chooseRating, ChooseRatingAction, clearReview, ClearReviewAction, closeDiscardChangesModal,
-    CloseDiscardChangesModalAction, openDiscardChangesModal, OpenDiscardChangesModalAction, submitServiceReview, SubmitServiceReviewAction,
+    CloseDiscardChangesModalAction, openDiscardChangesModal, OpenDiscardChangesModalAction,
+    saveComment, SaveCommentAction, submitServiceReview, SubmitServiceReviewAction,
 } from '../../stores/reviews/actions';
 import { Id } from '../../stores/services';
 import { ServiceReviewActions, ServiceReviewComponent, ServiceReviewProps } from './service_review_component';
@@ -19,6 +21,7 @@ const mapStateToProps = (store: Store, ownProps: RouterProps): ServiceReviewProp
         serviceId: serviceId,
         serviceName: selectServiceNameById(store, serviceId),
         rating: selectRating(store),
+        comment: selectComment(store),
         showDiscardChangesModal: selectShowDiscardChangesModal(store),
         isSending: selectIsSendingReview(store),
     };
@@ -28,10 +31,12 @@ type Actions = ChooseRatingAction |
     OpenDiscardChangesModalAction |
     CloseDiscardChangesModalAction |
     SubmitServiceReviewAction |
-    ClearReviewAction;
+    ClearReviewAction |
+    SaveCommentAction;
 
 const mapDispatchToProps = (dispatch: Dispatch<Actions>): ServiceReviewActions => ({
     chooseRating: (rating: Rating, serviceId: Id): ChooseRatingAction => dispatch(chooseRating(rating, serviceId)),
+    saveComment: (comment: string): SaveCommentAction => dispatch(saveComment(comment)),
     openDiscardChangesModal: (): OpenDiscardChangesModalAction => dispatch(openDiscardChangesModal()),
     closeDiscardChangesModal: (): CloseDiscardChangesModalAction => dispatch(closeDiscardChangesModal()),
     submitServiceReview: (serviceId: Id, comment: string): SubmitServiceReviewAction => dispatch(submitServiceReview(serviceId, comment)),
