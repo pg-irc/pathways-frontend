127db7e2272e2878e64d705d91c9b25e05e7d8b9
rasmus <rasmus@peacegeeks.org>
2021-03-30 15:00:52 -0700

    Issue pg-irc/pathways-backend#615 Update home screen title to reflect current region

--- src/components/recommended_topics/recommended_topics_connected_component.tsx
+++ src/components/recommended_topics/recommended_topics_connected_component.tsx
@@ -13,6 +13,7 @@ import { selectShowLinkAlerts } from '../../selectors/user_profile/select_show_l
 import { AnalyticsLinkPressedAction, analyticsLinkPressed, AnalyticsLinkProps } from '../../stores/analytics';
 import { OpenHeaderMenuAction, openHeaderMenu } from '../../stores/user_experience/actions';
 import { hideLinkAlerts, HideLinkAlertsAction } from '../../stores/user_profile';
+import { selectRegion } from '../../selectors/region/select_region';
 
 const mapStateToProps = (store: Store): RecommendedTopicsProps => ({
     hasChosenAnswers: getIdsOfChosenAnswers(pickAnswers(store)).length > 0,
@@ -20,6 +21,7 @@ const mapStateToProps = (store: Store): RecommendedTopicsProps => ({
     recommendedTopics: selectRecommendedTopics(store),
     alerts: selectAlerts(store),
     showLinkAlerts: selectShowLinkAlerts(store),
+    region: selectRegion(store),
 });
 
 type Actions = BookmarkTopicAction | UnbookmarkTopicAction | OpenHeaderMenuAction | AnalyticsLinkPressedAction | HideLinkAlertsAction;
