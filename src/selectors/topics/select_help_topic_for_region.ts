5f3157641b44ae6da787f50c0f8316e0d02eed93
rasmus <rasmus@peacegeeks.org>
2021-04-01 12:35:45 -0700

    Issue #1460 Use enum rather than string form for region

--- src/selectors/topics/select_help_topic_for_region.ts
+++ src/selectors/topics/select_help_topic_for_region.ts
@@ -1,12 +1,13 @@
 import { Store } from '../../stores';
 import { pickRegion } from '../region/pick_region';
+import { RegionCode } from '../../validation/region/types';
 
 export const selectHelpTopicForRegion = (appStore: Store): string => {
-    const region = pickRegion(appStore)?.toLowerCase();
-    if (!region || region === 'bc') {
+    const region = pickRegion(appStore);
+    if (!region || region === RegionCode.BC) {
         return 'contact-workers-at-your-local-settlement-agency_bc';
     }
-    if (region === 'mb') {
+    if (region === RegionCode.MB) {
         return 'contact-workers-at-your-local-settlement-agency_mb';
     }
     throw new Error('Invalid region');
