91231231692e1c3c4009ee0b657a9844d23227cc
rasmus <rasmus@peacegeeks.org>
2021-04-01 11:54:34 -0700

    Issue #1451 Fix crash when deleting all memory for the app

--- src/selectors/topics/filter_topics_by_region.ts
+++ src/selectors/topics/filter_topics_by_region.ts
@@ -2,5 +2,5 @@ import { TopicMap } from '../../stores/topics';
 import * as R from 'ramda';
 
 export const filterTopicsByRegion = (region: string, topics: TopicMap): TopicMap => (
-    R.filter(R.propEq('region', region), topics)
+    R.filter(R.propEq('region', region || 'bc'), topics)
 );
