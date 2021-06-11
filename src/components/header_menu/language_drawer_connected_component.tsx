236db7cb96a987f0141dba8a24384a26cbd87a9c
Ian Lin <ian@peacegeeks.org>
2021-06-11 14:53:08 -0700

    Issue #1451 Move set locale logic to language drawer

--- src/components/header_menu/language_drawer_connected_component.tsx
+++ src/components/header_menu/language_drawer_connected_component.tsx
@@ -1,10 +1,32 @@
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import { Store } from '../../stores';
+import { SaveLocaleRequestAction, saveLocaleRequest } from '../../stores/locale/actions';
+import { selectAvailableLocales } from '../../selectors/locale/select_available_locales';
+import { selectLocale } from '../../selectors/locale/select_locale';
+import { PushNotificationTokenRequestAction, pushNotificationTokenRequest } from '../../sagas/post_push_notification_token';
+import { LocaleWithLabel } from '../../application/locales';
+import { pickRegion } from '../../selectors/region/pick_region';
 import { closeLanguageDrawer, CloseLanguageDrawerAction } from '../../stores/user_experience/actions';
-import { LanguageDrawerActions, LanguageDrawerComponent } from './language_drawer_component';
+import { LanguageDrawerActions, LanguageDrawerComponent, LanguageDrawerProps } from './language_drawer_component';
 
-const mapDispatchToProps = (dispatch: Dispatch<CloseLanguageDrawerAction>): LanguageDrawerActions => ({
+const selectOtherLocales = (store: Store): ReadonlyArray<LocaleWithLabel> => {
+    const locales = selectAvailableLocales(store);
+    const currentLocale = selectLocale(store);
+    return locales.filter((l: LocaleWithLabel): boolean => l.code !== currentLocale);
+};
+
+const mapStateToProps = (store: Store): LanguageDrawerProps => ({
+    currentLocale: selectLocale(store),
+    currentRegion: pickRegion(store),
+    otherLocales: selectOtherLocales(store),
+});
+
+type Actions = CloseLanguageDrawerAction | SaveLocaleRequestAction | PushNotificationTokenRequestAction;
+const mapDispatchToProps = (dispatch: Dispatch<Actions>): LanguageDrawerActions => ({
+    setLocale: (localeCode: string, flipOrientation: boolean): SaveLocaleRequestAction => dispatch(saveLocaleRequest(localeCode, flipOrientation)),
+    updateNotificationToken: (): PushNotificationTokenRequestAction => dispatch(pushNotificationTokenRequest()),
     closeLanguageDrawer: (): CloseLanguageDrawerAction => dispatch(closeLanguageDrawer()),
 });
 
-export const LanguageDrawerConnectedComponent = connect(undefined, mapDispatchToProps)(LanguageDrawerComponent);
+export const LanguageDrawerConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(LanguageDrawerComponent);
