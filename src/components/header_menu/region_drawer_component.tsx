6397cc55861bc96cd7a2db2a8c3343c6d3ca2eb1
Ian Lin <ian@peacegeeks.org>
2021-06-14 10:34:23 -0700

    Issue #1451 create a placeholder region drawer component

---
+++ src/components/header_menu/region_drawer_component.tsx
@@ -0,0 +1,31 @@
+// tslint:disable: no-expression-statement
+import React from 'react';
+import { View, Header, Title, Icon } from 'native-base';
+import { colors, textStyles } from '../../application/styles';
+import { getStatusBarHeightForPlatform } from '../main/get_status_bar_height_for_platform';
+import { isAndroid } from '../../application/helpers/is_android';
+import { I18nManager, TouchableOpacity } from 'react-native';
+import { Trans } from '@lingui/react';
+
+export const RegionDrawerComponent = (): JSX.Element => (
+    <View style={{ flex: 1, backgroundColor: getViewBackgroundColorForPlatform() }}>
+        <Header
+            style={{
+                backgroundColor: colors.lightTeal,
+                marginTop: getStatusBarHeightForPlatform(),
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+            }}
+            androidStatusBarColor={colors.teal}
+        >
+            <TouchableOpacity style={{ padding: 15 }} >
+                <Icon name={I18nManager.isRTL ? 'arrow-forward' : 'arrow-back'} style={{ color: colors.white }} />
+            </TouchableOpacity >
+            <Title style={textStyles.headlineH3StyleWhiteCenter}><Trans>SELECT YOUR REGION</Trans></Title>
+        </Header>
+    </View>
+);
+
+const getViewBackgroundColorForPlatform = (): string => (
+    isAndroid() ? colors.teal : colors.white
+);
