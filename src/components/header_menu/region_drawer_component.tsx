4ac1d1b6633cc470fc3242bfe38ddffa889bee9d
Ian Lin <ian@peacegeeks.org>
2021-06-14 16:24:46 -0700

    Issue #1451 build out the rest of the region drawer component

--- src/components/header_menu/region_drawer_component.tsx
+++ src/components/header_menu/region_drawer_component.tsx
@@ -1,13 +1,14 @@
 // tslint:disable: no-expression-statement
 import React from 'react';
-import { View, Header, Title, Icon } from 'native-base';
-import { colors, textStyles } from '../../application/styles';
+import { View, Header, Title, Icon, Text } from 'native-base';
+import { applicationStyles, colors, textStyles } from '../../application/styles';
 import { getStatusBarHeightForPlatform } from '../main/get_status_bar_height_for_platform';
 import { isAndroid } from '../../application/helpers/is_android';
 import { I18nManager, TouchableOpacity } from 'react-native';
 import { Trans } from '@lingui/react';
 import { CloseRegionDrawerAction } from '../../stores/user_experience/actions';
-import { RegionCode } from '../../validation/region/types';
+import { RegionCode, regionCodeToLabel } from '../../validation/region/types';
+import { SaveRegionAction } from '../../stores/user_profile';
 
 export interface RegionDrawerProps {
     readonly currentRegion: RegionCode;
@@ -15,6 +16,7 @@ export interface RegionDrawerProps {
 
 export interface RegionDrawerActions {
     readonly closeRegionDrawer: () => CloseRegionDrawerAction;
+    readonly saveRegion: (region: RegionCode) => SaveRegionAction;
 }
 
 type Props = RegionDrawerProps & RegionDrawerActions;
@@ -35,7 +37,43 @@ export const RegionDrawerComponent = (props: Props): JSX.Element => (
             </TouchableOpacity >
             <Title style={textStyles.headlineH3StyleWhiteCenter}><Trans>SELECT YOUR REGION</Trans></Title>
         </Header>
+        <RegionSection {...props} />
     </View>
+);
+
+const RegionSection = (props: Props): JSX.Element => {
+    const otherRegion = props.currentRegion === RegionCode.BC ? RegionCode.MB : RegionCode.BC;
+    return (
+        <View>
+            <SelectedRegionItem {...props} />
+            <OtherRegion
+                region={otherRegion}
+                saveRegion={props.saveRegion} />
+        </View>
+    );
+};
+
+const SelectedRegionItem = (props: Props): JSX.Element => {
+    return (
+        <View key={props.currentRegion} style={applicationStyles.listItem}>
+            <View style={{ marginLeft: 12, marginRight: 7 }}>
+                <Icon
+                    name='check'
+                    type='FontAwesome'
+                    style={{ fontSize: 22, color: colors.teal, marginHorizontal: -2, marginVertical: -2 }}
+                />
+            </View>
+            <Text style={[textStyles.headlineH4StyleBlackLeft, { fontWeight: 'bold', marginLeft: 12 }]}>
+                {regionCodeToLabel(props.currentRegion)}
+            </Text>
+        </View>
+    );
+};
+
+const OtherRegion = (props: { readonly region: RegionCode, readonly saveRegion: (region: RegionCode) => SaveRegionAction }): JSX.Element => (
+    <TouchableOpacity key={props.region} style={applicationStyles.listItem} onPress={(): SaveRegionAction => props.saveRegion(props.region)}>
+        <Text style={[textStyles.headlineH4StyleBlackLeft, { marginLeft: 50 }]}>{regionCodeToLabel(props.region)}</Text>
+    </TouchableOpacity>
 );
 
 const getViewBackgroundColorForPlatform = (): string => (
--- src/components/header_menu/region_drawer_connected_component.tsx
+++ src/components/header_menu/region_drawer_connected_component.tsx
@@ -4,13 +4,18 @@ import { Store } from '../../stores';
 import { pickRegion } from '../../selectors/region/pick_region';
 import { closeRegionDrawer, CloseRegionDrawerAction } from '../../stores/user_experience/actions';
 import { RegionDrawerActions, RegionDrawerComponent, RegionDrawerProps } from './region_drawer_component';
+import { SaveRegionAction, saveRegion } from '../../stores/user_profile';
+import { RegionCode } from '../../validation/region/types';
 
 const mapStateToProps = (store: Store): RegionDrawerProps => ({
     currentRegion: pickRegion(store),
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<CloseRegionDrawerAction>): RegionDrawerActions => ({
+const mapDispatchToProps = (dispatch: Dispatch<CloseRegionDrawerAction | SaveRegionAction>): RegionDrawerActions => ({
     closeRegionDrawer: (): CloseRegionDrawerAction => dispatch(closeRegionDrawer()),
+    saveRegion: (regionCode: RegionCode): SaveRegionAction => (
+        dispatch(saveRegion(regionCode))
+    ),
 });
 
 export const RegionDrawerConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(RegionDrawerComponent);
