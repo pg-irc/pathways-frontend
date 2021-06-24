37f1e7ed254668737bb99217af8025486828c1b6
Brandon <brandon@peacegeeks.org>
2021-06-24 15:37:39 -0700

    Issue #1511 Seperate feedback strings into multiple lines

--- src/components/feedback/explain_feedback_component.tsx
+++ src/components/feedback/explain_feedback_component.tsx
@@ -1,4 +1,4 @@
-// tslint:disable:no-expression-statement max-line-length
+// tslint:disable:no-expression-statement
 import React from 'react';
 import { t } from '@lingui/macro';
 import { Text  } from 'react-native';
@@ -45,7 +45,9 @@ export const ExplainFeedbackComponent = (props: Props): JSX.Element => {
                 </Text>
                 <Text style={[textStyles.paragraphStyle, { marginTop: 10, marginBottom: 20 }]}>
                     <Trans>
-                    The services listed on Arrival Advisor come from <Link href={serviceUrl} style={textStyles.link} >{serviceName}</Link>, a nonprofit information and referral organization that publishes a searchable online directory of community, government, and social services in {regionName}. {serviceName}’s staff members collect and update this services information.
+                        The services listed on Arrival Advisor come from <Link href={serviceUrl} style={textStyles.link} >{serviceName}</Link>,
+                        a nonprofit information and referral organization that publishes a searchable online directory of community, government,
+                        and social services in {regionName}. {serviceName}’s staff members collect and update this services information.
                     </Trans>
                 </Text>
                 <Text style={textStyles.headlineH2StyleBlackLeft}>
@@ -53,7 +55,10 @@ export const ExplainFeedbackComponent = (props: Props): JSX.Element => {
                 </Text>
                 <Text style={[textStyles.paragraphStyle, { marginTop: 10, marginBottom: 20 }]}>
                     <Trans>
-                    You can make a suggestion by clicking “Suggest an update” at the bottom of a service’s detail page in Arrival Advisor. Then, you can provide more information in the form to let us know what needs to be updated. You can choose to leave your contact email so that our team can follow up with you. The Arrival Advisor team will work with {serviceName} to review your suggestions before we make the final changes.
+                        You can make a suggestion by clicking “Suggest an update” at the bottom of a service’s detail page in Arrival Advisor.
+                        Then, you can provide more information in the form to let us know what needs to be updated. You can choose to leave your
+                        contact email so that our team can follow up with you. The Arrival Advisor team will work with {serviceName} to review
+                        your suggestions before we make the final changes.
                     </Trans>
                 </Text>
                 <Text style={textStyles.headlineH2StyleBlackLeft}>
@@ -67,7 +72,13 @@ export const ExplainFeedbackComponent = (props: Props): JSX.Element => {
                 </Text>
                 <Text style={[textStyles.paragraphStyle, { marginTop: 10, marginBottom: 20 }]}>
                     <Trans>
-                    When you give feedback about a service, PeaceGeeks collect and shares with {serviceName} your suggestions as well as the name of the service you're commenting on. We record the date and time the suggestion was received. If you choose to provide your email address, we collect that so that PeaceGeeks or {serviceName} may contact you regarding your suggested change. If you work at the service provider organization, you will have the option to also share your full name, job title and organization name. It is useful for us to know that the suggestion comes from someone who is familiar with the organization's work. See also PeaceGeeks' general <Link href={'https://peacegeeks.org/privacy/'} style={textStyles.link}>privacy policy</Link> and <ServiceFAQText region={props.region} />.
+                        When you give feedback about a service, PeaceGeeks collect and shares with {serviceName} your suggestions as well as the name
+                        of the service you're commenting on. We record the date and time the suggestion was received. If you choose to provide your
+                        email address, we collect that so that PeaceGeeks or {serviceName} may contact you regarding your suggested change. If you
+                        work at the service provider organization, you will have the option to also share your full name, job title and organization
+                        name.It is useful for us to know that the suggestion comes from someone who is familiar with the organization's work. See
+                        also PeaceGeeks' general <Link href={'https://peacegeeks.org/privacy/'} style={textStyles.link}>privacy policy</Link>{' '}
+                        and <ServiceFAQText region={props.region} />.
                    </Trans>
                 </Text>
             </View>
@@ -101,13 +112,21 @@ const ServiceInclusionText = (props: { readonly region: RegionCode }): JSX.Eleme
     if (props.region === RegionCode.MB) {
         return (
             <Trans>
-                You can read <Link href={'https://mb.211.ca/forserviceproviders/'} style={textStyles.link}>211 Manitoba’s Inclusion Policy here</Link> to see what organizations and services are listed. You can also find more information and a form to list your organization or service on <Link href={'https://mb.211.ca/forserviceproviders/'} style={textStyles.link}>the 211 Manitoba website</Link>. Visit <Link href={'https://mb.211.ca/'} style={textStyles.link} >mb.211.ca</Link> for more information.
+                You can read{' '}
+                <Link href={'https://mb.211.ca/forserviceproviders/'} style={textStyles.link}>211 Manitoba’s Inclusion Policy here</Link>{' '}
+                to see what organizations and services are listed. You can also find more information and a form to list your organization or service
+                on <Link href={'https://mb.211.ca/forserviceproviders/'} style={textStyles.link}>the 211 Manitoba website</Link>.
+                Visit <Link href={'https://mb.211.ca/'} style={textStyles.link} >mb.211.ca</Link> for more information.
             </Trans>
         );
     }
     return (
         <Trans>
-            You can read <Link href={'http://www.bc211.ca/inclusion-policy/'} style={textStyles.link}>bc211’s Inclusion Policy here</Link> to see what organizations and services are listed. If you believe that your organization or service fits the inclusion criteria, you can email <Link href={'mailto:updates@bc211.ca'} style={textStyles.link}>updates@bc211.ca</Link> so the bc211 team can review your organization or service. Visit <Link href={'http://www.bc211.ca'} style={textStyles.link} >http://www.bc211.ca</Link> for more information.
+            You can read <Link href={'http://www.bc211.ca/inclusion-policy/'} style={textStyles.link}>bc211’s Inclusion Policy here</Link> to
+            see what organizations and services are listed. If you believe that your organization or service fits the inclusion criteria,
+            you can email <Link href={'mailto:updates@bc211.ca'} style={textStyles.link}>updates@bc211.ca</Link> so the bc211 team can review
+            your organization or service.
+            Visit <Link href={'http://www.bc211.ca'} style={textStyles.link} >http://www.bc211.ca</Link> for more information.
         </Trans>
     );
 };
